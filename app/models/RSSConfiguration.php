b5c39ef5ef10ca80816260030ef6a3e9a6c59acf
Marien Fressinaud <dev@marienfressinaud.fr>
2012-10-28 00:54:52 +0200

    nombre de non lus est màj directement en js

--- app/models/RSSConfiguration.php
+++ app/models/RSSConfiguration.php
@@ -100,7 +100,7 @@ class RSSConfigurationDAO extends Model_array {
 	public $shortcuts = array (
 		'mark_read' => 'r',
 		'mark_favorite' => 'f',
-		'go_website' => 'enter',
+		'go_website' => 'space',
 		'next_entry' => 'page_down',
 		'prev_entry' => 'page_up',
 		'next_page' => 'right',
