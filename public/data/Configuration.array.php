f53c01dcd49e2a1e974efb7b91bfe50981640b11
Marien Fressinaud <dev@marienfressinaud.fr>
2013-03-14 16:49:14 +0100

    Bien meilleure gestion des formulaires

--- public/data/Configuration.array.php
+++ public/data/Configuration.array.php
@@ -6,4 +6,13 @@ return array (
 	'sort_order' => 'low_to_high',
 	'old_entries' => 3,
 	'mail_login' => '',
+	'shortcuts' => array (
+		'mark_read' => 'r',
+		'mark_favorite' => 'f',
+		'go_website' => 'space',
+		'next_entry' => 'page_down',
+		'prev_entry' => 'page_up',
+		'next_page' => 'right',
+		'prev_page' => 'left',
+	),
 );
