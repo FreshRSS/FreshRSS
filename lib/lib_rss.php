81ea32d53e85b623356f56910b0a7aa9acf6bcd6
Marien Fressinaud <dev@marienfressinaud.fr>
2013-04-27 15:17:10 +0200

    Gestion de la traduction pour les dates (bug #38)

--- lib/lib_rss.php
+++ lib/lib_rss.php
@@ -20,55 +20,14 @@ function small_hash ($txt) {
 }
 
 function timestamptodate ($t, $hour = true) {
-	$jour = date ('d', $t);
-	$mois = date ('m', $t);
-	$annee = date ('Y', $t);
-	
-	switch ($mois) {
-	case 1:
-		$mois = 'janvier';
-		break;
-	case 2:
-		$mois = 'février';
-		break;
-	case 3:
-		$mois = 'mars';
-		break;
-	case 4:
-		$mois = 'avril';
-		break;
-	case 5:
-		$mois = 'mai';
-		break;
-	case 6:
-		$mois = 'juin';
-		break;
-	case 7:
-		$mois = 'juillet';
-		break;
-	case 8:
-		$mois = 'août';
-		break;
-	case 9:
-		$mois = 'septembre';
-		break;
-	case 10:
-		$mois = 'octobre';
-		break;
-	case 11:
-		$mois = 'novembre';
-		break;
-	case 12:
-		$mois = 'décembre';
-		break;
-	}
-	
-	$date = $jour . ' ' . $mois . ' ' . $annee;
+	$month = Translate::t (date('M', $t));
 	if ($hour) {
-		return $date . date (' \à H\:i', $t);
+		$date = Translate::t ('format_date_hour', $month);
 	} else {
-		return $date;
+		$date = Translate::t ('format_date', $month);
 	}
+
+	return date ($date, $t);
 }
 
 function sortEntriesByDate ($entry1, $entry2) {
