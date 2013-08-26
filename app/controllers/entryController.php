32842bef0a8bdec0cbeed04383ad2dcc97437486
Alexandre Alapetite <alexandre@alapetite.fr>
2013-08-26 21:07:17 +0200

    Cleaning after SQL model updated
    A "lastUpdate" field was forgotten

--- app/controllers/entryController.php
+++ app/controllers/entryController.php
@@ -90,7 +90,6 @@ class entryController extends ActionController {
 			if ($entry != false) {
 				$values = array (
 					'is_favorite' => $is_fav,
-					'lastUpdate' => time ()
 				);
 
 				$entryDAO->updateEntry ($entry->id (), $values);
