96bf9e5159df8a1217b2dbcff3210505c5c9ac1d
Alexandre Alapetite <alexandre@alapetite.fr>
2013-12-15 03:39:59 +0100

    set_include_path oublié
    Suite de
    https://github.com/marienfressinaud/FreshRSS/commit/755fdd9026c7d8c38fa34586a52bc80a97d81aaa

--- actualize_script.php
+++ actualize_script.php
@@ -6,14 +6,6 @@ $_GET['a'] = 'actualize';
 $_GET['force'] = true;
 $_SERVER['HTTP_HOST'] = '';
 
-set_include_path (get_include_path ()
-		 . PATH_SEPARATOR
-		 . LIB_PATH
-		 . PATH_SEPARATOR
-		 . LIB_PATH . '/minz'
-		 . PATH_SEPARATOR
-		 . APP_PATH);
-
 require(LIB_PATH . '/lib_rss.php');	//Includes class autoloader
 
 $front_controller = new FreshRSS ();
