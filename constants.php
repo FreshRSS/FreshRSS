f0c8cd8847335c0fa1060f6b58e9bb829a8fdd81
Alexandre Alapetite <alexandre@alapetite.fr>
2013-12-25 14:16:48 +0100

    Évite realpath pour problème open_basedir
    https://github.com/marienfressinaud/FreshRSS/issues/331
    À tester plus

--- constants.php
+++ constants.php
@@ -3,7 +3,8 @@ define('FRESHRSS_VERSION', '0.7-beta2');
 define('FRESHRSS_WEBSITE', 'http://freshrss.org');
 
 // Constantes de chemins
-define ('FRESHRSS_PATH', realpath (dirname (__FILE__)));
+define ('FRESHRSS_PATH', dirname(__FILE__));
+
 define ('PUBLIC_PATH', FRESHRSS_PATH . '/public');
 define ('DATA_PATH', FRESHRSS_PATH . '/data');
 define ('LIB_PATH', FRESHRSS_PATH . '/lib');
