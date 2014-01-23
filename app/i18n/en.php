e98ac32716dfe9343baa5bce7ad2c1e98c40540c
Alexis Degrugillier <github@ainw.org>
2014-01-22 19:06:46 -0500

    Ajout de statistiques de l'application
    Conflicts:
    	app/i18n/en.php
    	app/i18n/fr.php

--- app/i18n/en.php
+++ app/i18n/en.php
@@ -13,6 +13,7 @@ return array (
 	'category'			=> 'Category',
 	'shortcuts'			=> 'Shortcuts',
 	'about'				=> 'About',
+	'stats'				=> 'Statistics',
 
 	'your_rss_feeds'		=> 'Your RSS feeds',
 	'add_rss_feed'			=> 'Add a RSS feed',
@@ -20,7 +21,8 @@ return array (
 	'import_export_opml'		=> 'Import / export (OPML)',
 
 	'subscription_management'	=> 'Subscriptions management',
-	'all_feeds'			=> 'Main stream',
+	'main_stream'			=> 'Main stream',
+	'all_feeds'				=> 'All feeds',
 	'favorite_feeds'		=> 'Favourites (%d)',
 	'not_read'			=> '%d unread',
 	'not_reads'			=> '%d unread',
@@ -297,4 +299,14 @@ return array (
 	// format for date() function, %s allows to indicate month in letter
 	'format_date'			=> '%s j\<\s\u\p\>S\<\/\s\u\p\> Y',
 	'format_date_hour'		=> '%s j\<\s\u\p\>S\<\/\s\u\p\> Y \a\t H\:i',
+	
+	'status_favorites'	=> 'favourites',
+	'status_read'		=> 'read',
+	'status_unread'		=> 'unread',
+	'status_total'		=> 'total',
+	
+	'stats_entry_repartition'	=> 'Entry repartition',
+	'stats_entry_per_day'		=> 'Entry per day (last 30 days)',
+	'stats_feed_per_category'	=> 'Feed per category',
+	'stats_entry_per_category'	=> 'Entry per category',
 );
