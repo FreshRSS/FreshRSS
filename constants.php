3dd6677421c9ba776f4734a890b4e7241c2faafb
Alexandre Alapetite <alexandre@alapetite.fr>
2014-02-21 10:01:40 +0100

    CHANGELOG 0.7.1 + TMP_PATH 0.8

--- constants.php
+++ constants.php
@@ -16,5 +16,4 @@ define('FRESHRSS_PATH', dirname(__FILE__));
 	define('LIB_PATH', FRESHRSS_PATH . '/lib');
 		define('APP_PATH', FRESHRSS_PATH . '/app');
 
-//define('TMP_PATH', sys_get_temp_dir());  // need more tests so...
-define('TMP_PATH', DATA_PATH);  // ... we use DATA_PATH for the 0.7.1
+define('TMP_PATH', sys_get_temp_dir());
