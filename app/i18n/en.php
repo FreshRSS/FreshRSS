d289c5340f3b21f1561e57390e89b0b5c7881d17
Marien Fressinaud <dev@marienfressinaud.fr>
2014-08-08 21:30:23 +0200

    Add support of HTML5 notifications
    Show a notification if there are at least 1 new article to read.
    Support only window.Notification API.
    
    See https://github.com/marienfressinaud/FreshRSS/issues/399

--- app/i18n/en.php
+++ app/i18n/en.php
@@ -343,6 +343,8 @@ return array (
 	'login_required'		=> 'Login required:',
 
 	'confirm_action'		=> 'Are you sure you want to perform this action? It cannot be cancelled!',
+	'notif_title_new_articles'	=> 'FreshRSS: new articles!',
+	'notif_body_new_articles'	=> 'There are \d new articles to read on FreshRSS.',
 
 	// DATE
 	'january'			=> 'January',
