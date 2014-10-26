b05a5365216d5f57a73de67f5a448577e5c12248
Luc Didry <luc@didry.org>
2014-10-26 18:29:56 +0100

    Fixes #682
    I used http://iconizr.com/ (free software, MIT license) to create the
    sprites.

--- app/Models/Themes.php
+++ app/Models/Themes.php
@@ -113,7 +113,7 @@ class FreshRSS_Themes extends Minz_Model {
 			(self::$defaultIconsUrl . $url);
 
 		return $urlOnly ? Minz_Url::display($url) :
-			'<img class="icon" src="' . Minz_Url::display($url) . '" alt="' . $alts[$name] . '" />';
+			'<span class="icon icon-' . $name . ' icons-' . $name . '-dims"></span>';
 	}
 }
 
