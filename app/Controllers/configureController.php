FreshRSS/FreshRSS

commit ee5bbe48269a2fd5bc9c175fdb1e5a92a2c04502

Fix bug size with SQLite

app/Controllers/configureController.php
@@ -225,7 +225,7 @@ class FreshRSS_configure_Controller extends Minz_ActionController {
 
 		Minz_View::prependTitle(_t('archiving_configuration') . ' · ');
 
-		$entryDAO = FreshRSS_Factory::createEntryDao();
+		$entryDAO = FreshRSS_Factory::createEntryDao('freshrss');
 		$this->view->nb_total = $entryDAO->count();
 		$this->view->size_user = $entryDAO->size();
 

lib/Minz/ModelPdo.php
@@ -17,6 +17,7 @@ class Minz_ModelPdo {
 	private static $sharedBd = null;
 	private static $sharedPrefix;
 	private static $has_transaction = false;
+	private static $sharedCurrentUser;
 	protected static $sharedDbType;
 
 	/**
@@ -39,6 +40,7 @@ class Minz_ModelPdo {
 		if (self::$useSharedBd && self::$sharedBd != null && $currentUser === null) {
 			$this->bd = self::$sharedBd;
 			$this->prefix = self::$sharedPrefix;
+			$this->current_user = self::$sharedCurrentUser;
 			return;
 		}
 
@@ -48,6 +50,7 @@ class Minz_ModelPdo {
 			$currentUser = Minz_Session::param('currentUser', '_');
 		}
 		$this->current_user = $currentUser;
+		self::$sharedCurrentUser = $currentUser;
 
 		try {
 			$type = $db['type'];

commit 99763412c2f49e4e52b5a0343bb465141be76188

Fix bug on archiving page
Introduced by https://github.com/FreshRSS/FreshRSS/commit/ee5bbe48269a2fd5bc9c175fdb1e5a92a2c04502

app/Controllers/configureController.php
@@ -222,7 +222,7 @@ class FreshRSS_configure_Controller extends Minz_ActionController {
 
 		Minz_View::prependTitle(_t('archiving_configuration') . ' · ');
 
-		$entryDAO = FreshRSS_Factory::createEntryDao('freshrss');
+		$entryDAO = FreshRSS_Factory::createEntryDao();
 		$this->view->nb_total = $entryDAO->count();
 		$this->view->size_user = $entryDAO->size();
 