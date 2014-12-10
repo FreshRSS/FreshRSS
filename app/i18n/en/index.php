7dee863577aff2103277e08bf6db2f8e16b029a7
Marien Fressinaud <dev@marienfressinaud.fr>
2014-12-10 22:29:13 +0100

    Fix i18n for aside_feed

--- app/i18n/en/index.php
+++ app/i18n/en/index.php
@@ -1,5 +1,16 @@
 <?php
 
 return array(
-
+	'menu' => array(
+		'about' => 'About FreshRSS',
+		'actualize' => 'Actualize',
+		'favorites' => 'Favourites (%s)',
+		'filter' => 'Filter',
+		'main_stream' => 'Main stream',
+		'manage' => 'Manage',
+		'mark_read' => 'Mark as read',
+		'see_website' => 'See website',
+		'stats' => 'Statistics',
+		'subscription' => 'Subscriptions management',
+	),
 );
--- app/i18n/fr/index.php
+++ app/i18n/fr/index.php
@@ -1,5 +1,16 @@
 <?php
 
 return array(
-
+	'menu' => array(
+		'about' => 'À propos de FreshRSS',
+		'actualize' => 'Actualiser',
+		'favorites' => 'Favoris (%s)',
+		'filter' => 'Filtrer',
+		'main_stream' => 'Flux principal',
+		'manage' => 'Gérer',
+		'mark_read' => 'Marquer comme lu',
+		'see_website' => 'Voir le site',
+		'stats' => 'Statistiques',
+		'subscription' => 'Gestion des abonnements',
+	),
 );
