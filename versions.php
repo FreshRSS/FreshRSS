1e597ce4193fcc7891b268da8adba636aaf37a0c
Marien Fressinaud <dev@marienfressinaud.fr>
2015-07-30 15:45:21 +0200

    Update versions.php
    - reorganise versions
    - prepare update for 1.1.2-beta
    - prepare next 1.1.3-dev

--- versions.php
+++ versions.php
@@ -1,18 +1,23 @@
 <?php
 
 return array(
+	// STABLE
 	'0.8.0' => '1.0.0',
 	'0.8.1' => '1.0.0',
+	'1.0.0' => '1.0.1',  // doesn't exist (yet)
+	//BETA
 	'0.9.0' => '1.1.0',
 	'0.9.1' => '1.1.0',
 	'0.9.2' => '1.1.0',
 	'0.9.3' => '1.1.0',
 	'0.9.4' => '1.1.0',
-	'1.0.0' => '1.0.1',  // doesn't exist (yet)
-	'1.1.0' => '1.1.1',
-	'1.1.1' => '1.1.2',  // doesn't exist (yet)
-	'0.8-dev' => 'dev',
-	'0.10-dev' => 'dev',
-	'1.1.2-dev' => 'dev',  // appeared after 1.1.1 release
+	'1.1.0' => '1.1.2-beta',
+	'1.1.1' => '1.1.2-beta',
+	'1.1.2-beta' => '1.1.3-beta',  // doesn't exist (yet)
+	// DEV
+	'0.8-dev' => 'dev',  // TO REMOVE
+	'0.10-dev' => 'dev',  // TO REMOVE
+	'1.1.2-dev' => 'dev',
+	'1.1.3-dev' => 'dev',
 	'1.2-dev' => 'dev',
 );
