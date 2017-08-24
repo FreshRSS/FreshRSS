70eb8175f232dbea3e8e36b6164119e2fe602ca5
Frans de Jonge <fransdejonge@gmail.com>
2017-08-24 20:04:10 +0200

    cli/list-users line length

--- cli/list-users.php
+++ cli/list-users.php
@@ -4,7 +4,8 @@ require('_cli.php');
 
 $users = listUsers();
 sort($users);
-if (FreshRSS_Context::$system_conf->default_user !== '' && in_array(FreshRSS_Context::$system_conf->default_user, $users, true)) {
+if (FreshRSS_Context::$system_conf->default_user !== ''
+	&& in_array(FreshRSS_Context::$system_conf->default_user, $users, true)) {
 	array_unshift($users, FreshRSS_Context::$system_conf->default_user);
 	$users = array_unique($users);
 }
