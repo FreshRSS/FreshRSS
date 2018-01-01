58ca3516918ca29237c71fc4015bc8f7e5a425d6
Alexis Degrugillier <github@ainw.org>
2018-01-01 12:08:50 +0100

    Add shortcuts to switch views

--- app/Controllers/configureController.php
+++ app/Controllers/configureController.php
@@ -170,7 +170,7 @@ class FreshRSS_configure_Controller extends Minz_ActionController {
 		                   'm', 'n', 'o', 'p', 'page_down', 'page_up', 'q', 'r', 'return', 'right',
 		                   's', 'space', 't', 'tab', 'u', 'up', 'v', 'w', 'x', 'y',
 		                   'z', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
-		                   'f10', 'f11', 'f12');
+		                   'f10', 'f11', 'f12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
 		$this->view->list_keys = $list_keys;
 
 		if (Minz_Request::isPost()) {
--- p/scripts/main.js
+++ p/scripts/main.js
@@ -704,6 +704,29 @@ function init_shortcuts() {
 		'disable_in_input': true
 	});
 
+	shortcut.add(shortcuts.normal_view, function () {
+		$('#nav_menu_views .view_normal').get(0).click();
+	}, {
+		'disable_in_input': true
+	});
+
+	shortcut.add(shortcuts.global_view, function () {
+		$('#nav_menu_views .view_global').get(0).click();
+	}, {
+		'disable_in_input': true
+	});
+
+	shortcut.add(shortcuts.reading_view, function () {
+		$('#nav_menu_views .view_reader').get(0).click();
+	}, {
+		'disable_in_input': true
+	});
+
+	shortcut.add(shortcuts.rss_view, function () {
+		$('#nav_menu_views .view_rss').get(0).click();
+	}, {
+		'disable_in_input': true
+	});
 }
 
 function init_stream(divStream) {
