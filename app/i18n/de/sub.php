2cb5449591f2da795309eda80ca5a18cef1df2f7
Gregor Nathanael Meyer <Gregor@der-meyer.de>
2018-10-22 20:39:57 +0200

    German translation according to #2052 (#2068)

--- app/i18n/de/sub.php
+++ app/i18n/de/sub.php
@@ -27,7 +27,7 @@ return array(
 			'password' => 'HTTP-Passwort',
 			'username' => 'HTTP-Nutzername',
 		),
-		'clear_cache' => 'Always clear cache',	//TODO
+		'clear_cache' => 'Nicht cachen (für defekte Feeds)',
 		'css_help' => 'Ruft gekürzte RSS-Feeds ab (Achtung, benötigt mehr Zeit!)',
 		'css_path' => 'Pfad zur CSS-Datei des Artikels auf der Original-Webseite',
 		'description' => 'Beschreibung',
