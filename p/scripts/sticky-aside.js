966100725d21b0d67c9bb15e759f580416cc4225
Patrick Crandol <patrick@crandol.com>
2018-11-03 21:38:19 -0500

    Improve sticky-aside

--- p/scripts/sticky-aside.js
+++ p/scripts/sticky-aside.js
@@ -1,12 +1,19 @@
 window.onscroll = function() {addSticky()};
+$(document).ready(function() {recalc()});
+
 
 var sidebar = document.getElementById("sidebar");
 var sticky = sidebar.offsetTop;
 
 function addSticky() {
-  if (window.pageYOffset >= sticky) {
-    sidebar.classList.add("sticky")
-  } else {
-    sidebar.classList.remove("sticky");
-  }
+	if (window.pageYOffset >= sticky) {
+		sidebar.classList.add("sticky")
+	} else {
+		sidebar.classList.remove("sticky");
+	}
+}
+
+function recalc() {
+	$('#sidebar').width($('#sidebar').parent().width());
+	$('#sidebar').height($(window).height() - $('#sidebar').offset().top - $('#nav_entries').height());
 }
