912406ec20d585121d2b218d25142276d346469b
Patrick Crandol <patrick@crandol.com>
2018-11-04 00:18:04 -0500

    Updates to Sticky-Aside

--- p/scripts/sticky-aside.js
+++ p/scripts/sticky-aside.js
@@ -7,13 +7,15 @@ var sticky = sidebar.offsetTop;
 
 function addSticky() {
 	if (window.pageYOffset >= sticky) {
-		sidebar.classList.add("sticky")
+		sidebar.classList.add("sticky");
+		recalc();
 	} else {
 		sidebar.classList.remove("sticky");
+		recalc();
 	}
 }
 
 function recalc() {
 	$('#sidebar').width($('#sidebar').parent().width());
-	$('#sidebar').height($(window).height() - $('#sidebar').offset().top - $('#nav_entries').height());
+	$('#sidebar').height($(window).height() - $('#sidebar').getBoundingClientRect().top - $('#nav_entries').height());
 }
