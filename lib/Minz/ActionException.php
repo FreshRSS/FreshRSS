3d96a5b9f2cadcb284d316920dcae84dd41f5d1b
Alexandre Alapetite <alexandre@alapetite.fr>
2018-11-28 22:00:23 +0100

    Fix HTML injections
    Fix https://github.com/FreshRSS/FreshRSS/issues/2121
    Reported by https://www.netsparker.com

--- lib/Minz/ActionException.php
+++ lib/Minz/ActionException.php
@@ -1,9 +1,7 @@
 <?php
 class Minz_ActionException extends Minz_Exception {
 	public function __construct ($controller_name, $action_name, $code = self::ERROR) {
-		$message = '`' . $action_name . '` cannot be invoked on `'
-		         . $controller_name . '`';
-
+		$message = 'Invalid action name for controller ' . $controller_name;
 		parent::__construct ($message, $code);
 	}
 }
