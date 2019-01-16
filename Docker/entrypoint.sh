b27d7b237d31a4d56964f439aba7fe5b2c986036
Alexandre Alapetite <alexandre@alapetite.fr>
2019-01-16 22:11:15 +0100

    Docker Alpine PHP timezone
    https://github.com/FreshRSS/FreshRSS/issues/2153

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -5,6 +5,8 @@ php -f ./cli/prepare.php > /dev/null
 chown -R :www-data .
 chmod -R g+r . && chmod -R g+w ./data/
 
+find /etc/php*/ -name php.ini -exec sed -r -i "\#^;?date.timezone#s#^.*#date.timezone = $TZ#" {} \;
+
 if [ -n "$CRON_MIN" ]; then
 	sed -r -i "\#FreshRSS#s#^[^ ]+ #$CRON_MIN #" /var/spool/cron/crontabs/root
 fi
