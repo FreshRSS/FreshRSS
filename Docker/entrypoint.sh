9681500f62b8293e69a1d82ca4f29d49b7ee4364
Alexandre Alapetite <alexandre@alapetite.fr>
2019-04-02 21:46:11 +0200

    Docker fix cron logs
    Fix environment variable bug

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -8,6 +8,7 @@ chmod -R g+r . && chmod -R g+w ./data/
 find /etc/php*/ -name php.ini -exec sed -r -i "\#^;?date.timezone#s#^.*#date.timezone = $TZ#" {} \;
 
 if [ -n "$CRON_MIN" ]; then
+	(echo "export TZ=$TZ" ; echo "export COPY_SYSLOG_TO_STDERR=$COPY_SYSLOG_TO_STDERR") > /var/www/FreshRSS/Docker/env.txt
 	crontab -l | sed -r "\#FreshRSS#s#^[^ ]+ #$CRON_MIN #" | crontab -
 fi
 
