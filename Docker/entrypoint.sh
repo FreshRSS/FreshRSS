2daf567adb9134d2846c8054c3b55bdf63e12602
Alexandre Alapetite <alexandre@alapetite.fr>
2019-04-02 14:19:47 +0200

    Docker better use of crontab
    Misc. from https://github.com/FreshRSS/FreshRSS/pull/2325
    https://github.com/FreshRSS/FreshRSS/issues/2319

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -8,7 +8,7 @@ chmod -R g+r . && chmod -R g+w ./data/
 find /etc/php*/ -name php.ini -exec sed -r -i "\#^;?date.timezone#s#^.*#date.timezone = $TZ#" {} \;
 
 if [ -n "$CRON_MIN" ]; then
-	sed -r -i "\#FreshRSS#s#^[^ ]+ #$CRON_MIN #" /var/spool/cron/crontabs/root
+	crontab -l | sed -r "\#FreshRSS#s#^[^ ]+ #$CRON_MIN #" | crontab -
 fi
 
 exec "$@"
