021457ed0668997332ad79ce7ec3343bfc4e0398
Matt DeMoss <demoss.matt@gmail.com>
2019-06-04 00:59:09 -0400

    put limit back outside join

--- app/Models/EntryDAO.php
+++ app/Models/EntryDAO.php
@@ -906,8 +906,8 @@ class FreshRSS_EntryDAO extends Minz_ModelPdo implements FreshRSS_Searchable {
 				' : ' ' )
 			. ($type === 't' || $type === 'T' ? 'INNER JOIN `' . $this->prefix . 'entrytag` et ON et.id_entry = e.id ' : '')
 			. 'WHERE ' . $where
-			. $search
-			. ($order !== 'SHUF' ? 'ORDER BY e.id ' . $order : ' '));
+			. $search );
+			
 	}
 
 	public function listWhereRaw($type = 'a', $id = '', $state = FreshRSS_Entry::STATE_ALL, $order = 'DESC', $limit = 1, $firstId = '', $filters = null, $date_min = 0) {
@@ -930,6 +930,7 @@ class FreshRSS_EntryDAO extends Minz_ModelPdo implements FreshRSS_Searchable {
 			. ') e2 ON e2.id=e0.id '
 			. ($order === 'SHUF' ? 'ORDER BY shuffleOrderKey ' : 'ORDER BY e0.id ' . $order )
 			. ($limit > 0 ? ' LIMIT ' . intval($limit) : '');	//TODO: See http://explainextended.com/2009/10/23/mysql-order-by-limit-performance-late-row-lookups/
+			
 
 		$stm = $this->bd->prepare($sql);
 		if ($stm && $stm->execute($values)) {
