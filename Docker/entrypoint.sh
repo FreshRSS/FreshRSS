ad92518fe44279b69308aaf0a757469f5bf42910
Frans de Jonge <fransdejonge@gmail.com>
2019-07-22 16:07:40 +0200

    [CI] Run shellcheck and shfmt (#2454)
    * [CI] Run shellcheck and shfmt
    
    Cf. https://github.com/FreshRSS/FreshRSS/pull/2436#discussion_r305640019
    
    * rename
    
    * no need for disable anymore
    
    * also remove leftover indentation flags even if it makes no difference to syntax checking
    
    * define colors and reset before exit for local use

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-php -f ./cli/prepare.php > /dev/null
+php -f ./cli/prepare.php >/dev/null
 
 chown -R :www-data .
 chmod -R g+r . && chmod -R g+w ./data/
 
-find /etc/php*/ -name php.ini -exec sed -r -i "\#^;?date.timezone#s#^.*#date.timezone = $TZ#" {} \;
+find /etc/php*/ -name php.ini -exec sed -r -i "\\#^;?date.timezone#s#^.*#date.timezone = $TZ#" {} \;
 
 if [ -n "$CRON_MIN" ]; then
-	(echo "export TZ=$TZ" ; echo "export COPY_SYSLOG_TO_STDERR=$COPY_SYSLOG_TO_STDERR") > /var/www/FreshRSS/Docker/env.txt
-	crontab -l | sed -r "\#FreshRSS#s#^[^ ]+ #$CRON_MIN #" | crontab -
+	(
+		echo "export TZ=$TZ"
+		echo "export COPY_SYSLOG_TO_STDERR=$COPY_SYSLOG_TO_STDERR"
+	) >/var/www/FreshRSS/Docker/env.txt
+	crontab -l | sed -r "\\#FreshRSS#s#^[^ ]+ #$CRON_MIN #" | crontab -
 fi
 
 exec "$@"
