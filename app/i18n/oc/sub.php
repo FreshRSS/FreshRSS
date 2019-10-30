a8c64049cccfbb4121a6ea301753270f0b738fc8
Quentí <33203663+Quenty31@users.noreply.github.com>
2019-10-30 20:07:19 +0100

    Update sub.php

--- app/i18n/oc/sub.php
+++ app/i18n/oc/sub.php
@@ -74,7 +74,7 @@ return array(
 	),
 	'firefox' => array(
 		'documentation' => 'Seguissètz las etapas descrichas <a href="https://developer.mozilla.org/en-US/Firefox/Releases/2/Adding_feed_readers_to_Firefox#Adding_a_new_feed_reader_manually">aquí</a> per ajustar FreshRSS a la lista dels lectors de flux de Firefox.',
-		'obsolete_63' => 'From version 63 and onwards, Firefox has removed the ability to add your own subscription services that are not standalone programs.',	//TODO - Translation
+		'obsolete_63' => 'A partir de la version 63 e las seguentas, Firefox permet pas mai d’ajustar vòstres pròpris servicis d’abonament.',
 		'title' => 'Lector de flux de Firefox',
 	),
 	'import_export' => array(
