ed7bdfc8dbf1e83dce0bc7f04a38ca0548494b7e
Alexandre Alapetite <alexandre@alapetite.fr>
2019-11-21 15:40:31 +0100

    Update app/Controllers/feedController.php

--- app/Controllers/feedController.php
+++ app/Controllers/feedController.php
@@ -416,7 +416,7 @@ class FreshRSS_feed_Controller extends Minz_ActionController {
 			}
 
 			if ($pubSubHubbubEnabled && $feed->hubUrl() && $feed->selfUrl()) {	//selfUrl has priority for WebSub
-				if ($feed->selfUrl() !== $url) {	//https://code.google.com/p/pubsubhubbub/wiki/MovingFeedsOrChangingHubs
+				if ($feed->selfUrl() !== $url) {	// https://github.com/pubsubhubbub/PubSubHubbub/wiki/Moving-Feeds-or-changing-Hubs
 					$selfUrl = checkUrl($feed->selfUrl());
 					if ($selfUrl) {
 						Minz_Log::debug('WebSub unsubscribe ' . $feed->url(false));
