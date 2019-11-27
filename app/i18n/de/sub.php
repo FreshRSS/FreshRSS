7d9e66f71ca2c8f0b2c3340af0d4d026a667b81c
Sebastian Kroll <info@skroll.me>
2019-11-27 18:30:21 +0100

    more translations

--- app/i18n/de/sub.php
+++ app/i18n/de/sub.php
@@ -17,8 +17,8 @@ return array(
 		'empty' => 'Leere Kategorie',
 		'information' => 'Information',
 		'new' => 'Neue Kategorie',
-		'position' => 'Display position',	//TODO - Translation
-		'position_help' => 'To control category sort order',	//TODO - Translation
+		'position' => 'Reihenfolge',
+		'position_help' => 'Steuert die Kategoriesortierung',
 		'title' => 'Titel',
 	),
 	'feed' => array(
@@ -39,8 +39,8 @@ return array(
 		'empty' => 'Dieser Feed ist leer. Bitte stellen Sie sicher, dass er noch gepflegt wird.',
 		'error' => 'Dieser Feed ist auf ein Problem gestoßen. Bitte stellen Sie sicher, dass er immer lesbar ist und aktualisieren Sie ihn dann.',
 		'filteractions' => array(
-			'_' => 'Filter actions',	//TODO - Translation
-			'help' => 'Write one search filter per line.',	//TODO - Translation
+			'_' => 'Filteraktionen',
+			'help' => 'Ein Suchfilter pro Zeile',
 		),
 		'information' => 'Information',
 		'keep_min' => 'Minimale Anzahl an Artikeln, die behalten wird',
@@ -56,11 +56,11 @@ return array(
 		),
 		'websub' => 'Sofortbenachrichtigung mit WebSub',
 		'show' => array(
-			'all' => 'Show all feeds',	//TODO - Translation
-			'error' => 'Show only feeds with error',	//TODO - Translation
+			'all' => 'Alle Feeds zeigen',
+			'error' => 'Nur Feeds mit Fehlern zeigen',
 		),
 		'showing' => array(
-			'error' => 'Showing only feeds with error',	//TODO - Translation
+			'error' => 'Nur Feeds mit Fehlern zeigen',
 		),
 		'ssl_verify' => 'Überprüfe SSL Sicherheit',
 		'stats' => 'Statistiken',
@@ -75,14 +75,14 @@ return array(
 	),
 	'firefox' => array(
 		'documentation' => 'Folge den <a href="https://developer.mozilla.org/en-US/Firefox/Releases/2/Adding_feed_readers_to_Firefox#Adding_a_new_feed_reader_manually">hier</a> beschriebenen Schritten um FreshRSS zu Deiner Firefox RSS-Reader Liste hinzuzufügen.',
-		'obsolete_63' => 'From version 63 and onwards, Firefox has removed the ability to add your own subscription services that are not standalone programs.',	//TODO - Translation
+		'obsolete_63' => 'Seit Version 63 hat Firefox die Möglichkeit entfernt, Dienste hinzuzufügen, die keine eigenständigen Anwendungen sind.',
 		'title' => 'Firefox RSS-Reader',
 	),
 	'import_export' => array(
 		'export' => 'Exportieren',
 		'export_opml' => 'Liste der Feeds exportieren (OPML)',
 		'export_starred' => 'Ihre Favoriten exportieren',
-		'export_labelled' => 'Export your labelled articles',	//TODO
+		'export_labelled' => 'Artikel mit Labeln exportieren',
 		'feed_list' => 'Liste von %s Artikeln',
 		'file_to_import' => 'Zu importierende Datei<br />(OPML, JSON oder ZIP)',
 		'file_to_import_no_zip' => 'Zu importierende Datei<br />(OPML oder JSON)',
