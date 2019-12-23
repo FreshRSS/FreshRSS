FreshRSS/FreshRSS

commit b323ed084620cac2222fe1c93ec05b9773eb81e6

Improve authors (#2025)
* Links for authors and multiple authors

Favour ';' as a separator instead of ',' to better cope with
multi-author scientific articles.
Follow-up of https://github.com/FreshRSS/FreshRSS/pull/1997 ,
https://github.com/FreshRSS/FreshRSS/issues/1968,
https://github.com/FreshRSS/FreshRSS/pull/2023

* Change i18n authors

* Update layout

* Unicode-compatible search

Example for `author:Loïc`

* author <em> styling

* Final details

* Minor spacing

app/Models/Category.php
@@ -68,7 +68,7 @@ class FreshRSS_Category extends Minz_Model {
 		$this->id = $value;
 	}
 	public function _name($value) {
-		$this->name = mb_strcut(trim($value), 0, 255, 'UTF-8');
+		$this->name = trim($value);
 	}
 	public function _feeds($values) {
 		if (!is_array($values)) {

app/Models/Entry.php
@@ -10,7 +10,7 @@ class FreshRSS_Entry extends Minz_Model {
 	private $id = 0;
 	private $guid;
 	private $title;
-	private $author;
+	private $authors;
 	private $content;
 	private $link;
 	private $date;
@@ -21,17 +21,16 @@ class FreshRSS_Entry extends Minz_Model {
 	private $feed;
 	private $tags;
 
-	public function __construct($feedId = '', $guid = '', $title = '', $author = '', $content = '',
+	public function __construct($feedId = '', $guid = '', $title = '', $authors = '', $content = '',
 	                            $link = '', $pubdate = 0, $is_read = false, $is_favorite = false, $tags = '') {
 		$this->_title($title);
-		$this->_author($author);
+		$this->_authors($authors);
 		$this->_content($content);
 		$this->_link($link);
 		$this->_date($pubdate);
 		$this->_isRead($is_read);
 		$this->_isFavorite($is_favorite);
 		$this->_feedId($feedId);
-		$tags = mb_strcut($tags, 0, 1023, 'UTF-8');
 		$this->_tags($tags);
 		$this->_guid($guid);
 	}
@@ -45,8 +44,12 @@ class FreshRSS_Entry extends Minz_Model {
 	public function title() {
 		return $this->title;
 	}
-	public function author() {
-		return $this->author === null ? '' : $this->author;
+	public function authors($asString = false) {
+		if ($asString) {
+			return $this->authors == null ? '' : ';' . implode('; ', $this->authors);
+		} else {
+			return $this->authors;
+		}
 	}
 	public function content() {
 		return $this->content;
@@ -88,7 +91,7 @@ class FreshRSS_Entry extends Minz_Model {
 	}
 	public function tags($asString = false) {
 		if ($asString) {
-			return $this->tags == '' ? '' : '#' . implode(' #', $this->tags);
+			return $this->tags == null ? '' : '#' . implode(' #', $this->tags);
 		} else {
 			return $this->tags;
 		}
@@ -97,7 +100,7 @@ class FreshRSS_Entry extends Minz_Model {
 	public function hash() {
 		if ($this->hash === null) {
 			//Do not include $this->date because it may be automatically generated when lacking
-			$this->hash = md5($this->link . $this->title . $this->author . $this->content . $this->tags(true));
+			$this->hash = md5($this->link . $this->title . $this->authors(true) . $this->content . $this->tags(true));
 		}
 		return $this->hash;
 	}
@@ -124,11 +127,18 @@ class FreshRSS_Entry extends Minz_Model {
 	}
 	public function _title($value) {
 		$this->hash = null;
-		$this->title = mb_strcut($value, 0, 255, 'UTF-8');
+		$this->title = $value;
 	}
-	public function _author($value) {
+	public function _authors($value) {
 		$this->hash = null;
-		$this->author = mb_strcut($value, 0, 255, 'UTF-8');
+		if (!is_array($value)) {
+			if (strpos($value, ';') !== false) {
+				$value = preg_split('/\s*[;]\s*/', $value, -1, PREG_SPLIT_NO_EMPTY);
+			} else {
+				$value = preg_split('/\s*[,]\s*/', $value, -1, PREG_SPLIT_NO_EMPTY);
+			}
+		}
+		$this->authors = $value;
 	}
 	public function _content($value) {
 		$this->hash = null;
@@ -280,7 +290,7 @@ class FreshRSS_Entry extends Minz_Model {
 			'id' => $this->id(),
 			'guid' => $this->guid(),
 			'title' => $this->title(),
-			'author' => $this->author(),
+			'author' => $this->authors(true),
 			'content' => $this->content(),
 			'link' => $this->link(),
 			'date' => $this->date(true),

app/Models/Feed.php
@@ -420,7 +420,7 @@ class FreshRSS_Feed extends Minz_Model {
 			$author_names = '';
 			if (is_array($authors)) {
 				foreach ($authors as $author) {
-					$author_names .= html_only_entity_decode(strip_tags($author->name == '' ? $author->email : $author->name)) . ', ';
+					$author_names .= html_only_entity_decode(strip_tags($author->name == '' ? $author->email : $author->name)) . '; ';
 				}
 			}
 			$author_names = substr($author_names, 0, -2);

app/Models/Search.php
@@ -141,7 +141,7 @@ class FreshRSS_Search {
 			$this->intitle = $matches['search'];
 			$input = str_replace($matches[0], '', $input);
 		}
-		if (preg_match_all('/\bintitle:(?P<search>[\w+]*)/', $input, $matches)) {
+		if (preg_match_all('/\bintitle:(?P<search>[^\s"]*)/', $input, $matches)) {
 			$this->intitle = array_merge($this->intitle ? $this->intitle : array(), $matches['search']);
 			$input = str_replace($matches[0], '', $input);
 		}
@@ -155,7 +155,7 @@ class FreshRSS_Search {
 			$this->not_intitle = $matches['search'];
 			$input = str_replace($matches[0], '', $input);
 		}
-		if (preg_match_all('/[!-]intitle:(?P<search>[\w+]*)/', $input, $matches)) {
+		if (preg_match_all('/[!-]intitle:(?P<search>[^\s"]*)/', $input, $matches)) {
 			$this->not_intitle = array_merge($this->not_intitle ? $this->not_intitle : array(), $matches['search']);
 			$input = str_replace($matches[0], '', $input);
 		}
@@ -179,7 +179,7 @@ class FreshRSS_Search {
 			$this->author = $matches['search'];
 			$input = str_replace($matches[0], '', $input);
 		}
-		if (preg_match_all('/\bauthor:(?P<search>[\w+]*)/', $input, $matches)) {
+		if (preg_match_all('/\bauthor:(?P<search>[^\s"]*)/', $input, $matches)) {
 			$this->author = array_merge($this->author ? $this->author : array(), $matches['search']);
 			$input = str_replace($matches[0], '', $input);
 		}
@@ -193,7 +193,7 @@ class FreshRSS_Search {
 			$this->not_author = $matches['search'];
 			$input = str_replace($matches[0], '', $input);
 		}
-		if (preg_match_all('/[!-]author:(?P<search>[\w+]*)/', $input, $matches)) {
+		if (preg_match_all('/[!-]author:(?P<search>[^\s"]*)/', $input, $matches)) {
 			$this->not_author = array_merge($this->not_author ? $this->not_author : array(), $matches['search']);
 			$input = str_replace($matches[0], '', $input);
 		}

app/i18n/cz/gen.php
@@ -181,7 +181,7 @@ return array(
 	'short' => array(
 		'attention' => 'Upozornění!',
 		'blank_to_disable' => 'Zakázat - ponechte prázdné',
-		'by_author' => 'Od <em>%s</em>',
+		'by_author' => 'Od:',
 		'by_default' => 'Výchozí',
 		'damn' => 'Sakra!',
 		'default_category' => 'Nezařazeno',

app/i18n/de/gen.php
@@ -180,7 +180,7 @@ return array(
 	'short' => array(
 		'attention' => 'Achtung!',
 		'blank_to_disable' => 'Zum Deaktivieren frei lassen',
-		'by_author' => 'Von <em>%s</em>',
+		'by_author' => 'Von:',
 		'by_default' => 'standardmäßig',
 		'damn' => 'Verdammt!',
 		'default_category' => 'Unkategorisiert',

app/i18n/en/gen.php
@@ -181,7 +181,7 @@ return array(
 	'short' => array(
 		'attention' => 'Warning!',
 		'blank_to_disable' => 'Leave blank to disable',
-		'by_author' => 'By <em>%s</em>',
+		'by_author' => 'By:',
 		'by_default' => 'By default',
 		'damn' => 'Blast!',
 		'default_category' => 'Uncategorized',

app/i18n/es/gen.php
@@ -181,7 +181,7 @@ return array(
 	'short' => array(
 		'attention' => '¡Aviso!',
 		'blank_to_disable' => 'Deja en blanco para desactivar',
-		'by_author' => 'Por <em>%s</em>',
+		'by_author' => 'Por:',
 		'by_default' => 'Por defecto',
 		'damn' => '¡Córcholis!',
 		'default_category' => 'Sin categorizar',

commit 32b7d856fbf764ee2c8b6d2fb853a6e9e31da871

Fix failing tests
The category `_name` regression was introduced in commit b323ed084.

I wasn't able to understand when and why Search tests was wrong.

The rest is about upgrade of PHPUnit.

app/Models/Category.php
@@ -84,7 +84,7 @@ class FreshRSS_Category extends Minz_Model {
 		}
 	}
 	public function _name($value) {
-		$this->name = trim($value);
+		$this->name = mb_strcut(trim($value), 0, 255, 'UTF-8');
 	}
 	public function _isDefault($value) {
 		$this->isDefault = $value;

tests/app/Models/SearchTest.php
@@ -150,8 +150,8 @@ class SearchTest extends PHPUnit\Framework\TestCase {
 	public function provideDateSearch() {
 		return array(
 			array('date:2007-03-01T13:00:00Z/2008-05-11T15:30:00Z', '1172754000', '1210519800'),
-			array('date:2007-03-01T13:00:00Z/P1Y2M10DT2H30M', '1172754000', '1210516199'),
-			array('date:P1Y2M10DT2H30M/2008-05-11T15:30:00Z', '1172757601', '1210519800'),
+			array('date:2007-03-01T13:00:00Z/P1Y2M10DT2H30M', '1172754000', '1210519799'),
+			array('date:P1Y2M10DT2H30M/2008-05-11T15:30:00Z', '1172754001', '1210519800'),
 			array('date:2007-03-01/2008-05-11', strtotime('2007-03-01'), strtotime('2008-05-12') - 1),
 			array('date:2007-03-01/', strtotime('2007-03-01'), ''),
 			array('date:/2008-05-11', '', strtotime('2008-05-12') - 1),
@@ -176,8 +176,8 @@ class SearchTest extends PHPUnit\Framework\TestCase {
 	public function providePubdateSearch() {
 		return array(
 			array('pubdate:2007-03-01T13:00:00Z/2008-05-11T15:30:00Z', '1172754000', '1210519800'),
-			array('pubdate:2007-03-01T13:00:00Z/P1Y2M10DT2H30M', '1172754000', '1210516199'),
-			array('pubdate:P1Y2M10DT2H30M/2008-05-11T15:30:00Z', '1172757601', '1210519800'),
+			array('pubdate:2007-03-01T13:00:00Z/P1Y2M10DT2H30M', '1172754000', '1210519799'),
+			array('pubdate:P1Y2M10DT2H30M/2008-05-11T15:30:00Z', '1172754001', '1210519800'),
 			array('pubdate:2007-03-01/2008-05-11', strtotime('2007-03-01'), strtotime('2008-05-12') - 1),
 			array('pubdate:2007-03-01/', strtotime('2007-03-01'), ''),
 			array('pubdate:/2008-05-11', '', strtotime('2008-05-12') - 1),

tests/app/Models/UserQueryTest.php
@@ -19,24 +19,22 @@ class UserQueryTest extends PHPUnit\Framework\TestCase {
 		$this->assertEquals('favorite', $user_query->getGetType());
 	}
 
-	/**
-	 * @expectedException Exceptions/FreshRSS_DAO_Exception
-	 * @expectedExceptionMessage Category DAO is not loaded in UserQuery
-	 */
 	public function test__construct_whenCategoryQueryAndNoDao_throwsException() {
-		$this->markTestIncomplete('There is a problem with the exception autoloading. We need to make a better autoloading process');
+		$this->expectException(FreshRSS_DAO_Exception::class);
+		$this->expectExceptionMessage('Category DAO is not loaded in UserQuery');
+
 		$query = array('get' => 'c_1');
 		new FreshRSS_UserQuery($query);
 	}
 
 	public function test__construct_whenCategoryQuery_storesCategoryParameters() {
 		$category_name = 'some category name';
-		$cat = $this->getMock('FreshRSS_Category');
+		$cat = $this->createMock('FreshRSS_Category');
 		$cat->expects($this->atLeastOnce())
 			->method('name')
 			->withAnyParameters()
 			->willReturn($category_name);
-		$cat_dao = $this->getMock('FreshRSS_Searchable');
+		$cat_dao = $this->createMock('FreshRSS_Searchable');
 		$cat_dao->expects($this->atLeastOnce())
 			->method('searchById')
 			->withAnyParameters()
@@ -47,24 +45,22 @@ class UserQueryTest extends PHPUnit\Framework\TestCase {
 		$this->assertEquals('category', $user_query->getGetType());
 	}
 
-	/**
-	 * @expectedException Exceptions/FreshRSS_DAO_Exception
-	 * @expectedExceptionMessage Feed DAO is not loaded in UserQuery
-	 */
 	public function test__construct_whenFeedQueryAndNoDao_throwsException() {
-		$this->markTestIncomplete('There is a problem with the exception autoloading. We need to make a better autoloading process');
-		$query = array('get' => 'c_1');
+		$this->expectException(FreshRSS_DAO_Exception::class);
+		$this->expectExceptionMessage('Feed DAO is not loaded in UserQuery');
+
+		$query = array('get' => 'f_1');
 		new FreshRSS_UserQuery($query);
 	}
 
 	public function test__construct_whenFeedQuery_storesFeedParameters() {
 		$feed_name = 'some feed name';
-		$feed = $this->getMock('FreshRSS_Feed', array(), array('', false));
+		$feed = $this->createMock('FreshRSS_Feed', array(), array('', false));
 		$feed->expects($this->atLeastOnce())
 			->method('name')
 			->withAnyParameters()
 			->willReturn($feed_name);
-		$feed_dao = $this->getMock('FreshRSS_Searchable');
+		$feed_dao = $this->createMock('FreshRSS_Searchable');
 		$feed_dao->expects($this->atLeastOnce())
 			->method('searchById')
 			->withAnyParameters()
@@ -112,7 +108,7 @@ class UserQueryTest extends PHPUnit\Framework\TestCase {
 
 	public function testToArray_whenNoData_returnsEmptyArray() {
 		$user_query = new FreshRSS_UserQuery(array());
-		$this->assertInternalType('array', $user_query->toArray());
+		$this->assertIsIterable($user_query->toArray());
 		$this->assertCount(0, $user_query->toArray());
 	}
 
@@ -126,7 +122,7 @@ class UserQueryTest extends PHPUnit\Framework\TestCase {
 			'url' => 'some url',
 		);
 		$user_query = new FreshRSS_UserQuery($query);
-		$this->assertInternalType('array', $user_query->toArray());
+		$this->assertIsIterable($user_query->toArray());
 		$this->assertCount(6, $user_query->toArray());
 		$this->assertEquals($query, $user_query->toArray());
 	}
@@ -163,8 +159,8 @@ class UserQueryTest extends PHPUnit\Framework\TestCase {
 	}
 
 	public function testIsDeprecated_whenCategoryExists_returnFalse() {
-		$cat = $this->getMock('FreshRSS_Category');
-		$cat_dao = $this->getMock('FreshRSS_Searchable');
+		$cat = $this->createMock('FreshRSS_Category');
+		$cat_dao = $this->createMock('FreshRSS_Searchable');
 		$cat_dao->expects($this->atLeastOnce())
 			->method('searchById')
 			->withAnyParameters()
@@ -175,7 +171,7 @@ class UserQueryTest extends PHPUnit\Framework\TestCase {
 	}
 
 	public function testIsDeprecated_whenCategoryDoesNotExist_returnTrue() {
-		$cat_dao = $this->getMock('FreshRSS_Searchable');
+		$cat_dao = $this->createMock('FreshRSS_Searchable');
 		$cat_dao->expects($this->atLeastOnce())
 			->method('searchById')
 			->withAnyParameters()
@@ -186,8 +182,8 @@ class UserQueryTest extends PHPUnit\Framework\TestCase {
 	}
 
 	public function testIsDeprecated_whenFeedExists_returnFalse() {
-		$feed = $this->getMock('FreshRSS_Feed', array(), array('', false));
-		$feed_dao = $this->getMock('FreshRSS_Searchable');
+		$feed = $this->createMock('FreshRSS_Feed', array(), array('', false));
+		$feed_dao = $this->createMock('FreshRSS_Searchable');
 		$feed_dao->expects($this->atLeastOnce())
 			->method('searchById')
 			->withAnyParameters()
@@ -198,7 +194,7 @@ class UserQueryTest extends PHPUnit\Framework\TestCase {
 	}
 
 	public function testIsDeprecated_whenFeedDoesNotExist_returnTrue() {
-		$feed_dao = $this->getMock('FreshRSS_Searchable');
+		$feed_dao = $this->createMock('FreshRSS_Searchable');
 		$feed_dao->expects($this->atLeastOnce())
 			->method('searchById')
 			->withAnyParameters()