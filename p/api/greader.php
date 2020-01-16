9819a72293a57e6ed75c5bac18a44d7e16c9127f
Julien-Pierre Avérous <github@sourcemac.com>
2020-01-16 13:02:05 +0100

    Pass the entries to 'entry_before_display' extension hook (#2763)
    * Pass the entries to 'entry_before_display' extension hook. - #2762
    
    * Fix code-style.
    
    * - Fix 'Whitespace found at end of line'.
    
    Co-authored-by: Frans de Jonge <fransdejonge@gmail.com>

--- p/api/greader.php
+++ p/api/greader.php
@@ -481,7 +481,11 @@ function entriesToArray($entries) {
 	}
 
 	$items = array();
-	foreach ($entries as $entry) {
+	foreach ($entries as $item) {
+		$entry = Minz_ExtensionManager::callHook('entry_before_display', $item);
+		if ($entry == null) {
+			continue;
+		}
 		$f_id = $entry->feed();
 		if (isset($arrayFeedCategoryNames[$f_id])) {
 			$c_name = $arrayFeedCategoryNames[$f_id]['c_name'];
@@ -916,7 +920,12 @@ $user = authorizationToUser();
 FreshRSS_Context::$user_conf = null;
 if ($user !== '') {
 	FreshRSS_Context::$user_conf = get_user_configuration($user);
+	Minz_ExtensionManager::init();
 	Minz_Translate::init(FreshRSS_Context::$user_conf->language);
+
+	if (FreshRSS_Context::$user_conf != null) {
+		Minz_ExtensionManager::enableByList(FreshRSS_Context::$user_conf->extensions_enabled);
+	}
 } else {
 	Minz_Translate::init();
 }
