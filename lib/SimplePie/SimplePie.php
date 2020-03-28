84dae136054510dfaa79d38d2c2991e9928137ed
Alexandre Alapetite <alexandre@alapetite.fr>
2020-03-28 12:52:42 +0100

    Another line

--- lib/SimplePie/SimplePie.php
+++ lib/SimplePie/SimplePie.php
@@ -1772,7 +1772,8 @@ class SimplePie
 					{
 						trigger_error("$this->cache_location is not writable. Make sure you've set the correct relative or absolute path, and that the location is server-writable.", E_USER_WARNING);
 					}
-					$cache = $this->registry->call('Cache', 'get_handler', array($this->cache_location, call_user_func($this->cache_name_function, $file->url), 'spc'));
+					$url = $this->feed_url . ($this->force_feed ? '#force_feed' : '');	//FreshRSS
+					$cache = $this->registry->call('Cache', 'get_handler', array($this->cache_location, call_user_func($this->cache_name_function, $url), 'spc'));
 				}
 			}
 			$this->feed_url = $file->url;
