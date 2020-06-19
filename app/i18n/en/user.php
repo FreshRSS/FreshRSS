55a8895d536941470b55c3a4099202a898e5a9c7
Frans de Jonge <fransdejonge@gmail.com>
2020-06-19 23:25:41 +0200

    [i18n] Update en-UK grammar and style
    Taken from <https://github.com/FreshRSS/FreshRSS/pull/3060>

--- app/i18n/en/user.php
+++ app/i18n/en/user.php
@@ -3,19 +3,19 @@
 return array(
 	'email' => array(
 		'feedback' => array(
-			'invalid' => 'The email address is invalid.',
-			'required' => 'The email address is required.',
+			'invalid' => 'This email address is invalid.',
+			'required' => 'An email address is required.',
 		),
 		'validation' => array(
 			'change_email' => 'You can change your email address <a href="%s">on the profile page</a>.',
-			'email_sent_to' => 'We sent you an email at <strong>%s</strong>, please follow its indications to validate your address.',
+			'email_sent_to' => 'We sent you an email at <strong>%s</strong>, please follow its instructions to validate your address.',
 			'feedback' => array(
-				'email_failed' => 'We couldn’t send you an email because of a misconfiguration of the server.',
+				'email_failed' => 'We couldn’t send you an email because of a server configuration error.',
 				'email_sent' => 'An email has been sent to your address.',
-				'error' => 'The email address failed to be validated.',
-				'ok' => 'The email address has been validated.',
-				'unneccessary' => 'The email address was already validated.',
-				'wrong_token' => 'The email address failed to be validated due to a wrong token.',
+				'error' => 'This email address validation failed.',
+				'ok' => 'This email address has been validated.',
+				'unneccessary' => 'This email address was already validated.',
+				'wrong_token' => 'This email address failed to be validated due to a wrong token.',
 			),
 			'need_to' => 'You need to validate your email address before being able to use %s.',
 			'resend_email' => 'Resend the email',
@@ -24,7 +24,7 @@ return array(
 	),
 	'mailer' => array(
 		'email_need_validation' => array(
-			'body' => 'You’ve just registered on %s but you still need to validate your email. For that, just follow the link:',
+			'body' => 'You’ve just registered on %s, but you still need to validate your email address. For that, just follow the link:',
 			'title' => 'You need to validate your account',
 			'welcome' => 'Welcome %s,',
 		),
@@ -38,7 +38,7 @@ return array(
 		),
 	),
 	'username' => array(
-		'invalid' => 'The username is invalid.',
-		'taken' => 'The username %s is taken.',
+		'invalid' => 'This username is invalid.',
+		'taken' => 'This username, %s, is taken.',
 	),
 );
