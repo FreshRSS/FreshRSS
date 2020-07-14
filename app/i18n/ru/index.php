df0f671f3921bad5da6b0d44c64b2a72bee84c6c
Frans de Jonge <fransdejonge@gmail.com>
2020-07-14 11:16:22 +0200

    older than one week

--- app/i18n/ru/index.php
+++ app/i18n/ru/index.php
@@ -32,8 +32,8 @@ return array(
 	'menu' => array(
 		'about' => 'About FreshRSS',	// TODO - Translation
 		'add_query' => 'Add a query',	// TODO - Translation
-		'before_one_day' => 'Before one day',	// TODO - Translation
-		'before_one_week' => 'Before one week',	// TODO - Translation
+		'before_one_day' => 'Older than one day',	// TODO - Translation
+		'before_one_week' => 'Older than one week',	// TODO - Translation
 		'favorites' => 'Favourites (%s)',	// TODO - Translation
 		'global_view' => 'Global view',	// TODO - Translation
 		'main_stream' => 'Main stream',	// TODO - Translation
