8961c7beac40ee8b2d5623e136d57eb32dc910e9
rom-1 <37835829+rom-1@users.noreply.github.com>
2021-02-17 16:33:33 +0100

    Update DE Translation (#3455)
    * Update DE Translation
    
    * even better translations

--- app/i18n/de/sub.php
+++ app/i18n/de/sub.php
@@ -34,10 +34,10 @@ return array(
 		),
 		'clear_cache' => 'Nicht cachen (für defekte Feeds)',
 		'content_action' => array(
-			'_' => 'Content action when fetching the article content',	// TODO - Translation
-			'append' => 'Add after existing content',	// TODO - Translation
-			'prepend' => 'Add before existing content',	// TODO - Translation
-			'replace' => 'Replace existing content',	// TODO - Translation
+			'_' => 'Behandlung von Feed-Inhalt beim Herunterladen von Artikelinhalt',
+			'append' => 'Artikelinhalt nach Feed-Inhalt hinzufügen',
+			'prepend' => 'Artikelinhalt vor Feed-Inhalt hinzufügen',
+			'replace' => 'Artikelinhalt ersetzt Feed-Inhalt (Standard)',
 		),
 		'css_cookie' => 'Verwende Cookies beim Herunterladen des Feed-Inhalts mit CSS-Filtern',
 		'css_cookie_help' => 'Beispiel: <kbd>foo=bar; gdpr_consent=true; cookie=value</kbd>',
