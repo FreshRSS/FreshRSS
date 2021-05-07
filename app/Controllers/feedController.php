4bf2599d1edfdf1b9d207f756caa92c5961f11bb
Alexandre Alapetite <alexandre@alapetite.fr>
2021-05-07 11:42:33 +0200

    Make SSL options available to non-admins #fix https://github.com/FreshRSS/FreshRSS/issues/3611

--- app/Controllers/feedController.php
+++ app/Controllers/feedController.php
@@ -181,11 +181,9 @@ class FreshRSS_feed_Controller extends Minz_ActionController {
 				'timeout' => null,
 				'curl_params' => empty($opts) ? null : $opts,
 			);
-			if (FreshRSS_Auth::hasAccess('admin')) {
-				$attributes['ssl_verify'] = Minz_Request::paramTernary('ssl_verify');
-				$timeout = intval(Minz_Request::param('timeout', 0));
-				$attributes['timeout'] = $timeout > 0 ? $timeout : null;
-			}
+			$attributes['ssl_verify'] = Minz_Request::paramTernary('ssl_verify');
+			$timeout = intval(Minz_Request::param('timeout', 0));
+			$attributes['timeout'] = $timeout > 0 ? $timeout : null;
 
 			try {
 				$feed = self::addFeed($url, '', $cat, null, $http_auth, $attributes);
