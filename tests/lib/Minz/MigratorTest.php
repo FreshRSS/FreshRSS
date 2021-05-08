07c2db4b50da4462d4e9407d9d009ee9d452ed16
Alexandre Alapetite <alexandre@alapetite.fr>
2021-05-08 12:06:06 +0200

    Test details

--- tests/lib/Minz/MigratorTest.php
+++ tests/lib/Minz/MigratorTest.php
@@ -310,7 +310,7 @@ class MigratorTest extends TestCase
 	public function testExecuteFailsIfAMigrationIsFailing() {
 		$migrations_path = TESTS_PATH . '/fixtures/migrations_with_failing/';
 		$applied_migrations_path = tempnam('/tmp', 'applied_migrations.txt');
-		$expected_result = 'A migration failed to be applied, please see previous logs';
+		$expected_result = 'A migration failed to be applied, please see previous logs.';
 
 		$result = Minz_Migrator::execute($migrations_path, $applied_migrations_path);
 
