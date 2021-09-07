c88c39b061b8bdda8bd8a98fe76594868992f216
312k <55316963+312k@users.noreply.github.com>
2021-09-07 15:17:51 +0900

    finish transtlate index.php

--- app/i18n/ja/index.php
+++ app/i18n/ja/index.php
@@ -2,65 +2,65 @@
 
 return array(
 	'about' => array(
-		'_' => 'About',	// TODO - Translation
+		'_' => 'これについて',	// TODO - Translation
 		'agpl3' => '<a href="https://www.gnu.org/licenses/agpl-3.0.html">AGPL 3</a>',	// TODO - Translation
-		'bugs_reports' => 'Bug reports',	// TODO - Translation
+		'bugs_reports' => 'バグレポート',	// TODO - Translation
 		'credits' => 'Credits',	// TODO - Translation
 		'credits_content' => 'Some design elements come from <a href="http://twitter.github.io/bootstrap/">Bootstrap</a> although FreshRSS doesn’t use this framework. <a href="https://git.gnome.org/browse/gnome-icon-theme-symbolic">Icons</a> come from <a href="https://www.gnome.org/">GNOME project</a>. <em>Open Sans</em> font police has been created by <a href="https://fonts.google.com/specimen/Open+Sans">Steve Matteson</a>. FreshRSS is based on <a href="https://github.com/marienfressinaud/MINZ">Minz</a>, a PHP framework.',	// TODO - Translation
 		'freshrss_description' => 'FreshRSS is a RSS feeds aggregator to self-host like <a href="http://tontof.net/kriss/feed/">Kriss Feed</a> or <a href="https://github.com/LeedRSS/Leed">Leed</a>. It is light and easy to take in hand while being powerful and configurable tool.',	// TODO - Translation
 		'github' => '<a href="https://github.com/FreshRSS/FreshRSS/issues">on Github</a>',	// TODO - Translation
-		'license' => 'License',	// TODO - Translation
-		'project_website' => 'Project website',	// TODO - Translation
-		'title' => 'About',	// TODO - Translation
-		'version' => 'Version',	// TODO - Translation
+		'license' => 'ライセンス',	// TODO - Translation
+		'project_website' => 'プロジェクトのwebsite',	// TODO - Translation
+		'title' => 'これについて',	// TODO - Translation
+		'version' => 'バージョン',	// TODO - Translation
 		'website' => 'Website',	// TODO - Translation
 	),
 	'feed' => array(
-		'add' => 'Please add some feeds.',	// TODO - Translation
-		'empty' => 'There are no articles to show.',	// TODO - Translation
-		'rss_of' => 'RSS feed of %s',	// TODO - Translation
-		'title' => 'Main stream',	// TODO - Translation
-		'title_fav' => 'Favourites',	// TODO - Translation
-		'title_global' => 'Global view',	// TODO - Translation
+		'add' => 'フィードを追加してください。',	// TODO - Translation
+		'empty' => '表示できる記事がありません',	// TODO - Translation
+		'rss_of' => '%s のRSSフィード',	// TODO - Translation
+		'title' => 'メインストリーム',	// TODO - Translation
+		'title_fav' => 'お気に入り',	// TODO - Translation
+		'title_global' => 'グローバルビュー',	// TODO - Translation
 	),
 	'log' => array(
-		'_' => 'Logs',	// TODO - Translation
-		'clear' => 'Clear the logs',	// TODO - Translation
-		'empty' => 'Log file is empty',	// TODO - Translation
-		'title' => 'Logs',	// TODO - Translation
+		'_' => 'ログ',	// TODO - Translation
+		'clear' => 'ログを消去する',	// TODO - Translation
+		'empty' => 'ログファイルは空です',	// TODO - Translation
+		'title' => 'ログ',	// TODO - Translation
 	),
 	'menu' => array(
-		'about' => 'About FreshRSS',	// TODO - Translation
-		'before_one_day' => 'Older than one day',	// TODO - Translation
-		'before_one_week' => 'Older than one week',	// TODO - Translation
-		'bookmark_query' => 'Bookmark current query',	// TODO - Translation
-		'favorites' => 'Favourites (%s)',	// TODO - Translation
-		'global_view' => 'Global view',	// TODO - Translation
-		'main_stream' => 'Main stream',	// TODO - Translation
-		'mark_all_read' => 'Mark all as read',	// TODO - Translation
-		'mark_cat_read' => 'Mark category as read',	// TODO - Translation
-		'mark_feed_read' => 'Mark feed as read',	// TODO - Translation
-		'mark_selection_unread' => 'Mark selection as unread',	// TODO - Translation
-		'newer_first' => 'Newer first',	// TODO - Translation
-		'non-starred' => 'Show non-favourites',	// TODO - Translation
-		'normal_view' => 'Normal view',	// TODO - Translation
-		'older_first' => 'Oldest first',	// TODO - Translation
-		'queries' => 'User queries',	// TODO - Translation
-		'read' => 'Show read',	// TODO - Translation
-		'reader_view' => 'Reading view',	// TODO - Translation
-		'rss_view' => 'RSS feed',	// TODO - Translation
-		'search_short' => 'Search',	// TODO - Translation
-		'starred' => 'Show favourites',	// TODO - Translation
-		'stats' => 'Statistics',	// TODO - Translation
-		'subscription' => 'Subscription management',	// TODO - Translation
-		'tags' => 'My labels',	// TODO - Translation
-		'unread' => 'Show unread',	// TODO - Translation
+		'about' => 'FreshRSSについて',	// TODO - Translation
+		'before_one_day' => '一日以上前',	// TODO - Translation
+		'before_one_week' => '一週間以上前',	// TODO - Translation
+		'bookmark_query' => '現在のブックマーククエリ',	// TODO - Translation
+		'favorites' => 'お気に入り (%s)',	// TODO - Translation
+		'global_view' => 'グローバルビュー',	// TODO - Translation
+		'main_stream' => 'メインストリーム',	// TODO - Translation
+		'mark_all_read' => 'すべての記事に既読をつける',	// TODO - Translation
+		'mark_cat_read' => 'カテゴリーに既読をつける',	// TODO - Translation
+		'mark_feed_read' => 'フィードに既読をつける',	// TODO - Translation
+		'mark_selection_unread' => '選択したものに未読をつける',	// TODO - Translation
+		'newer_first' => '最新のものを先に',	// TODO - Translation
+		'non-starred' => 'お気に入りに登録されてないものを表示する',	// TODO - Translation
+		'normal_view' => 'ノーマルビュー',	// TODO - Translation
+		'older_first' => '最古のものを先に',	// TODO - Translation
+		'queries' => 'ユーザークエリ',	// TODO - Translation
+		'read' => '読み取りを表示する',	// TODO - Translation
+		'reader_view' => 'リーディングビュー',	// TODO - Translation
+		'rss_view' => 'RSSフィード',	// TODO - Translation
+		'search_short' => '検索',	// TODO - Translation
+		'starred' => 'お気に入りを表示する',	// TODO - Translation
+		'stats' => '統計',	// TODO - Translation
+		'subscription' => '購読したものの管理',	// TODO - Translation
+		'tags' => 'ラベル',	// TODO - Translation
+		'unread' => '未読のものを表示する',	// TODO - Translation
 	),
-	'share' => 'Share',	// TODO - Translation
+	'share' => '共有',	// TODO - Translation
 	'tag' => array(
-		'related' => 'Article tags',	// TODO - Translation
+		'related' => '記事のタグ',	// TODO - Translation
 	),
 	'tos' => array(
-		'title' => 'Terms of Service',	// TODO - Translation
+		'title' => '利用規約',	// TODO - Translation
 	),
 );
