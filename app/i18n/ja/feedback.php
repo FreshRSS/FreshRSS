2d2a2c67ab1c722be18359858449b998114c397d
312k <55316963+312k@users.noreply.github.com>
2021-09-07 14:40:09 +0900

    finish transtlate feedback.php

--- app/i18n/ja/feedback.php
+++ app/i18n/ja/feedback.php
@@ -2,138 +2,138 @@
 
 return array(
 	'access' => array(
-		'denied' => 'You don’t have permission to access this page',	// TODO - Translation
-		'not_found' => 'You are looking for a page that doesn’t exist',	// TODO - Translation
+		'denied' => 'あなたはこのページにアクセスする権限がありません。',
+		'not_found' => 'あなたが探しているページは存在しません。',
 	),
 	'admin' => array(
-		'optimization_complete' => 'Optimization complete',	// TODO - Translation
+		'optimization_complete' => '最適化が完了しました',
 	),
 	'api' => array(
 		'password' => array(
-			'failed' => 'Your password cannot be modified',	// TODO - Translation
-			'updated' => 'Your password has been modified',	// TODO - Translation
+			'failed' => 'あなたのパスワードは変更できません',
+			'updated' => 'あなたのパスワードは変更されました',
 		),
 	),
 	'auth' => array(
 		'form' => array(
-			'not_set' => 'A problem occurred during authentication system configuration. Please try again later.',	// TODO - Translation
-			'set' => 'Form is now your default authentication system.',	// TODO - Translation
+			'not_set' => '認証システムを設定している間問題が発生しました。もう一度お試しください。',
+			'set' => 'デフォルトの認証システムが設定されました。',
 		),
 		'login' => array(
-			'invalid' => 'Login is invalid',	// TODO - Translation
-			'success' => 'You are connected',	// TODO - Translation
+			'invalid' => 'ログインは無効です。',
+			'success' => 'あなたは接続されました。',
 		),
 		'logout' => array(
-			'success' => 'You are disconnected',	// TODO - Translation
+			'success' => 'あなたは切断されました。',
 		),
-		'no_password_set' => 'Administrator password hasn’t been set. This feature isn’t available.',	// TODO - Translation
+		'no_password_set' => '管理者のパスワードはまだ設定されていません。この機能は無効になっています。',
 	),
 	'conf' => array(
-		'error' => 'An error occurred while saving configuration',	// TODO - Translation
-		'query_created' => 'Query "%s" has been created.',	// TODO - Translation
-		'shortcuts_updated' => 'Shortcuts have been updated',	// TODO - Translation
-		'updated' => 'Configuration has been updated',	// TODO - Translation
+		'error' => '設定を保存するとき、エラーが発生しました。',
+		'query_created' => '"%s"クエリは作成されました。',
+		'shortcuts_updated' => 'ショートカットはアップデートされました。',
+		'updated' => '設定が更新されました。',
 	),
 	'extensions' => array(
-		'already_enabled' => '%s is already enabled',	// TODO - Translation
-		'cannot_remove' => '%s cannot be removed',	// TODO - Translation
+		'already_enabled' => '%sはすでに有効になっています',
+		'cannot_remove' => '%sは消去できません',
 		'disable' => array(
-			'ko' => '%s cannot be disabled. <a href="%s">Check FreshRSS logs</a> for details.',	// TODO - Translation
-			'ok' => '%s is now disabled',	// TODO - Translation
+			'ko' => '%sは表示できません。 <a href="%s">FreshRSS のログを確認してください</a> 詳細が表示されます。',
+			'ok' => '%sは無効にされています。',
 		),
 		'enable' => array(
-			'ko' => '%s cannot be enabled. <a href="%s">Check FreshRSS logs</a> for details.',	// TODO - Translation
-			'ok' => '%s is now enabled',	// TODO - Translation
+			'ko' => '%sは有効にできません。 <a href="%s">FreshRSS のログを確認してください</a> 詳細が表示されます。',
+			'ok' => '%sは有効にされています。',
 		),
-		'no_access' => 'You have no access on %s',	// TODO - Translation
-		'not_enabled' => '%s is not enabled',	// TODO - Translation
-		'not_found' => '%s does not exist',	// TODO - Translation
-		'removed' => '%s removed',	// TODO - Translation
+		'no_access' => 'あなたは%sにアクセスする権限がありません',
+		'not_enabled' => '%sは有効にされていません',
+		'not_found' => '%sは存在しません',
+		'removed' => '%sは消去されました',
 	),
 	'import_export' => array(
-		'export_no_zip_extension' => 'The ZIP extension is not present on your server. Please try to export files one by one.',	// TODO - Translation
-		'feeds_imported' => 'Your feeds have been imported and will now be updated',	// TODO - Translation
-		'feeds_imported_with_errors' => 'Your feeds have been imported, but some errors occurred',	// TODO - Translation
-		'file_cannot_be_uploaded' => 'File cannot be uploaded!',	// TODO - Translation
-		'no_zip_extension' => 'The ZIP extension is not present on your server.',	// TODO - Translation
-		'zip_error' => 'An error occurred during ZIP import.',	// TODO - Translation
+		'export_no_zip_extension' => 'ZIP 拡張は現在あなたのサーバーに存在しません。一つずつファイルをエクスポートしてみてください。',
+		'feeds_imported' => 'あなたのフィードはインポートされ、更新されます。',
+		'feeds_imported_with_errors' => 'あなたのフィードはインポートされましたが、エラーが起きました。',
+		'file_cannot_be_uploaded' => 'ファイルをアップロードすることはできません!',
+		'no_zip_extension' => 'ZIP拡張は現在あなたのサーバーに存在しません。',
+		'zip_error' => 'ZIPをインポートするときエラーが発生しました。',
 	),
 	'profile' => array(
-		'error' => 'Your profile cannot be modified',	// TODO - Translation
-		'updated' => 'Your profile has been modified',	// TODO - Translation
+		'error' => 'あなたのプロフィールを変更することはできません',
+		'updated' => 'あなたのプロフィールを変更されました',
 	),
 	'sub' => array(
-		'actualize' => 'Updating',	// TODO - Translation
+		'actualize' => '更新中',
 		'articles' => array(
-			'marked_read' => 'The selected articles have been marked as read.',	// TODO - Translation
-			'marked_unread' => 'The articles have been marked as unread.',	// TODO - Translation
+			'marked_read' => '選択された記事は既読として目印がつけられます。',
+			'marked_unread' => '選択された記事は未読として目印がつけられます。',
 		),
 		'category' => array(
-			'created' => 'Category %s has been created.',	// TODO - Translation
-			'deleted' => 'Category has been deleted.',	// TODO - Translation
-			'emptied' => 'Category has been emptied',	// TODO - Translation
-			'error' => 'Category cannot be updated',	// TODO - Translation
-			'name_exists' => 'Category name already exists.',	// TODO - Translation
-			'no_id' => 'You must specify the id of the category.',	// TODO - Translation
-			'no_name' => 'Category name cannot be empty.',	// TODO - Translation
-			'not_delete_default' => 'You cannot delete the default category!',	// TODO - Translation
-			'not_exist' => 'The category does not exist!',	// TODO - Translation
-			'over_max' => 'You have reached your limit of categories (%d)',	// TODO - Translation
-			'updated' => 'Category has been updated.',	// TODO - Translation
+			'created' => '%sカテゴリは作成されました',
+			'deleted' => 'カテゴリは消去されました',
+			'emptied' => 'カテゴリは空になりました',
+			'error' => 'カテゴリが更新することができません',
+			'name_exists' => 'そのカテゴリは既に存在します',
+			'no_id' => 'あなたはカテゴリのIDを指定する必要があります。',
+			'no_name' => 'カテゴリ名を空白にすることはできません!',
+			'not_delete_default' => 'デフォルトカテゴリを消去することはできません!',
+			'not_exist' => 'カテゴリは存在しません!',
+			'over_max' => 'カテゴリの上限に達しました(%d)',
+			'updated' => 'カテゴリは更新されました。',
 		),
 		'feed' => array(
-			'actualized' => '<em>%s</em> has been updated',	// TODO - Translation
-			'actualizeds' => 'RSS feeds have been updated',	// TODO - Translation
-			'added' => 'RSS feed <em>%s</em> has been added',	// TODO - Translation
-			'already_subscribed' => 'You have already subscribed to <em>%s</em>',	// TODO - Translation
-			'cache_cleared' => '<em>%s</em> cache has been cleared',	// TODO - Translation
-			'deleted' => 'Feed has been deleted',	// TODO - Translation
-			'error' => 'Feed cannot be updated',	// TODO - Translation
-			'internal_problem' => 'The newsfeed could not be added. <a href="%s">Check FreshRSS logs</a> for details. You can try force adding by appending <code>#force_feed</code> to the URL.',	// TODO - Translation
-			'invalid_url' => 'URL <em>%s</em> is invalid',	// TODO - Translation
-			'n_actualized' => '%d feeds have been updated',	// TODO - Translation
-			'n_entries_deleted' => '%d articles have been deleted',	// TODO - Translation
-			'no_refresh' => 'There are no feeds to refresh',	// TODO - Translation
-			'not_added' => '<em>%s</em> could not be added',	// TODO - Translation
-			'not_found' => 'Feed cannot be found',	// TODO - Translation
-			'over_max' => 'You have reached your limit of feeds (%d)',	// TODO - Translation
-			'reloaded' => '<em>%s</em> has been reloaded',	// TODO - Translation
+			'actualized' => '<em>%s</em>は更新されました。',
+			'actualizeds' => 'RSSフィードは更新されました。',
+			'added' => 'RSS feed <em>%s</em> has been added',
+			'already_subscribed' => 'すでにあなたは<em>%s</em>を購読しています',
+			'cache_cleared' => '<em>%s</em>キャッシュは作られました',
+			'deleted' => 'フィードは消去されました',
+			'error' => 'フィードを更新することができません',
+			'internal_problem' => 'newsfeedを追加することはできません。<a href="%s">FreshRSSログの詳細を</a>確かめてください。強制的に追加することを試せます <code>#force_feed</code>このURLを確認ください。',
+			'invalid_url' => 'URL <em>%s</em>は無効です',
+			'n_actualized' => '%d フィードはアップデートされました',
+			'n_entries_deleted' => '%d 記事が消去されました',
+			'no_refresh' => 'リフレッシュするフィードがありません',
+			'not_added' => '<em>%s</em> は追加することができません',
+			'not_found' => 'フィードを見つけることができませんでした',
+			'over_max' => 'フィードの最大値に達しました (%d)',
+			'reloaded' => '<em>%s</em> は再読み込みされました',
 			'selector_preview' => array(
-				'http_error' => 'Failed to load website content.',	// TODO - Translation
-				'no_entries' => 'There are no articles in this feed. You need at least one article to create a preview.',	// TODO - Translation
-				'no_feed' => 'Internal error (feed cannot be found).',	// TODO - Translation
-				'no_result' => 'The selector didn\'t match anything. As a fallback the original feed text will be displayed instead.',	// TODO - Translation
-				'selector_empty' => 'The selector is empty. You need to define one to create a preview.',	// TODO - Translation
+				'http_error' => 'websiteの読み込みに失敗しました',
+				'no_entries' => 'このフィードには記事がありません。少なくともプレビュー表示を作成するには一つの記事が必要です。',
+				'no_feed' => '内部エラー (フィードが見つかりませんでした).',
+				'no_result' => '選択されたものはどれともマッチしませんでした。代わりにフォールバックとして、元のテキストが表示されます。',
+				'selector_empty' => '選択されたものは空白です。プレビューするには一つ定義することが必要です。',
 			),
-			'updated' => 'Feed has been updated',	// TODO - Translation
+			'updated' => 'フィードは更新されました。',
 		),
-		'purge_completed' => 'Purge completed (%d articles deleted)',	// TODO - Translation
+		'purge_completed' => 'パージが完了されました (%d 記事は消去されました)',
 	),
 	'tag' => array(
-		'created' => 'Tag "%s" has been created.',	// TODO - Translation
-		'name_exists' => 'Tag name already exists.',	// TODO - Translation
-		'renamed' => 'Tag "%s" has been renamed to "%s".',	// TODO - Translation
+		'created' => '"%s" タグが作成されました',
+		'name_exists' => 'このタグ名は既に存在します',
+		'renamed' => '"%s"タグは"%s"に改名されました',
 	),
 	'update' => array(
-		'can_apply' => 'FreshRSS will now be updated to the <strong>version %s</strong>.',	// TODO - Translation
-		'error' => 'The update process has encountered an error: %s',	// TODO - Translation
-		'file_is_nok' => 'New <strong>version %s</strong> available, but check permissions on <em>%s</em> directory. HTTP server must have have write permission',	// TODO - Translation
-		'finished' => 'Update complete!',	// TODO - Translation
-		'none' => 'No update to apply',	// TODO - Translation
-		'server_not_found' => 'Update server cannot be found. [%s]',	// TODO - Translation
+		'can_apply' => 'FreshRSSは<strong>バージョン %s</strong>に更新されます。',
+		'error' => 'アップデートプロセスはエラーによって失敗しました: %s',
+		'file_is_nok' => '新規 <strong>バージョン %s</strong> があります、しかし <em>%s</em> ディレクトリには権限がありません。HTTP は書き込み権限が必要です。',
+		'finished' => 'アップデートが完了しました!',
+		'none' => '適用できるアップデートはありません',
+		'server_not_found' => 'アップデートサーバーが見つかりませんでした [%s]',
 	),
 	'user' => array(
 		'created' => array(
-			'_' => 'User %s has been created',	// TODO - Translation
-			'error' => 'User %s cannot be created',	// TODO - Translation
+			'_' => '%s ユーザーが作成されました',
+			'error' => '%s ユーザーの作成はできません',
 		),
 		'deleted' => array(
-			'_' => 'User %s has been deleted',	// TODO - Translation
-			'error' => 'User %s cannot be deleted',	// TODO - Translation
+			'_' => '%s ユーザーが消去されました',
+			'error' => '%s ユーザーの消去はできません',
 		),
 		'updated' => array(
-			'_' => 'User %s has been updated',	// TODO - Translation
-			'error' => 'User %s has not been updated',	// TODO - Translation
+			'_' => '%s ユーザーが更新されました',
+			'error' => '%s ユーザーの更新はできません',
 		),
 	),
 );
