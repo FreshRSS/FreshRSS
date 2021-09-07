2ef02b5149431f1744a65dd5530254ee6fa0b1a7
312k <55316963+312k@users.noreply.github.com>
2021-09-07 17:18:15 +0900

    finish transtlate sub.php

--- app/i18n/ja/sub.php
+++ app/i18n/ja/sub.php
@@ -1,136 +1,136 @@
 <?php
 
 return array(
-	'add' => 'Feed and category creation has been moved <a href=\'%s\'>here</a>. It is also accessible from the menu on the left and from the ✚ icon available on the main page.',	// TODO - Translation
+	'add' => 'フィードとカテゴリは移動されて <a href=\'%s\'>ここへ</a>。また左側のメニューや ✚ アイコンからフィードとカテゴリへ移動できます。',	// TODO - Translation
 	'api' => array(
-		'documentation' => 'Copy the following URL to use it within an external tool.',	// TODO - Translation
+		'documentation' => '外部ツール内で使うURLをコピーします。',	// TODO - Translation
 		'title' => 'API',	// TODO - Translation
 	),
 	'bookmarklet' => array(
-		'documentation' => 'Drag this button to your bookmarks toolbar or right-click it and choose "Bookmark This Link". Then click the "Subscribe" button in any page you want to subscribe to.',	// TODO - Translation
-		'label' => 'Subscribe',	// TODO - Translation
-		'title' => 'Bookmarklet',	// TODO - Translation
+		'documentation' => 'このボタンをブックマークツールバーへドラッグするか、右クリックして、「このリンクをブックマークする」を選択します。そうすることで「購読」ボタンがどのページでも購読できるようになります。',	// TODO - Translation
+		'label' => '購読',	// TODO - Translation
+		'title' => 'ブックマーク',	// TODO - Translation
 	),
 	'category' => array(
-		'_' => 'Category',	// TODO - Translation
-		'add' => 'Add a category',	// TODO - Translation
-		'archiving' => 'Archiving',	// TODO - Translation
-		'empty' => 'Empty category',	// TODO - Translation
-		'information' => 'Information',	// TODO - Translation
-		'position' => 'Display position',	// TODO - Translation
-		'position_help' => 'To control category sort order',	// TODO - Translation
-		'title' => 'Title',	// TODO - Translation
+		'_' => 'カテゴリ',	// TODO - Translation
+		'add' => 'カテゴリに追加する',	// TODO - Translation
+		'archiving' => 'アーカイブ',	// TODO - Translation
+		'empty' => 'からのカテゴリ',	// TODO - Translation
+		'information' => 'インフォメーション',	// TODO - Translation
+		'position' => '表示位置',	// TODO - Translation
+		'position_help' => 'カテゴリの表示順を操作する',	// TODO - Translation
+		'title' => 'タイトル',	// TODO - Translation
 	),
 	'feed' => array(
-		'add' => 'Add a RSS feed',	// TODO - Translation
-		'advanced' => 'Advanced',	// TODO - Translation
-		'archiving' => 'Archiving',	// TODO - Translation
+		'add' => 'RSSフィードに追加する',	// TODO - Translation
+		'advanced' => '応用',	// TODO - Translation
+		'archiving' => 'アーカイブ',	// TODO - Translation
 		'auth' => array(
-			'configuration' => 'Login',	// TODO - Translation
-			'help' => 'Allows access to HTTP protected RSS feeds',	// TODO - Translation
-			'http' => 'HTTP Authentication',	// TODO - Translation
-			'password' => 'HTTP password',	// TODO - Translation
-			'username' => 'HTTP username',	// TODO - Translation
+			'configuration' => 'ログイン',	// TODO - Translation
+			'help' => 'RSSフィードをHTTP経由でのアクセスを許可する',	// TODO - Translation
+			'http' => 'HTTP認証',	// TODO - Translation
+			'password' => 'HTTP パスワード',	// TODO - Translation
+			'username' => 'HTTP ユーザー名',	// TODO - Translation
 		),
-		'clear_cache' => 'Always clear cache',	// TODO - Translation
+		'clear_cache' => 'いつでもキャッシュをクリアする',	// TODO - Translation
 		'content_action' => array(
-			'_' => 'Content action when fetching the article content',	// TODO - Translation
-			'append' => 'Add after existing content',	// TODO - Translation
-			'prepend' => 'Add before existing content',	// TODO - Translation
-			'replace' => 'Replace existing content',	// TODO - Translation
+			'_' => '記事のコンテンツを読み出したとき、コンテンツアクションを実行する',	// TODO - Translation
+			'append' => '既に存在するコンテンツの後に追加する',	// TODO - Translation
+			'prepend' => '既に存在するコンテンツの前に追加する',	// TODO - Translation
+			'replace' => '既に存在するコンテンツを置換する',	// TODO - Translation
 		),
-		'css_cookie' => 'Use Cookies when fetching the article content',	// TODO - Translation
-		'css_cookie_help' => 'Example: <kbd>foo=bar; gdpr_consent=true; cookie=value</kbd>',	// TODO - Translation
-		'css_help' => 'Retrieves truncated RSS feeds (caution, requires more time!)',	// TODO - Translation
-		'css_path' => 'Article CSS selector on original website',	// TODO - Translation
-		'description' => 'Description',	// TODO - Translation
-		'empty' => 'This feed is empty. Please verify that it is still maintained.',	// TODO - Translation
-		'error' => 'This feed has encountered a problem. Please verify that it is always reachable then update it.',	// TODO - Translation
+		'css_cookie' => '記事のコンテンツを読み出したとき、クッキーを使用する',	// TODO - Translation
+		'css_cookie_help' => '例: <kbd>foo=bar; gdpr_consent=true; cookie=value</kbd>',	// TODO - Translation
+		'css_help' => 'あきらめられたRSSフィードを取得します (注意してください、より多くの時間が必要になるでしょう!)',	// TODO - Translation
+		'css_path' => '元のwebサイトの記事のCSS',	// TODO - Translation
+		'description' => '説明',	// TODO - Translation
+		'empty' => 'このフィードは空です。運営されているかどうかを確認してみてください。',	// TODO - Translation
+		'error' => 'このフィードに問題が発生しました。ここにいつでもアクセスできるかどうかを確認して更新してみてください。',	// TODO - Translation
 		'filteractions' => array(
-			'_' => 'Filter actions',	// TODO - Translation
-			'help' => 'Write one search filter per line.',	// TODO - Translation
+			'_' => 'フィルターアクション',	// TODO - Translation
+			'help' => '1行に1つの検索フィルターを設定してください',	// TODO - Translation
 		),
-		'information' => 'Information',	// TODO - Translation
-		'keep_min' => 'Minimum number of articles to keep',	// TODO - Translation
+		'information' => 'インフォメーション',	// TODO - Translation
+		'keep_min' => '最小数の記事は保持されます',	// TODO - Translation
 		'maintenance' => array(
-			'clear_cache' => 'Clear cache',	// TODO - Translation
-			'clear_cache_help' => 'Clear the cache for this feed.',	// TODO - Translation
-			'reload_articles' => 'Reload articles',	// TODO - Translation
-			'reload_articles_help' => 'Reload articles and fetch complete content if a selector is defined.',	// TODO - Translation
-			'title' => 'Maintenance',	// TODO - Translation
+			'clear_cache' => 'キャッシュのクリア',	// TODO - Translation
+			'clear_cache_help' => 'このフィードのキャッシュをクリアします。',	// TODO - Translation
+			'reload_articles' => '記事を再読み込みする',	// TODO - Translation
+			'reload_articles_help' => '記事を再読み込みして、セレクターが定義したコンテンツを完全に取得します。',	// TODO - Translation
+			'title' => 'メンテナンス',	// TODO - Translation
 		),
-		'moved_category_deleted' => 'When you delete a category, its feeds are automatically classified under <em>%s</em>.',	// TODO - Translation
-		'mute' => 'mute',	// TODO - Translation
-		'no_selected' => 'No feed selected.',	// TODO - Translation
-		'number_entries' => '%d articles',	// TODO - Translation
+		'moved_category_deleted' => 'カテゴリを削除したとき、フィードは自動的に<em>%s</em>下に分類されます。',	// TODO - Translation
+		'mute' => 'ミュート',	// TODO - Translation
+		'no_selected' => 'どのフィードも選択されていません',	// TODO - Translation
+		'number_entries' => '%d 記事数',	// TODO - Translation
 		'priority' => array(
-			'_' => 'Visibility',	// TODO - Translation
-			'archived' => 'Do not show (archived)',	// TODO - Translation
-			'main_stream' => 'Show in main stream',	// TODO - Translation
-			'normal' => 'Show in its category',	// TODO - Translation
+			'_' => '見ることのできる',	// TODO - Translation
+			'archived' => '非表示にする(アーカイブ)',	// TODO - Translation
+			'main_stream' => 'メインストリームで表示する',	// TODO - Translation
+			'normal' => 'カテゴリで表示する',	// TODO - Translation
 		),
-		'proxy' => 'Set a proxy for fetching this feed',	// TODO - Translation
-		'proxy_help' => 'Select a protocol (e.g: SOCKS5) and enter the proxy address (e.g: <kbd>127.0.0.1:1080</kbd>)',	// TODO - Translation
+		'proxy' => 'フィードを読み込むためにproxyを設定してください',	// TODO - Translation
+		'proxy_help' => 'プロトコルを選択し (例: SOCKS5) proxyアドレスを入力してください (例: <kbd>127.0.0.1:1080</kbd>)',	// TODO - Translation
 		'selector_preview' => array(
-			'show_raw' => 'Show source code',	// TODO - Translation
-			'show_rendered' => 'Show content',	// TODO - Translation
+			'show_raw' => 'ソースコードを表示する',	// TODO - Translation
+			'show_rendered' => 'コンテンツを表示する',	// TODO - Translation
 		),
 		'show' => array(
-			'all' => 'Show all feeds',	// TODO - Translation
-			'error' => 'Show only feeds with errors',	// TODO - Translation
+			'all' => 'すべてのフィードを表示する',	// TODO - Translation
+			'error' => 'エラーがあるフィードを表示する',	// TODO - Translation
 		),
 		'showing' => array(
-			'error' => 'Showing only feeds with errors',	// TODO - Translation
+			'error' => 'エラーがあるフィードを表示する',	// TODO - Translation
 		),
-		'ssl_verify' => 'Verify SSL security',	// TODO - Translation
-		'stats' => 'Statistics',	// TODO - Translation
-		'think_to_add' => 'You may add some feeds.',	// TODO - Translation
-		'timeout' => 'Timeout in seconds',	// TODO - Translation
-		'title' => 'Title',	// TODO - Translation
-		'title_add' => 'Add an RSS feed',	// TODO - Translation
-		'ttl' => 'Do not automatically refresh more often than',	// TODO - Translation
-		'url' => 'Feed URL',	// TODO - Translation
-		'useragent' => 'Set the user agent for fetching this feed',	// TODO - Translation
-		'useragent_help' => 'Example: <kbd>Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0)</kbd>',	// TODO - Translation
-		'validator' => 'Check the validity of the feed',	// TODO - Translation
+		'ssl_verify' => 'SSL セキュリティを確認してください',	// TODO - Translation
+		'stats' => '統計',	// TODO - Translation
+		'think_to_add' => 'あなたはフィードを追加できるでしょう。',	// TODO - Translation
+		'timeout' => '秒でのタイムアウト',	// TODO - Translation
+		'title' => 'タイトル',	// TODO - Translation
+		'title_add' => 'RSS フィードを追加する',	// TODO - Translation
+		'ttl' => '自動更新の頻度を上げすぎないでください',	// TODO - Translation
+		'url' => 'フィードのURL',	// TODO - Translation
+		'useragent' => 'フィードを読み込む際のユーザーエージェントを設定してください',	// TODO - Translation
+		'useragent_help' => '例: <kbd>Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0)</kbd>',	// TODO - Translation
+		'validator' => 'フィードが有効であるかどうかを確認してください',	// TODO - Translation
 		'website' => 'Website URL',	// TODO - Translation
 		'websub' => 'Instant notification with WebSub',	// TODO - Translation
 	),
 	'import_export' => array(
-		'export' => 'Export',	// TODO - Translation
-		'export_labelled' => 'Export your labelled articles',	// TODO - Translation
-		'export_opml' => 'Export list of feeds (OPML)',	// TODO - Translation
-		'export_starred' => 'Export your favourites',	// TODO - Translation
-		'feed_list' => 'List of %s articles',	// TODO - Translation
-		'file_to_import' => 'File to import<br />(OPML, JSON or ZIP)',	// TODO - Translation
-		'file_to_import_no_zip' => 'File to import<br />(OPML or JSON)',	// TODO - Translation
-		'import' => 'Import',	// TODO - Translation
-		'starred_list' => 'List of favourite articles',	// TODO - Translation
-		'title' => 'Import / export',	// TODO - Translation
+		'export' => 'エクスポート',	// TODO - Translation
+		'export_labelled' => 'ラベル付けされた記事をエクスポートする',	// TODO - Translation
+		'export_opml' => 'フィードリストをエクスポートする (OPML)',	// TODO - Translation
+		'export_starred' => 'お気に入りをエクスポートする',	// TODO - Translation
+		'feed_list' => '%s 記事のリスト',	// TODO - Translation
+		'file_to_import' => 'インポートするファイル<br />(OPML, JSON あるいは ZIP)',	// TODO - Translation
+		'file_to_import_no_zip' => 'インポートするファイル<br />(OPML あるいは JSON)',	// TODO - Translation
+		'import' => 'インポート',	// TODO - Translation
+		'starred_list' => 'お気に入りの記事',	// TODO - Translation
+		'title' => 'インポート / エクスポート',	// TODO - Translation
 	),
 	'menu' => array(
-		'add' => 'Add a feed or category',	// TODO - Translation
-		'add_feed' => 'Add a feed',	// TODO - Translation
-		'bookmark' => 'Subscribe (FreshRSS bookmark)',	// TODO - Translation
-		'import_export' => 'Import / export',	// TODO - Translation
-		'label_management' => 'Label management',	// TODO - Translation
-		'subscription_management' => 'Subscription management',	// TODO - Translation
-		'subscription_tools' => 'Subscription tools',	// TODO - Translation
+		'add' => 'フィード化カテゴリを追加します',	// TODO - Translation
+		'add_feed' => 'フィードの追加',	// TODO - Translation
+		'bookmark' => '購読 (FreshRSS ブックマーク)',	// TODO - Translation
+		'import_export' => 'インポート / エクスポート',	// TODO - Translation
+		'label_management' => 'ラベル管理',	// TODO - Translation
+		'subscription_management' => '購読管理',	// TODO - Translation
+		'subscription_tools' => '購読ツール',	// TODO - Translation
 	),
 	'tag' => array(
-		'name' => 'Name',	// TODO - Translation
-		'new_name' => 'New name',	// TODO - Translation
-		'old_name' => 'Old name',	// TODO - Translation
+		'name' => '名前',	// TODO - Translation
+		'new_name' => '新しい名前',	// TODO - Translation
+		'old_name' => '古い名前',	// TODO - Translation
 	),
 	'title' => array(
-		'_' => 'Subscription management',	// TODO - Translation
-		'add' => 'Add a feed or category',	// TODO - Translation
-		'add_category' => 'Add a category',	// TODO - Translation
-		'add_feed' => 'Add a feed',	// TODO - Translation
-		'add_label' => 'Add a label',	// TODO - Translation
-		'delete_label' => 'Delete a label',	// TODO - Translation
-		'feed_management' => 'RSS feeds management',	// TODO - Translation
-		'rename_label' => 'Rename a label',	// TODO - Translation
-		'subscription_tools' => 'Subscription tools',	// TODO - Translation
+		'_' => '購読管理',	// TODO - Translation
+		'add' => 'フィードあるいはカテゴリを追加します',	// TODO - Translation
+		'add_category' => 'カテゴリの追加',	// TODO - Translation
+		'add_feed' => 'フィードの追加',	// TODO - Translation
+		'add_label' => 'ラベルの追加',	// TODO - Translation
+		'delete_label' => 'ラベルの削除',	// TODO - Translation
+		'feed_management' => 'RSSフィードの管理',	// TODO - Translation
+		'rename_label' => 'ラベルの名前変更',	// TODO - Translation
+		'subscription_tools' => '購読ツール',	// TODO - Translation
 	),
 );
