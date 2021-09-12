cbaed68a88db476ac70e18cf7db60e2912a57624
mathContao <math-home@web.de>
2021-09-12 16:15:45 +0200

    switched from informal designation to formal

--- app/i18n/de/conf.php
+++ app/i18n/de/conf.php
@@ -60,7 +60,7 @@ return array(
 		'api' => 'API-Verwaltung',
 		'delete' => array(
 			'_' => 'Accountlöschung',
-			'warn' => 'Dein Account und alle damit bezogenen Daten werden gelöscht.',
+			'warn' => 'Ihr Account und alle damit bezogenen Daten werden gelöscht.',
 		),
 		'email' => 'E-Mail-Adresse',
 		'password_api' => 'Passwort-API<br /><small>(z.B. für mobile Anwendungen)</small>',
