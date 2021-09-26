2088cb43393bca8b62a00040c6acbfac9e3eaedc
mathContao <math-home@web.de>
2021-09-26 12:15:59 +0200

    added manget link for LibreJS

--- p/scripts/statsWithChartjs.js
+++ p/scripts/statsWithChartjs.js
@@ -1,4 +1,6 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 "use strict";
+/* jshint esversion:6, strict:global */
 
 function initCharts() {
 	if (!window.Chart) {
