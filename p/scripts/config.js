6195686210475ee22a6adeee87147f3e20d2f827
mathContao <math-home@web.de>
2021-10-26 19:25:54 +0200

    improve the JS

--- p/scripts/config.js
+++ p/scripts/config.js
@@ -5,12 +5,16 @@ function init_selectInputChanger() {
 	const select = document.getElementsByClassName('select-input-changer');
 
 	for (let i = 0; i < select.length; i++) {
-		select[i].addEventListener('click', function () { onchange_selectInputChanger(this); });
-		onchange_selectInputChanger(select[i]);
+		select[i].addEventListener('change', updateSelectInput);
+        select[i].dispatchEvent(new Event('change', {
+            bubbles: true,
+            cancelable: true,
+        }));
 	}
 }
 
-function onchange_selectInputChanger(elem) {
+function updateSelectInput(ev) {
+    const elem = ev.target;
 	const formGroup = document.getElementById(elem.dataset.name + '-block');
 	const input = document.getElementById(elem.dataset.name + '-input');
 	if (elem.selectedOptions[0].dataset.inputVisible == 'false') {
@@ -27,12 +31,16 @@ function onchange_selectInputChanger(elem) {
 function init_maxNumbersOfAccountsStatus() {
 	const input = document.getElementById('max-registrations-input');
 	if (input) {
-		input.addEventListener('click', function () { onchange_maxNumbersOfAccounts(this); });
-		onchange_maxNumbersOfAccounts(input);
+		input.addEventListener('change', onchange_maxNumbersOfAccounts);
+        input.dispatchEvent(new Event('change', {
+            bubbles: true,
+            cancelable: true,
+        }));
 	}
 }
 
-function onchange_maxNumbersOfAccounts(elem) {
+function onchange_maxNumbersOfAccounts(ev) {
+    const elem = ev.target;
 	if (elem.value > elem.dataset.number) {
 		document.getElementById('max-registrations-status-disabled').style.display = 'none';
 		document.getElementById('max-registrations-status-enabled').style.display = '';
