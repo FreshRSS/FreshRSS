ebf18b11c234a12a9500305c3fbf9e18533b2a6d
mathContao <math-home@web.de>
2021-10-26 09:49:37 +0200

    JS Syntax fixed for config.js

--- p/scripts/config.js
+++ p/scripts/config.js
@@ -2,43 +2,43 @@
 'use strict';
 
 function init_selectInputChanger() {
-	const select = document.getElementsByClassName("select-input-changer");
+	const select = document.getElementsByClassName('select-input-changer');
 
-	for (var i = 0; i < select.length; i++) {
-		select[i].addEventListener("click", function(){onchange_selectInputChanger(this)});
+	for (let i = 0; i < select.length; i++) {
+		select[i].addEventListener('click', function() { onchange_selectInputChanger(this); });
 		onchange_selectInputChanger(select[i]);
 	}
 }
 
-function onchange_selectInputChanger(elem){
-	const formGroup = document.getElementById(elem.dataset.name + "-block");
-	const input = document.getElementById(elem.dataset.name + "-input");
-	if (elem.selectedOptions[0].dataset.inputVisible == "false") {
+function onchange_selectInputChanger(elem) {
+	const formGroup = document.getElementById(elem.dataset.name + '-block');
+	const input = document.getElementById(elem.dataset.name + '-input');
+	if (elem.selectedOptions[0].dataset.inputVisible == 'false') {
 		formGroup.style.display = 'none';
-		input.name = "";
+		input.name = '';
 		elem.name = elem.dataset.name;
 	} else {
 		formGroup.style.display = '';
 		input.name = elem.dataset.name;
-		elem.name = "";
+		elem.name = '';
 	}
 }
 
 function init_maxNumbersOfAccountsStatus() {
-	const input = document.getElementById("max-registrations-input");
+	const input = document.getElementById('max-registrations-input');
 	if (input) {
-		input.addEventListener("click", function(){onchange_maxNumbersOfAccounts(this)});
+		input.addEventListener('click', function() { onchange_maxNumbersOfAccounts(this); });
 		onchange_maxNumbersOfAccounts(input);
 	}
 }
 
 function onchange_maxNumbersOfAccounts(elem) {
 	if (elem.value > elem.dataset.number) {
-		document.getElementById("max-registrations-status-disabled").style.display = "none";
-		document.getElementById("max-registrations-status-enabled").style.display = "";
+		document.getElementById('max-registrations-status-disabled').style.display = 'none';
+		document.getElementById('max-registrations-status-enabled').style.display = '';
 	} else {
-		document.getElementById("max-registrations-status-disabled").style.display = "";
-		document.getElementById("max-registrations-status-enabled").style.display = "none";
+		document.getElementById('max-registrations-status-disabled').style.display = '';
+		document.getElementById('max-registrations-status-enabled').style.display = 'none';
 	}
 }
 
