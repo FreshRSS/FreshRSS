19c23151f1fd2d9b032e1f94f82c150b9ca9ad7d
Alexandre Alapetite <alexandre@alapetite.fr>
2022-01-09 01:12:12 +0100

    Add spell checking with typos Implement https://github.com/FreshRSS/FreshRSS/pull/4134#issuecomment-1008027558

--- app/Controllers/statsController.php
+++ app/Controllers/statsController.php
@@ -23,25 +23,25 @@ class FreshRSS_stats_Controller extends FreshRSS_ActionController {
 		FreshRSS_View::prependTitle(_t('admin.stats.title') . ' · ');
 	}
 
-	private function convertToSerie($data) {
-		$serie = array();
+	private function convertToSeries($data) {
+		$series = array();
 
 		foreach ($data as $key => $value) {
-			$serie[] = array($key, $value);
+			$series[] = array($key, $value);
 		}
 
-		return $serie;
+		return $series;
 	}
 
-	private function convertToPieSerie($data) {
-		$serie = array();
+	private function convertToPieSeries($data) {
+		$series = array();
 
 		foreach ($data as $value) {
 			$value['data'] = array(array(0, (int) $value['data']));
-			$serie[] = $value;
+			$series[] = $value;
 		}
 
-		return $serie;
+		return $series;
 	}
 
 	/**
