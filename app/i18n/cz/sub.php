d879e080448ff17c4d588cd6af95c691d77a7ac6
marapavelka <53287236+marapavelka@users.noreply.github.com>
2022-01-15 22:39:26 +0100

    Update sub.php
    Corrected errors and typos, unified terminology, added missing translations.

--- app/i18n/cz/sub.php
+++ app/i18n/cz/sub.php
@@ -12,137 +12,137 @@
 
 return array(
 	'api' => array(
-		'documentation' => 'Copy the following URL to use it within an external tool.',	// TODO
-		'title' => 'API',	// TODO
+		'documentation' => 'Zkopírujte následující adresu URL pro její použití v externím nástroji.',
+		'title' => 'API',	// IGNORE
 	),
 	'bookmarklet' => array(
-		'documentation' => 'Drag this button to your bookmarks toolbar or right-click it and choose "Bookmark This Link". Then click the "Subscribe" button in any page you want to subscribe to.',	// TODO
-		'label' => 'Subscribe',	// TODO
-		'title' => 'Bookmarklet',	// TODO
+		'documentation' => 'Přetáhněte toto tlačítko do svého panelu nástrojů záložek nebo na něj klikněte pravým tlačítkem myši a zvolte „Uložit tento odkaz do záložek“. Pak klikněte na tlačítko „Přihlásit se k odběru“ na kterékoliv stránce, kde se chcete přihlásit k odběru.',
+		'label' => 'Přihlásit se k odběru',
+		'title' => 'Záložkový aplet',
 	),
 	'category' => array(
 		'_' => 'Kategorie',
-		'add' => 'Přidat kategorie',
+		'add' => 'Přidat kategorii',
 		'archiving' => 'Archivace',
 		'empty' => 'Vyprázdit kategorii',
 		'information' => 'Informace',
-		'position' => 'Display position',	// TODO
-		'position_help' => 'To control category sort order',	// TODO
+		'position' => 'Zobrazit pozici',
+		'position_help' => 'Pro ovládání pořadí řazení kategorií',
 		'title' => 'Název',
 	),
 	'feed' => array(
-		'add' => 'Přidat RSS kanál',
-		'advanced' => 'Pokročilé',
+		'add' => 'Přidat kanál RSS',
+		'advanced' => 'Rozšířené',
 		'archiving' => 'Archivace',
 		'auth' => array(
 			'configuration' => 'Přihlášení',
-			'help' => 'Umožní přístup k RSS kanálům chráneným HTTP autentizací',
-			'http' => 'HTTP přihlášení',
-			'password' => 'Heslo',
-			'username' => 'Přihlašovací jméno',
+			'help' => 'Umožní přístup ke kanálům RSS chráněným HTTP',
+			'http' => 'HTTP ověřování',
+			'password' => 'HTTP heslo',
+			'username' => 'HTTP uživatelské jméno',
 		),
-		'clear_cache' => 'Always clear cache',	// TODO
+		'clear_cache' => 'Vždy vymazat mezipaměť',
 		'content_action' => array(
-			'_' => 'Content action when fetching the article content',	// TODO
-			'append' => 'Add after existing content',	// TODO
-			'prepend' => 'Add before existing content',	// TODO
-			'replace' => 'Replace existing content',	// TODO
+			'_' => 'Akce obsahu při načítání obsahu článku',
+			'append' => 'Přidat za existující obsah',
+			'prepend' => 'Přidat před existující obsah',
+			'replace' => 'Nahradit existující obsah',
 		),
-		'css_cookie' => 'Use Cookies when fetching the article content',	// TODO
-		'css_cookie_help' => 'Example: <kbd>foo=bar; gdpr_consent=true; cookie=value</kbd>',	// TODO
-		'css_help' => 'Stáhne zkrácenou verzi RSS kanálů (pozor, náročnější na čas!)',
-		'css_path' => 'Původní CSS soubor článku z webových stránek',
+		'css_cookie' => 'Použít cookies při načítání obsahu článku',
+		'css_cookie_help' => 'Příklad: <kbd>foo=bar; gdpr_consent=true; cookie=value</kbd>',
+		'css_help' => 'Načte oříznuté kanály RSS (pozor, náročnější na čas!)',
+		'css_path' => 'Přepínač CSS článku na původních webových stránkách',
 		'description' => 'Popis',
-		'empty' => 'Kanál je prázdný. Ověřte prosím zda je ještě autorem udržován.',
-		'error' => 'Vyskytl se problém s kanálem. Ověřte že je vždy dostupný, prosím, a poté jej aktualizujte.',
+		'empty' => 'Tento kanál je prázdný. Ověřte prosím, zda je stále udržován.',
+		'error' => 'Vyskytl se problém s kanálem. Ověřte prosím, že je vždy dostupný, pak ho aktualizujte.',
 		'filteractions' => array(
-			'_' => 'Filter actions',	// TODO
-			'help' => 'Write one search filter per line.',	// TODO
+			'_' => 'Akce filtrování',
+			'help' => 'Zapište jeden filtr hledání na řádek.',
 		),
 		'information' => 'Informace',
-		'keep_min' => 'Zachovat tento minimální počet článků',
+		'keep_min' => 'Minimální počet článků pro ponechání',
 		'maintenance' => array(
-			'clear_cache' => 'Clear cache',	// TODO
-			'clear_cache_help' => 'Clear the cache for this feed.',	// TODO
-			'reload_articles' => 'Reload articles',	// TODO
-			'reload_articles_help' => 'Reload articles and fetch complete content if a selector is defined.',	// TODO
-			'title' => 'Maintenance',	// TODO
+			'clear_cache' => 'Vymazat mezipaměť',
+			'clear_cache_help' => 'Vymazat mezipaměť pro tento kanál.',
+			'reload_articles' => 'Znovu načíst články',
+			'reload_articles_help' => 'Znovu načíst články a získat úplný obsah, pokud je definován přepínač.',
+			'title' => 'Údržba',
 		),
-		'moved_category_deleted' => 'Po smazání kategorie budou v ní obsažené kanály automaticky přesunuty do <em>%s</em>.',
-		'mute' => 'mute',	// TODO
-		'no_selected' => 'Nejsou označeny žádné kanály.',
+		'moved_category_deleted' => 'Když odstraníte kategorii, její kanály jsou automaticky přesunuty do <em>%s</em>.',
+		'mute' => 'ztlumit',
+		'no_selected' => 'Nejsou vybrány žádné kanály.',
 		'number_entries' => '%d článků',
 		'priority' => array(
-			'_' => 'Visibility',	// TODO
-			'archived' => 'Do not show (archived)',	// TODO
-			'main_stream' => 'Zobrazit ve “Všechny kanály”',
-			'normal' => 'Show in its category',	// TODO
+			'_' => 'Viditelnost',
+			'archived' => 'Nezobrazovat (archivováno)',
+			'main_stream' => 'Zobrazit ve hlavním kanálu',
+			'normal' => 'Zobrazit v jeho kategorii',
 		),
-		'proxy' => 'Set a proxy for fetching this feed',	// TODO
-		'proxy_help' => 'Select a protocol (e.g: SOCKS5) and enter the proxy address (e.g: <kbd>127.0.0.1:1080</kbd>)',	// TODO
+		'proxy' => 'Nastavete proxy pro načítání tohoto kanálu',
+		'proxy_help' => 'Vyberte protokol (např.: SOCKS5) a zadejte adresu proxy (např.: <kbd>127.0.0.1:1080</kbd>)',
 		'selector_preview' => array(
-			'show_raw' => 'Show source code',	// TODO
-			'show_rendered' => 'Show content',	// TODO
+			'show_raw' => 'Zobrazit zdrojový kód',
+			'show_rendered' => 'Zobrazit obsah',
 		),
 		'show' => array(
-			'all' => 'Show all feeds',	// TODO
-			'error' => 'Show only feeds with errors',	// TODO
+			'all' => 'Zobrazit všechny kanály',
+			'error' => 'Zobrazit pouze kanály s chybami',
 		),
 		'showing' => array(
-			'error' => 'Showing only feeds with errors',	// TODO
+			'error' => 'Zobrazení pouze kanálů s chybami',
 		),
-		'ssl_verify' => 'Verify SSL security',	// TODO
+		'ssl_verify' => 'Ověřit zabezpečení SSL',
 		'stats' => 'Statistika',
-		'think_to_add' => 'Můžete přidat kanály.',
-		'timeout' => 'Timeout in seconds',	// TODO
+		'think_to_add' => 'Můžete přidat nějaké kanály.',
+		'timeout' => 'Časový limit v sekundách',
 		'title' => 'Název',
-		'title_add' => 'Přidat RSS kanál',
-		'ttl' => 'Neobnovovat častěji než',
-		'url' => 'URL kanálu',
-		'useragent' => 'Set the user agent for fetching this feed',	// TODO
-		'useragent_help' => 'Example: <kbd>Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0)</kbd>',	// TODO
+		'title_add' => 'Přidat kanál RSS',
+		'ttl' => 'Neobnovovat automaticky častěji než',
+		'url' => 'Adresa URL kanálu',
+		'useragent' => 'Nastavte uživatelský agent pro načítání tohoto kanálu',
+		'useragent_help' => 'Příklad: <kbd>Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0)</kbd>',
 		'validator' => 'Zkontrolovat platnost kanálu',
-		'website' => 'URL webové stránky',
+		'website' => 'Adresa URL webové stránky',
 		'websub' => 'Okamžité oznámení s WebSub',
 	),
 	'import_export' => array(
-		'export' => 'Export',	// TODO
-		'export_labelled' => 'Export your labelled articles',	// TODO
+		'export' => 'Exportovat',
+		'export_labelled' => 'Exportovat články s vašimi popisky',
 		'export_opml' => 'Exportovat seznam kanálů (OPML)',
-		'export_starred' => 'Exportovat oblíbené',
+		'export_starred' => 'Exportovat vaše oblíbené',
 		'feed_list' => 'Seznam %s článků',
 		'file_to_import' => 'Soubor k importu<br />(OPML, JSON nebo ZIP)',
 		'file_to_import_no_zip' => 'Soubor k importu<br />(OPML nebo JSON)',
-		'import' => 'Import',	// TODO
+		'import' => 'Importovat',
 		'starred_list' => 'Seznam oblíbených článků',
-		'title' => 'Import / export',	// TODO
+		'title' => 'Importovat / exportovat',
 	),
 	'menu' => array(
-		'add' => 'Add a feed or category',	// TODO
-		'import_export' => 'Import / export',	// TODO
-		'label_management' => 'Label management',	// TODO
+		'add' => 'Přidat kanál nebo kategorii',
+		'import_export' => 'Importovat / exportovat',
+		'label_management' => 'Správa popisků',
 		'stats' => array(
-			'idle' => 'Neaktivní kanály',
-			'main' => 'Přehled',
-			'repartition' => 'Rozdělení článků',
+			'idle' => 'Nečinné kanály',
+			'main' => 'Hlavní statistika',
+			'repartition' => 'Přerozdělení článků',
 		),
-		'subscription_management' => 'Správa subskripcí',
-		'subscription_tools' => 'Subscription tools',	// TODO
+		'subscription_management' => 'Správa odběrů',
+		'subscription_tools' => 'Nástroje odběrů',
 	),
 	'tag' => array(
-		'name' => 'Name',	// TODO
-		'new_name' => 'New name',	// TODO
-		'old_name' => 'Old name',	// TODO
+		'name' => 'Název',
+		'new_name' => 'Nový název',
+		'old_name' => 'Starý název',
 	),
 	'title' => array(
-		'_' => 'Správa subskripcí',
-		'add' => 'Add a feed or category',	// TODO
-		'add_category' => 'Add a category',	// TODO
-		'add_feed' => 'Add a feed',	// TODO
-		'add_label' => 'Add a label',	// TODO
-		'delete_label' => 'Delete a label',	// TODO
-		'feed_management' => 'Správa RSS kanálů',
-		'rename_label' => 'Rename a label',	// TODO
-		'subscription_tools' => 'Subscription tools',	// TODO
+		'_' => 'Správa odběrů',
+		'add' => 'Přidat kanál nebo kategorii',
+		'add_category' => 'Přidat kategorii',
+		'add_feed' => 'Přidat kanál',
+		'add_label' => 'Přidat popisek',
+		'delete_label' => 'Odstranit popisek',
+		'feed_management' => 'Správa kanálů RSS',
+		'rename_label' => 'Přejmenovat popisek',
+		'subscription_tools' => 'Nástroje odběrů',
 	),
 );
