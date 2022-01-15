bccae02c80b9756c4bfae1861940f17d28a4bcd8
marapavelka <53287236+marapavelka@users.noreply.github.com>
2022-01-15 13:39:51 +0100

    Update admin.php
    Editing and completing the translation

--- app/i18n/cz/admin.php
+++ app/i18n/cz/admin.php
@@ -12,21 +12,21 @@
 
 return array(
 	'auth' => array(
-		'allow_anonymous' => 'Umožnit anonymně číst články výchozího uživatele (%s)',
-		'allow_anonymous_refresh' => 'Umožnit anonymní obnovení článků',
-		'api_enabled' => 'Povolit přístup k <abbr>API</abbr> <small>(vyžadováno mobilními aplikacemi)</small>',
+		'allow_anonymous' => 'Povolit anonymní čtení článků výchozího uživatele (%s)',
+		'allow_anonymous_refresh' => 'Povolit anonymní obnovení článků',
+		'api_enabled' => 'Povolit přístup k <abbr>API</abbr> <small>(vyžadováno pro mobilní aplikace)</small>',
 		'form' => 'Webový formulář (tradiční, vyžaduje JavaScript)',
 		'http' => 'HTTP (pro pokročilé uživatele s HTTPS)',
 		'none' => 'Žádný (nebezpečné)',
-		'title' => 'Přihlášení',
-		'token' => 'Authentizační token',
-		'token_help' => 'Umožňuje přístup k RSS kanálu článků výchozího uživatele bez přihlášení:',
-		'type' => 'Způsob přihlášení',
-		'unsafe_autologin' => 'Povolit nebezpečné automatické přihlášení přes: ',
+		'title' => 'Ověřování',
+		'token' => 'Ověřovací token',
+		'token_help' => 'Umožňuje přístup k výstupu RSS výchozího uživatele bez ověřování:',
+		'type' => 'Metoda ověřování',
+		'unsafe_autologin' => 'Povolit nebezpečné automatické přihlášení pomocí formátu: ',
 	),
 	'check_install' => array(
 		'cache' => array(
-			'nok' => 'Zkontrolujte oprávnění adresáře <em>./data/cache</em>. HTTP server musí mít do tohoto adresáře práva zápisu',
+			'nok' => 'Zkontrolujte oprávnění adresáře <em>./data/cache</em>. Server HTTP musí mít oprávnění pro zápis.',
 			'ok' => 'Oprávnění adresáře cache jsou v pořádku.',
 		),
 		'categories' => array(
@@ -35,31 +35,31 @@ return array(
 		),
 		'connection' => array(
 			'nok' => 'Nelze navázat spojení s databází.',
-			'ok' => 'Připojení k databázi je v pořádku.',
+			'ok' => 'Spojení s databází je v pořádku.',
 		),
 		'ctype' => array(
-			'nok' => 'Nemáte požadovanou knihovnu pro ověřování znaků (php-ctype).',
-			'ok' => 'Máte požadovanou knihovnu pro ověřování znaků (ctype).',
+			'nok' => 'Nelze nalézt požadovanou knihovnu pro ověřování typu znaků (php-ctype).',
+			'ok' => 'Máte požadovanou knihovnu pro ověřování typu znaků (ctype).',
 		),
 		'curl' => array(
-			'nok' => 'Nemáte cURL (balíček php-curl).',
-			'ok' => 'Máte rozšíření cURL.',
+			'nok' => 'Nelze nalézt knihovnu cURL (balíček php-curl).',
+			'ok' => 'Máte knihovnu cURL.',
 		),
 		'data' => array(
-			'nok' => 'Zkontrolujte oprávnění adresáře <em>./data</em>. HTTP server musí mít do tohoto adresáře práva zápisu',
+			'nok' => 'Zkontrolujte oprávnění adresáře <em>./data</em>. Server HTTP musí mít oprávnění pro zápis.',
 			'ok' => 'Oprávnění adresáře data jsou v pořádku.',
 		),
 		'database' => 'Instalace databáze',
 		'dom' => array(
-			'nok' => 'Nemáte požadovanou knihovnu pro procházení DOM (balíček php-xml).',
+			'nok' => 'Nelze nalézt požadovanou knihovnu pro procházení DOM (balíček php-xml).',
 			'ok' => 'Máte požadovanou knihovnu pro procházení DOM.',
 		),
 		'entries' => array(
-			'nok' => 'Tabulka článků je nastavena špatně.',
-			'ok' => 'Tabulka kategorií je v pořádku.',
+			'nok' => 'Tabulka položek je nastavena špatně.',
+			'ok' => 'Tabulka položek je v pořádku.',
 		),
 		'favicons' => array(
-			'nok' => 'Zkontrolujte oprávnění adresáře <em>./data/favicons</em>. HTTP server musí mít do tohoto adresáře práva zápisu',
+			'nok' => 'Zkontrolujte oprávnění adresáře <em>./data/favicons</em>. Server HTTP musí mít oprávnění pro zápis.',
 			'ok' => 'Oprávnění adresáře favicons jsou v pořádku.',
 		),
 		'feeds' => array(
@@ -67,28 +67,28 @@ return array(
 			'ok' => 'Tabulka kanálů je v pořádku.',
 		),
 		'fileinfo' => array(
-			'nok' => 'Nemáte PHP fileinfo (balíček fileinfo).',
-			'ok' => 'Máte rozšíření fileinfo.',
+			'nok' => 'Nelze nalézt knihovnu PHP fileinfo (balíček fileinfo).',
+			'ok' => 'Máte knihovnu fileinfo.',
 		),
 		'files' => 'Instalace souborů',
 		'json' => array(
-			'nok' => 'Nemáte JSON (balíček php-json).',
+			'nok' => 'Nelze nalézt JSON (balíček php-json).',
 			'ok' => 'Máte rozšíření JSON.',
 		),
 		'mbstring' => array(
-			'nok' => 'Cannot find the recommended mbstring library for Unicode.',	// TODO
-			'ok' => 'You have the recommended mbstring library for Unicode.',	// TODO
+			'nok' => 'Nelze nalézt doporučenou knihovnu mbstring pro Unicode.',
+			'ok' => 'Máte doporučenou knihovnu mbstring pro Unicode.',
 		),
 		'pcre' => array(
-			'nok' => 'Nemáte požadovanou knihovnu pro regulární výrazy (php-pcre).',
+			'nok' => 'Nelze nalézt požadovanou knihovnu pro regulární výrazy (php-pcre).',
 			'ok' => 'Máte požadovanou knihovnu pro regulární výrazy (PCRE).',
 		),
 		'pdo' => array(
-			'nok' => 'Nemáte PDO nebo některý z podporovaných ovladačů (pdo_mysql, pdo_sqlite, pdo_pgsql).',
+			'nok' => 'Nelze nalézt PDO nebo některý z podporovaných ovladačů (pdo_mysql, pdo_sqlite, pdo_pgsql).',
 			'ok' => 'Máte PDO a alespoň jeden z podporovaných ovladačů (pdo_mysql, pdo_sqlite, pdo_pgsql).',
 		),
 		'php' => array(
-			'_' => 'PHP instalace',
+			'_' => 'Instalace PHP',
 			'nok' => 'Vaše verze PHP je %s, ale FreshRSS vyžaduje alespoň verzi %s.',
 			'ok' => 'Vaše verze PHP je %s a je kompatibilní s FreshRSS.',
 		),
@@ -98,75 +98,75 @@ return array(
 		),
 		'title' => 'Kontrola instalace',
 		'tokens' => array(
-			'nok' => 'Zkontrolujte oprávnění adresáře <em>./data/tokens</em>. HTTP server musí mít do tohoto adresáře práva zápisu',
+			'nok' => 'Zkontrolujte oprávnění adresáře <em>./data/tokens</em>. Server HTTP musí mít oprávnění pro zápis.',
 			'ok' => 'Oprávnění adresáře tokens jsou v pořádku.',
 		),
 		'users' => array(
-			'nok' => 'Zkontrolujte oprávnění adresáře <em>./data/users</em>. HTTP server musí mít do tohoto adresáře práva zápisu',
+			'nok' => 'Zkontrolujte oprávnění adresáře <em>./data/users</em>. Server HTTP musí mít oprávnění pro zápis.',
 			'ok' => 'Oprávnění adresáře users jsou v pořádku.',
 		),
 		'zip' => array(
-			'nok' => 'Nemáte rozšíření ZIP (balíček php-zip).',
+			'nok' => 'Nelze nalézt rozšíření ZIP (balíček php-zip).',
 			'ok' => 'Máte rozšíření ZIP.',
 		),
 	),
 	'extensions' => array(
-		'author' => 'Author',	// TODO
-		'community' => 'Available community extensions',	// TODO
-		'description' => 'Description',	// TODO
-		'disabled' => 'Vypnuto',
-		'empty_list' => 'Není naistalováno žádné rozšíření',
-		'enabled' => 'Zapnuto',
-		'latest' => 'Installed',	// TODO
-		'name' => 'Name',	// TODO
-		'no_configure_view' => 'Toto rozšíření nemá žádné možnosti nastavení.',
+		'author' => 'Autor',
+		'community' => 'Dostupná komunitní rozšíření',
+		'description' => 'Popis',
+		'disabled' => 'Zakázáno',
+		'empty_list' => 'Nejsou naistalována žádná rozšíření',
+		'enabled' => 'Povoleno',
+		'latest' => 'Nainstalováno',
+		'name' => 'Název',
+		'no_configure_view' => 'Toto rozšíření nemá žádná nastavení.',
 		'system' => array(
 			'_' => 'Systémová rozšíření',
-			'no_rights' => 'Systémová rozšíření (na ně nemáte oprávnění)',
+			'no_rights' => 'Systémová rozšíření (nemáte požadovaná oprávnění)',
 		),
 		'title' => 'Rozšíření',
-		'update' => 'Update available',	// TODO
+		'update' => 'Dostupná aktualizace',
 		'user' => 'Uživatelská rozšíření',
-		'version' => 'Version',	// TODO
+		'version' => 'Verze',
 	),
 	'stats' => array(
 		'_' => 'Statistika',
 		'all_feeds' => 'Všechny kanály',
 		'category' => 'Kategorie',
-		'entry_count' => 'Počet článků',
-		'entry_per_category' => 'Článků na kategorii',
-		'entry_per_day' => 'Článků za den (posledních 30 dní)',
-		'entry_per_day_of_week' => 'Za den v týdnu (průměr: %.2f zprávy)',
-		'entry_per_hour' => 'Za hodinu (průměr: %.2f zprávy)',
-		'entry_per_month' => 'Za měsíc (průměr: %.2f zprávy)',
-		'entry_repartition' => 'Rozdělení článků',
+		'entry_count' => 'Počet položek',
+		'entry_per_category' => 'Položek na kategorii',
+		'entry_per_day' => 'Položek za den (posledních 30 dní)',
+		'entry_per_day_of_week' => 'Za den v týdnu (průměr: %.2f zpráv)',
+		'entry_per_hour' => 'Za hodinu (průměr: %.2f zpráv)',
+		'entry_per_month' => 'Za měsíc (průměr: %.2f zpráv)',
+		'entry_repartition' => 'Přerozdělení položek',
 		'feed' => 'Kanál',
-		'feed_per_category' => 'Článků na kategorii',
-		'idle' => 'Neaktivní kanály',
-		'main' => 'Přehled',
+		'feed_per_category' => 'Kanálů na kategorii',
+		'idle' => 'Nečinné kanály',
+		'main' => 'Hlavní statistika',
 		'main_stream' => 'Všechny kanály',
-		'no_idle' => 'Žádné neaktivní kanály!',
+		'no_idle' => 'Nejsou žádné nečinné kanály!',
 		'number_entries' => '%d článků',
 		'percent_of_total' => '%% ze všech',
-		'repartition' => 'Rozdělení článků',
+		'repartition' => 'Přerozdělení článků',
 		'status_favorites' => 'Oblíbené',
 		'status_read' => 'Přečtené',
 		'status_total' => 'Celkem',
 		'status_unread' => 'Nepřečtené',
 		'title' => 'Statistika',
-		'top_feed' => 'Top ten kanálů',
+		'top_feed' => 'Top 10 kanálů',
 	),
 	'system' => array(
-		'_' => 'System configuration',	// TODO
-		'auto-update-url' => 'Auto-update server URL',	// TODO
+		'_' => 'Nastavení systému',
+		'auto-update-url' => 'Adresa URL serveru pro automatické aktualizace',
 		'cookie-duration' => array(
-			'help' => 'in seconds',	// TODO
-			'number' => 'Duration to keep logged in',	// TODO
+			'help' => 'v sekundách',
+			'number' => 'Trvání ponechání přihlášení',
 		),
-		'force_email_validation' => 'Force email address validation',	// TODO
-		'instance-name' => 'Instance name',	// TODO
-		'max-categories' => 'Max number of categories per user',	// TODO
-		'max-feeds' => 'Max number of feeds per user',	// TODO
+		'force_email_validation' => 'Vynutit ověření e-mailové adresy',
+		'instance-name' => 'Název instance',
+		'max-categories' => 'Maximální počet kategorií na uživatele',
+		'max-feeds' => 'Maximální počet kanálů na uživatele',
 		'registration' => array(
 			'help' => '0 znamená žádná omezení účtu',
 			'number' => 'Maximální počet účtů',
@@ -176,29 +176,29 @@ return array(
 		'_' => 'Aktualizace systému',
 		'apply' => 'Použít',
 		'check' => 'Zkontrolovat aktualizace',
-		'current_version' => 'Vaše instalace FreshRSS je verze %s.',
+		'current_version' => 'Vaše aktuální verze FreshRSS je %s.',
 		'last' => 'Poslední kontrola: %s',
 		'none' => 'Žádné nové aktualizace',
 		'title' => 'Aktualizovat systém',
 	),
 	'user' => array(
-		'admin' => 'Administrator',	// TODO
-		'article_count' => 'Articles',	// TODO
-		'back_to_manage' => '← Return to user list',	// TODO
+		'admin' => 'Administrátor',
+		'article_count' => 'Článků',
+		'back_to_manage' => '← Zpět na seznam uživatelů',
 		'create' => 'Vytvořit nového uživatele',
-		'database_size' => 'Database size',	// TODO
-		'email' => 'Email address',	// TODO
-		'enabled' => 'Enabled',	// TODO
-		'feed_count' => 'Feeds',	// TODO
-		'is_admin' => 'Is admin',	// TODO
+		'database_size' => 'Velikost databáze',
+		'email' => 'E-mailová adresa',
+		'enabled' => 'Povolen',
+		'feed_count' => 'Kanálů',
+		'is_admin' => 'Je admin',
 		'language' => 'Jazyk',
-		'last_user_activity' => 'Last user activity',	// TODO
-		'list' => 'User list',	// TODO
+		'last_user_activity' => 'Poslední aktivita uživatele',
+		'list' => 'Seznam uživatelů',
 		'number' => 'Zatím je vytvořen %d účet',
 		'numbers' => 'Zatím je vytvořeno %d účtů',
 		'password_form' => 'Heslo<br /><small>(pro přihlášení webovým formulářem)</small>',
 		'password_format' => 'Alespoň 7 znaků',
 		'title' => 'Správa uživatelů',
-		'username' => 'Přihlašovací jméno',
+		'username' => 'Uživatelské jméno',
 	),
 );
