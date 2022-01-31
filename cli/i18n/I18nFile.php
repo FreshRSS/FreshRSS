c72ea4f190a1dac9cb521dcd5490fbf1a61fdabd
Alexandre Alapetite <alexandre@alapetite.fr>
2022-01-31 13:06:23 +0100

    i18n CLI compat PHP 7.0 Minor syntax change to pass the linting in PHP 7.0

--- cli/i18n/I18nFile.php
+++ cli/i18n/I18nFile.php
@@ -152,20 +152,20 @@ class I18nFile {
 		$translation = preg_replace($patterns, $replacements, $translation);
 
 		return <<<OUTPUT
-		<?php
+<?php
 
-		/******************************************************************************/
-		/* Each entry of that file can be associated with a comment to indicate its   */
-		/* state. When there is no comment, it means the entry is fully translated.   */
-		/* The recognized comments are (comment matching is case-insensitive):        */
-		/*   + TODO: the entry has never been translated.                             */
-		/*   + DIRTY: the entry has been translated but needs to be updated.          */
-		/*   + IGNORE: the entry does not need to be translated.                      */
-		/* When a comment is not recognized, it is discarded.                         */
-		/******************************************************************************/
+/******************************************************************************/
+/* Each entry of that file can be associated with a comment to indicate its   */
+/* state. When there is no comment, it means the entry is fully translated.   */
+/* The recognized comments are (comment matching is case-insensitive):        */
+/*   + TODO: the entry has never been translated.                             */
+/*   + DIRTY: the entry has been translated but needs to be updated.          */
+/*   + IGNORE: the entry does not need to be translated.                      */
+/* When a comment is not recognized, it is discarded.                         */
+/******************************************************************************/
 
-		return {$translation};
+return {$translation};
 
-		OUTPUT;
+OUTPUT;
 	}
 }
