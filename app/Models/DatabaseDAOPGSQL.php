FreshRSS/FreshRSS

commit 947e918f05d70d5dce4efa4ef403e593581c3fa9

Travis: Enforce phpcs line length + whitespace (#3488)
* Update Travis line length

* Also check whitespace in CSS files

* Fix line length ext.php

* More syntax, string templates

* Fix exclude-pattern

* Test JS files as well

app/Models/DatabaseDAOPGSQL.php
@@ -57,13 +57,15 @@ class FreshRSS_DatabaseDAOPGSQL extends FreshRSS_DatabaseDAOSQLite {
 			$stm->bindParam(':base', $db['base']);
 			$stm->execute();
 		} else {
-			$sql = "SELECT "
-			     . "pg_total_relation_size('`{$this->pdo->prefix()}category`') + "
-			     . "pg_total_relation_size('`{$this->pdo->prefix()}feed`') + "
-			     . "pg_total_relation_size('`{$this->pdo->prefix()}entry`') + "
-			     . "pg_total_relation_size('`{$this->pdo->prefix()}entrytmp`') + "
-			     . "pg_total_relation_size('`{$this->pdo->prefix()}tag`') + "
-			     . "pg_total_relation_size('`{$this->pdo->prefix()}entrytag`')";
+			$sql = <<<SQL
+SELECT
+pg_total_relation_size('`{$this->pdo->prefix()}category`')
+pg_total_relation_size('`{$this->pdo->prefix()}feed`')
+pg_total_relation_size('`{$this->pdo->prefix()}entry`')
+pg_total_relation_size('`{$this->pdo->prefix()}entrytmp`')
+pg_total_relation_size('`{$this->pdo->prefix()}tag`')
+pg_total_relation_size('`{$this->pdo->prefix()}entrytag`')
+SQL;
 			$stm = $this->pdo->query($sql);
 		}
 		if ($stm == false) {

commit 5eaefe0c9ce269cbe1cccccfad4941d54336f43b

Fix PostgreSQL size calculation (#4249)
Regression from https://github.com/FreshRSS/FreshRSS/pull/3488

app/Models/DatabaseDAOPGSQL.php
@@ -59,11 +59,11 @@ class FreshRSS_DatabaseDAOPGSQL extends FreshRSS_DatabaseDAOSQLite {
 		} else {
 			$sql = <<<SQL
 SELECT
-pg_total_relation_size('`{$this->pdo->prefix()}category`')
-pg_total_relation_size('`{$this->pdo->prefix()}feed`')
-pg_total_relation_size('`{$this->pdo->prefix()}entry`')
-pg_total_relation_size('`{$this->pdo->prefix()}entrytmp`')
-pg_total_relation_size('`{$this->pdo->prefix()}tag`')
+pg_total_relation_size('`{$this->pdo->prefix()}category`') +
+pg_total_relation_size('`{$this->pdo->prefix()}feed`') +
+pg_total_relation_size('`{$this->pdo->prefix()}entry`') +
+pg_total_relation_size('`{$this->pdo->prefix()}entrytmp`') +
+pg_total_relation_size('`{$this->pdo->prefix()}tag`') +
 pg_total_relation_size('`{$this->pdo->prefix()}entrytag`')
 SQL;
 			$stm = $this->pdo->query($sql);