5110d1db3e030d4f7960e18bf3e9db8cb5a2fa3a
Artur Weigandt <Art4@users.noreply.github.com>
2022-05-14 23:38:16 +0200

    Update to PHPMailer 6.6.0 with composer (#4329)
    * Install PHPMailer 6.5.1 with composer
    
    * Fix folder name for phpmailer
    
    * Update PHPMailer to v6.6.0, add LICENSE and README.md
    
    * remove composer and autoload.php after install/update
    
    * Fix path to PHPMailer for PHPStan
    
    * Remove lib/composer.lock, lock PHPMailer at version 6.6.0
    
    * Improve PSR-4 autoloading for PHPMailer
    
    * Markdownlint ignore
    
    * Delete some obvious comments
    
    * ignores
    
    * Support composer update --no-autoloader
    
    * Automatic cleaning
    With rules based on https://github.com/PHPMailer/PHPMailer/blob/master/README.md#minimal-installation
    
    * Readme gitignore hint
    
    Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

---
+++ tests/lib/PHPMailer/PHPMailerTest.php
@@ -0,0 +1,8 @@
+<?php
+
+class PHPMailerTest extends PHPUnit\Framework\TestCase {
+
+	public function testPHPMailerClassExists() {
+		$this->assertTrue(class_exists('PHPMailer\\PHPMailer\\PHPMailer'));
+	}
+}
