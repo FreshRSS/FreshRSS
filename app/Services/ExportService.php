9ef849eb27075b30bc144a9f8a3baa040196c563
harshad389 <harshad.389@gmail.com>
2022-08-05 18:41:58 +0530

    @tempnam in ExportService.php #4304 (#4489)
    * @tempnam in ExportService.php #4304
    
    * Update CREDITS.md
    
    Change Name in alphabetic order
    
    Co-authored-by: Harshad <harshad@topsoinfosolutions.com>

--- app/Services/ExportService.php
+++ app/Services/ExportService.php
@@ -165,7 +165,7 @@ class FreshRSS_Export_Service {
 		$zip_filename = 'freshrss_' . $this->username . '_' . $day . '_export.zip';
 
 		// From https://stackoverflow.com/questions/1061710/php-zip-files-on-the-fly
-		$zip_file = @tempnam('/tmp', 'zip');
+		$zip_file = tempnam('/tmp', 'zip');
 		$zip_archive = new ZipArchive();
 		$zip_archive->open($zip_file, ZipArchive::OVERWRITE);
 
