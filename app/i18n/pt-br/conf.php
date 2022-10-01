e1fe2e1f48ca5bb703f53ff3414dc653c8b6a496
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-10-01 19:29:08 -0300

    feat(translate): add deprecated terms translatePTBR

--- app/i18n/pt-br/conf.php
+++ app/i18n/pt-br/conf.php
@@ -222,7 +222,7 @@ return array(
 		'_' => 'Compartilhando',
 		'add' => 'Adicionar um método de compartilhamento',
 		'blogotext' => 'Blogotext',	// IGNORE
-		'deprecated' => 'This service is deprecated and will be removed from FreshRSS in a <a href="https://freshrss.github.io/FreshRSS/en/users/08_sharing_services.html" title="Open documentation for more information" target="_blank">future release</a>.',	// TODO
+		'deprecated' => 'Este serviço está obceloeto e será removido do FreshRSS <a href="https://freshrss.github.io/FreshRSS/en/users/08_sharing_services.html" title="Abra este documento para mais informações" target="_blank">em versões futuras</a>.',
 		'diaspora' => 'Diaspora*',	// IGNORE
 		'email' => 'E-mail',
 		'facebook' => 'Facebook',	// IGNORE
