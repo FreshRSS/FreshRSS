648a876d77a7591eac6c4ad5f14b30988e18ce20
Alexandre Alapetite <alexandre@alapetite.fr>
2022-10-09 15:43:30 +0200

    Add support for custom XPath date/time format (#4703)
    * Add support for custom XPath date/time format
    #fix https://github.com/FreshRSS/FreshRSS/issues/4701
    Improvement of https://github.com/FreshRSS/FreshRSS/pull/4220
    
    * Format is not XPath
    
    * Remove TODOs in en-GB

--- app/i18n/pl/sub.php
+++ app/i18n/pl/sub.php
@@ -98,6 +98,10 @@ return array(
 					'_' => 'miniaturki',
 					'help' => 'Przykład: <code>descendant::img/@src</code>',
 				),
+				'item_timeFormat' => array(
+					'_' => 'Custom date/time format',	// TODO
+					'help' => 'Optional. A format supported by <a href="https://php.net/datetime.createfromformat" target="_blank"><code>DateTime::createFromFormat()</code></a> such as <code>d-m-Y H:i:s</code>',	// TODO
+				),
 				'item_timestamp' => array(
 					'_' => 'daty',
 					'help' => 'Wynik zostanie przetworzony za pomocą funkcji <a href="https://php.net/strtotime" target="_blank"><code>strtotime()</code></a>',
