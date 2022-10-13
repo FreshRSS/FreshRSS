859aa84c2d62242885d8a696ca68ce474896a589
Konstantinos Megas <44091936+nextdoorpanda@users.noreply.github.com>
2022-10-13 11:30:42 +0300

    adds greek translation (#4718)
    * adds greek translation
    
    * update translation
    
    * make fix-all
    
    * Typographic quotes merge
    
    Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

--- app/i18n/el/feedback.php
+++ app/i18n/el/feedback.php
@@ -12,11 +12,11 @@
 
 return array(
 	'access' => array(
-		'denied' => 'אין לך הרשאות לצפות בדף זה',
-		'not_found' => 'הדף הזה לא נמצא',
+		'denied' => 'You don’t have permission to access this page',	// TODO
+		'not_found' => 'You are looking for a page that doesn’t exist',	// TODO
 	),
 	'admin' => array(
-		'optimization_complete' => 'המיטוב הושלם',
+		'optimization_complete' => 'Optimization complete',	// TODO
 	),
 	'api' => array(
 		'password' => array(
@@ -26,7 +26,7 @@ return array(
 	),
 	'auth' => array(
 		'login' => array(
-			'invalid' => 'הכניסה לחשבון שגויה',
+			'invalid' => 'Login is invalid',	// TODO
 			'success' => 'You are connected',	// TODO
 		),
 		'logout' => array(
@@ -35,9 +35,9 @@ return array(
 	),
 	'conf' => array(
 		'error' => 'An error occurred while saving configuration',	// TODO
-		'query_created' => 'השאילתה “%s” נוצרה.',
-		'shortcuts_updated' => 'קיצורי הדרך עודכנו',
-		'updated' => 'ההגדרות עודכנו',
+		'query_created' => 'Query “%s” has been created.',	// TODO
+		'shortcuts_updated' => 'Shortcuts have been updated',	// TODO
+		'updated' => 'Configuration has been updated',	// TODO
 	),
 	'extensions' => array(
 		'already_enabled' => '%s is already enabled',	// TODO
@@ -51,24 +51,24 @@ return array(
 			'ok' => '%s is now enabled',	// TODO
 		),
 		'no_access' => 'You have no access on %s',	// TODO
-		'not_enabled' => '%s is not enabled yet',
+		'not_enabled' => '%s is not enabled',	// TODO
 		'not_found' => '%s does not exist',	// TODO
 		'removed' => '%s removed',	// TODO
 	),
 	'import_export' => array(
-		'export_no_zip_extension' => 'הרחבת ZIP אינה מותקנת על השרת.',
-		'feeds_imported' => 'ההזנות שלך יובאו וכעת יעודכנו',
-		'feeds_imported_with_errors' => 'ההזנות שלך יובאו אך אירעו מספר שגיאות',
-		'file_cannot_be_uploaded' => 'אין אפשרות להעלות את הקובץ!',
-		'no_zip_extension' => 'הרחבת ZIP אינה מותקנת על השרת.',
-		'zip_error' => 'אירעה שגיאה במהלך ייבוא קובץ הZIP.',
+		'export_no_zip_extension' => 'The ZIP extension is not present on your server. Please try to export files one by one.',	// TODO
+		'feeds_imported' => 'Your feeds have been imported and will now be updated',	// TODO
+		'feeds_imported_with_errors' => 'Your feeds have been imported, but some errors occurred',	// TODO
+		'file_cannot_be_uploaded' => 'File cannot be uploaded!',	// TODO
+		'no_zip_extension' => 'The ZIP extension is not present on your server.',	// TODO
+		'zip_error' => 'An error occurred during ZIP import.',	// TODO
 	),
 	'profile' => array(
 		'error' => 'Your profile cannot be modified',	// TODO
 		'updated' => 'Your profile has been modified',	// TODO
 	),
 	'sub' => array(
-		'actualize' => 'מימוש',
+		'actualize' => 'Updating',	// TODO
 		'articles' => array(
 			'marked_read' => 'The selected articles have been marked as read.',	// TODO
 			'marked_unread' => 'The articles have been marked as unread.',	// TODO
@@ -76,10 +76,10 @@ return array(
 		'category' => array(
 			'created' => 'Category %s has been created.',	// TODO
 			'deleted' => 'Category has been deleted.',	// TODO
-			'emptied' => 'הקטגוריה רוקנה',
+			'emptied' => 'Category has been emptied',	// TODO
 			'error' => 'Category cannot be updated',	// TODO
 			'name_exists' => 'Category name already exists.',	// TODO
-			'no_id' => 'You must precise the id of the category.',
+			'no_id' => 'You must specify the id of the category.',	// TODO
 			'no_name' => 'Category name cannot be empty.',	// TODO
 			'not_delete_default' => 'You cannot delete the default category!',	// TODO
 			'not_exist' => 'The category does not exist!',	// TODO
@@ -87,19 +87,19 @@ return array(
 			'updated' => 'Category has been updated.',	// TODO
 		),
 		'feed' => array(
-			'actualized' => '<em>%s</em> עודכן',
-			'actualizeds' => 'הזנות RSS עודכנו',
-			'added' => 'RSS הזנת <em>%s</em> נוספה',
-			'already_subscribed' => 'אתה כבר רשום ל <em>%s</em>',
+			'actualized' => '<em>%s</em> has been updated',	// TODO
+			'actualizeds' => 'RSS feeds have been updated',	// TODO
+			'added' => 'RSS feed <em>%s</em> has been added',	// TODO
+			'already_subscribed' => 'You have already subscribed to <em>%s</em>',	// TODO
 			'cache_cleared' => '<em>%s</em> cache has been cleared',	// TODO
-			'deleted' => 'ההזנה נמחקה',
+			'deleted' => 'Feed has been deleted',	// TODO
 			'error' => 'Feed cannot be updated',	// TODO
-			'internal_problem' => 'אין אפשרות להוסיף את ההזנה. <a href="%s">בדקו את הלוגים</a> לפרטים. You can try force adding by appending <code>#force_feed</code> to the URL.',	// DIRTY
-			'invalid_url' => 'URL <em>%s</em> אינו תקין',
-			'n_actualized' => '%d הזנות עודכנו',
-			'n_entries_deleted' => '%d המאמרים נמחקו',
-			'no_refresh' => 'אין הזנה שניתן לרענן…',
-			'not_added' => '<em>%s</em> אין אפשרות להוסיף את',
+			'internal_problem' => 'The newsfeed could not be added. <a href="%s">Check FreshRSS logs</a> for details. You can try force adding by appending <code>#force_feed</code> to the URL.',	// TODO
+			'invalid_url' => 'URL <em>%s</em> is invalid',	// TODO
+			'n_actualized' => '%d feeds have been updated',	// TODO
+			'n_entries_deleted' => '%d articles have been deleted',	// TODO
+			'no_refresh' => 'There are no feeds to refresh',	// TODO
+			'not_added' => '<em>%s</em> could not be added',	// TODO
 			'not_found' => 'Feed cannot be found',	// TODO
 			'over_max' => 'You have reached your limit of feeds (%d)',	// TODO
 			'reloaded' => '<em>%s</em> has been reloaded',	// TODO
@@ -110,9 +110,9 @@ return array(
 				'no_result' => 'The selector didn’t match anything. As a fallback the original feed text will be displayed instead.',	// TODO
 				'selector_empty' => 'The selector is empty. You need to define one to create a preview.',	// TODO
 			),
-			'updated' => 'ההזנה התעדכנה',
+			'updated' => 'Feed has been updated',	// TODO
 		),
-		'purge_completed' => 'הניקוי הושלם (%d מאמרים נמחקו)',
+		'purge_completed' => 'Purge completed (%d articles deleted)',	// TODO
 	),
 	'tag' => array(
 		'created' => 'Tag “%s” has been created.',	// TODO
@@ -120,20 +120,20 @@ return array(
 		'renamed' => 'Tag “%s” has been renamed to “%s”.',	// TODO
 	),
 	'update' => array(
-		'can_apply' => 'FreshRSS will be now updated to the <strong>version %s</strong>.',
-		'error' => 'תהליך העדכון נתקל בשגיאה: %s',
-		'file_is_nok' => 'יש לבדוק את ההרשאות בתיקייה <em>%s</em>. שרת הHTTP חייב להיות בעל הרשאות כתיבה.',
-		'finished' => 'העדכון הושלם!',
-		'none' => 'אין עדכון להחלה',
-		'server_not_found' => 'לא ניתן למצוא את שרת העדכון. [%s]',
+		'can_apply' => 'FreshRSS will now be updated to the <strong>version %s</strong>.',	// TODO
+		'error' => 'The update process has encountered an error: %s',	// TODO
+		'file_is_nok' => 'New <strong>version %s</strong> available, but check permissions on <em>%s</em> directory. HTTP server must have have write permission',	// TODO
+		'finished' => 'Update complete!',	// TODO
+		'none' => 'No update to apply',	// TODO
+		'server_not_found' => 'Update server cannot be found. [%s]',	// TODO
 	),
 	'user' => array(
 		'created' => array(
-			'_' => 'המשתמש %s נוצר',
+			'_' => 'User %s has been created',	// TODO
 			'error' => 'User %s cannot be created',	// TODO
 		),
 		'deleted' => array(
-			'_' => 'המשתמש %s נמחק',
+			'_' => 'User %s has been deleted',	// TODO
 			'error' => 'User %s cannot be deleted',	// TODO
 		),
 		'updated' => array(
