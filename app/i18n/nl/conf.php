1c1a466214957966048aeaa16367ac2d5196fa0d
Alexandre Alapetite <alexandre@alapetite.fr>
2022-10-25 00:21:07 +0200

    Update app/i18n/nl/conf.php
    Co-authored-by: Frans de Jonge <fransdejonge@gmail.com>

--- app/i18n/nl/conf.php
+++ app/i18n/nl/conf.php
@@ -102,7 +102,7 @@ return array(
 			'order' => 'Sorteren op datum',
 			'search' => 'Expressie',
 			'state' => 'Status',
-			'tags' => 'Weergeven op tag',	// DIRTY
+			'tags' => 'Weergeven op label',
 			'type' => 'Type',	// IGNORE
 		),
 		'get_all' => 'Toon alle artikelen',
