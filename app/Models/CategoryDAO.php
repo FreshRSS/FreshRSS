87b82f9528b6cebe4f10dc65347bb7866845d99e
Alexandre Alapetite <alexandre@alapetite.fr>
2022-11-06 16:43:27 +0100

    Avoid exception in searchById https://github.com/FreshRSS/FreshRSS/issues/4820 Not a root-cause fix though (cannot reproduce so far)

--- app/Models/CategoryDAO.php
+++ app/Models/CategoryDAO.php
@@ -228,16 +228,16 @@ SQL;
 	public function searchById($id) {
 		$sql = 'SELECT * FROM `_category` WHERE id=:id';
 		$stm = $this->pdo->prepare($sql);
-		$stm->bindParam(':id', $id, PDO::PARAM_INT);
-		$stm->execute();
-		$res = $stm->fetchAll(PDO::FETCH_ASSOC);
-		$cat = self::daoToCategory($res);
-
-		if (isset($cat[0])) {
-			return $cat[0];
-		} else {
-			return null;
+		if ($stm &&
+			$stm->bindParam(':id', $id, PDO::PARAM_INT) &&
+			$stm->execute()) {
+			$res = $stm->fetchAll(PDO::FETCH_ASSOC);
+			$cat = self::daoToCategory($res);
+			if (isset($cat[0])) {
+				return $cat[0];
+			}
 		}
+		return null;
 	}
 
 	/** @return FreshRSS_Category|null|false */
