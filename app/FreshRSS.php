502c70fd4f869851f0f0a7c940059aa30a08e930
Alexandre Alapetite <alexandre@alapetite.fr>
2022-11-29 14:57:51 +0100

    Timezone when creating a new user

--- app/FreshRSS.php
+++ app/FreshRSS.php
@@ -101,7 +101,7 @@ class FreshRSS extends Minz_FrontController {
 		Minz_Translate::init($language);
 
 		$timezone = isset(FreshRSS_Context::$user_conf) ? FreshRSS_Context::$user_conf->timezone : '';
-		date_default_timezone_set($timezone != '' ? $timezone : ini_get('date.timezone'));
+		date_default_timezone_set($timezone != '' ? $timezone : '' . ini_get('date.timezone'));
 	}
 
 	private static function getThemeFileUrl($theme_id, $filename) {
