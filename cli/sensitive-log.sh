c192164a45ca31c243ae214999b7b77b98a18c0b
Alexandre Alapetite <alexandre@alapetite.fr>
2023-01-10 20:05:37 +0100

    shfmt

--- cli/sensitive-log.sh
+++ cli/sensitive-log.sh
@@ -5,5 +5,5 @@
 # N.B.: `sed -u` is not available in BusyBox and without it there are buffering delays (even with stdbuf)
 sed -Eu 's/([?&])(Passwd|token)=[^& \t\n]+/\1\2=█/ig' 2>/dev/null ||
 
-# For systems with gawk (not available by default in Docker of Debian or Alpine) or with BuzyBox such as Alpine
-$(which gawk || which awk) -v IGNORECASE=1 '{ print gensub(/([?&])(Passwd|token)=[^& \t\n]+/, "\\1\\2=█", "g") }'
+	# For systems with gawk (not available by default in Docker of Debian or Alpine) or with BuzyBox such as Alpine
+	$(which gawk || which awk) -v IGNORECASE=1 '{ print gensub(/([?&])(Passwd|token)=[^& \t\n]+/, "\\1\\2=█", "g") }'
