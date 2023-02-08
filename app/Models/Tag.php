c3b0e367aba1158006c597c680a765141d3e5f13
Luc <sanchezluc+freshrss@gmail.com>
2023-02-08 12:17:53 +0100

    code improvement for phpstan and humans ;)

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -10,35 +10,38 @@ class FreshRSS_Tag extends Minz_Model {
 	private $nbEntries = -1;
 	private $nbUnread = -1;
 
-	public function __construct($name = '') {
+	public function __construct(string $name = '') {
 		$this->_name($name);
 	}
 
-	public function id() {
+	public function id(): int {
 		return $this->id;
 	}
 
-	public function _id($value) {
+	public function _id($value): void {
 		$this->id = (int)$value;
 	}
 
-	public function name() {
+	public function name(): string {
 		return $this->name;
 	}
 
-	public function _name($value) {
+	public function _name(string $value): void {
 		$this->name = trim($value);
 	}
 
+	/**
+	 * @return array|mixed|null
+	 */
 	public function attributes($key = '') {
 		if ($key == '') {
 			return $this->attributes;
 		} else {
-			return isset($this->attributes[$key]) ? $this->attributes[$key] : null;
+			return $this->attributes[$key] ?? null;
 		}
 	}
 
-	public function _attributes($key, $value) {
+	public function _attributes($key, $value): void {
 		if ($key == '') {
 			if (is_string($value)) {
 				$value = json_decode($value, true);
@@ -61,10 +64,13 @@ class FreshRSS_Tag extends Minz_Model {
 		return $this->nbEntries;
 	}
 
-	public function _nbEntries($value) {
+	public function _nbEntries($value): void {
 		$this->nbEntries = (int)$value;
 	}
 
+	/**
+	 * @return mixed
+	 */
 	public function nbUnread() {
 		if ($this->nbUnread < 0) {
 			$tagDAO = FreshRSS_Factory::createTagDao();
@@ -73,7 +79,7 @@ class FreshRSS_Tag extends Minz_Model {
 		return $this->nbUnread;
 	}
 
-	public function _nbUnread($value) {
+	public function _nbUnread($value): void {
 		$this->nbUnread = (int)$value;
 	}
 }
