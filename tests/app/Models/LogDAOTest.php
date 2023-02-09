696d69f48d63fcc8d36a2f809e0f0ee51a14c29a
Luc <sanchezluc+freshrss@gmail.com>
2023-02-09 12:16:04 +0100

    add test

--- tests/app/Models/LogDAOTest.php
+++ tests/app/Models/LogDAOTest.php
@@ -6,13 +6,16 @@ use PHPUnit\Framework\TestCase;
 
 class LogDAOTest extends TestCase {
 
+	private const PATH_TO_LOG = APP_PATH.'/log.txt';
 	private $logDAO;
 	protected function setUp(): void {
 		$this->logDAO = new FreshRSS_LogDAO();
+		file_put_contents(self::PATH_TO_LOG, '[Wed, 08 Feb 2023 15:35:05 +0000] [notice] --- Migration 2019_12_22_FooBar: OK');
 	}
 
 	public function test_lines_is_array(): void {
 		$this->assertIsArray($this->logDAO::lines());
 		$this->assertInstanceOf(FreshRSS_Log::class, $this->logDAO::lines()[0]);
 	}
+
 }
