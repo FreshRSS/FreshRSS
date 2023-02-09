66b9c046855c1a00810e7580155a61e878a79ca2
Luc <sanchezluc+freshrss@gmail.com>
2023-02-09 13:18:56 +0100

    add test

--- tests/app/Models/LogDAOTest.php
+++ tests/app/Models/LogDAOTest.php
@@ -8,19 +8,16 @@ class LogDAOTest extends TestCase {
 	private $logDAO;
 	protected function setUp(): void {
 		$this->logDAO = new FreshRSS_LogDAO();
-		$migrations_path = TESTS_PATH . '/fixtures/migrations/';
-		new Minz_Migrator($migrations_path);
+		file_put_contents(ADMIN_LOG, '[Wed, 08 Feb 2023 15:35:05 +0000] [notice] --- Migration 2019_12_22_FooBar: OK');
 	}
 
 	public function test_lines_is_array(): void {
-		dump($this->logDAO::lines());
 		$this->assertIsArray($this->logDAO::lines());
-		$this->assertInstanceOf(FreshRSS_Log::class, $this->logDAO::lines()[0]);
-	}
-
-	protected function tearDown(): void
-	{
-		$this->logDAO::truncate();
+		$line =  $this->logDAO::lines()[0];
+		$this->assertInstanceOf(FreshRSS_Log::class, $line);
+		$this->assertEquals('Wed, 08 Feb 2023 15:35:05 +0000', $line->date());
+		$this->assertEquals('notice', $line->level());
+		$this->assertEquals("Migration 2019_12_22_FooBar: OK", $line->info());
 	}
 
 }
