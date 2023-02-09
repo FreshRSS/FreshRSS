65d97539e0d2b84f68a4df759ed0abad62e1802b
Luc <sanchezluc+freshrss@gmail.com>
2023-02-09 13:20:25 +0100

    add test

--- tests/app/Models/LogDAOTest.php
+++ tests/app/Models/LogDAOTest.php
@@ -13,7 +13,7 @@ class LogDAOTest extends TestCase {
 
 	public function test_lines_is_array(): void {
 		$this->assertIsArray($this->logDAO::lines());
-		$line =  $this->logDAO::lines()[0];
+		$line = $this->logDAO::lines()[0];
 		$this->assertInstanceOf(FreshRSS_Log::class, $line);
 		$this->assertEquals('Wed, 08 Feb 2023 15:35:05 +0000', $line->date());
 		$this->assertEquals('notice', $line->level());
