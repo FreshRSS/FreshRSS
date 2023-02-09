4c05434c4ff247b5b0adc25ee19fe29d43aacdcb
Luc <sanchezluc+freshrss@gmail.com>
2023-02-09 12:51:13 +0100

    add test

--- tests/app/Models/LogDAOTest.php
+++ tests/app/Models/LogDAOTest.php
@@ -5,17 +5,22 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
 class LogDAOTest extends TestCase {
-
-	private const PATH_TO_LOG = APP_PATH.'/log.txt';
 	private $logDAO;
 	protected function setUp(): void {
 		$this->logDAO = new FreshRSS_LogDAO();
-		file_put_contents(self::PATH_TO_LOG, '[Wed, 08 Feb 2023 15:35:05 +0000] [notice] --- Migration 2019_12_22_FooBar: OK');
+		$migrations_path = TESTS_PATH . '/fixtures/migrations/';
+		new Minz_Migrator($migrations_path);
 	}
 
 	public function test_lines_is_array(): void {
+		dump($this->logDAO::lines());
 		$this->assertIsArray($this->logDAO::lines());
 		$this->assertInstanceOf(FreshRSS_Log::class, $this->logDAO::lines()[0]);
+	}
+
+	protected function tearDown(): void
+	{
+		$this->logDAO::truncate();
 	}
 
 }
