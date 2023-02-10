d14bbddb851bc7ad136098cd421d11dc347accd7
Luc <sanchezluc+freshrss@gmail.com>
2023-02-10 11:53:41 +0100

    use specific log file for test

--- app/Models/LogDAO.php
+++ app/Models/LogDAO.php
@@ -4,9 +4,9 @@ class FreshRSS_LogDAO {
 	/**
 	 * @return array<FreshRSS_Log>
 	 */
-	public static function lines(): array {
+	public static function lines(?string $logFileName = null): array {
 		$logs = [];
-		$handle = @fopen(self::currentUserLogPath(), 'rb');
+		$handle = @fopen(self::currentUserLogPath($logFileName), 'rb');
 		if ($handle) {
 			while (($line = fgets($handle)) !== false) {
 				if (preg_match('/^\[([^\[]+)\] \[([^\[]+)\] --- (.*)$/', $line, $matches)) {
@@ -22,8 +22,8 @@ class FreshRSS_LogDAO {
 		return array_reverse($logs);
 	}
 
-	public static function truncate() : void {
-		file_put_contents(self::currentUserLogPath(), '');
+	public static function truncate(?string $logFileName = null) : void {
+		file_put_contents(self::currentUserLogPath($logFileName), '');
 		if (FreshRSS_Auth::hasAccess('admin')) {
 			file_put_contents(ADMIN_LOG, '');
 			file_put_contents(API_LOG, '');
@@ -31,7 +31,11 @@ class FreshRSS_LogDAO {
 		}
 	}
 
-	private static function currentUserLogPath(): string {
-		return DATA_PATH . '/users/' . Minz_Session::param('currentUser', '_') . '/' . LOG_FILENAME;
+	private static function currentUserLogPath(?string $logFileName = null): string {
+
+		if (is_null($logFileName)) {
+			$logFileName = LOG_FILENAME;
+		}
+		return USERS_PATH . '/' . Minz_Session::param('currentUser', '_') . '/' . $logFileName;
 	}
 }
--- tests/app/Models/LogDAOTest.php
+++ tests/app/Models/LogDAOTest.php
@@ -5,23 +5,31 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
 class LogDAOTest extends TestCase {
+	private const LOG_FILE_TEST = 'logFileTest.txt';
 	private $logDAO;
 	protected function setUp(): void {
 		$this->logDAO = new FreshRSS_LogDAO();
-		file_put_contents(ADMIN_LOG, '[Wed, 08 Feb 2023 15:35:05 +0000] [notice] --- Migration 2019_12_22_FooBar: OK');
+		file_put_contents(
+			USERS_PATH . '/_/'  . self::LOG_FILE_TEST,
+			'[Wed, 08 Feb 2023 15:35:05 +0000] [notice] --- Migration 2019_12_22_FooBar: OK'
+		);
 	}
 
-	public function test_lines_is_array(): void {
-		$this->assertIsArray($this->logDAO::lines());
-		$line = $this->logDAO::lines()[0];
-		$this->assertInstanceOf(FreshRSS_Log::class, $line);
-		$this->assertEquals('Wed, 08 Feb 2023 15:35:05 +0000', $line->date());
-		$this->assertEquals('notice', $line->level());
-		$this->assertEquals("Migration 2019_12_22_FooBar: OK", $line->info());
+	public function test_lines_is_array_and_truncate_function_work(): void {
+		$line = $this->logDAO::lines(self::LOG_FILE_TEST);
+
+		$this->assertIsArray($line);
+		$this->assertInstanceOf(FreshRSS_Log::class, $line[0]);
+		$this->assertEquals('Wed, 08 Feb 2023 15:35:05 +0000', $line[0]->date());
+		$this->assertEquals('notice', $line[0]->level());
+		$this->assertEquals("Migration 2019_12_22_FooBar: OK", $line[0]->info());
+
+		$this->logDAO::truncate( self::LOG_FILE_TEST);
+
+		$this->assertStringContainsString('', file_get_contents(USERS_PATH . '/_/'  . self::LOG_FILE_TEST));
 	}
 
 	protected function tearDown(): void {
-		$this->logDAO::truncate();
-		unlink(ADMIN_LOG);
+		unlink(USERS_PATH . '/_/'  . self::LOG_FILE_TEST);
 	}
 }
