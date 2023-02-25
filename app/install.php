d4c6a421f875d58798878c86fad61318c842014c
mathContao <math-home@web.de>
2023-02-25 18:49:57 +0100

    improve buttons in goup-controls

--- app/install.php
+++ app/install.php
@@ -436,8 +436,8 @@ function printStep1() {
 		<div class="group-controls">
 			<form action="index.php?step=1" method="post">
 				<input type="hidden" name="freshrss-keep-install" value="1" />
-				<button type="submit" class="btn btn-important next-step" tabindex="1" ><?= _t('install.action.keep_install') ?></button>
-				<a class="btn btn-attention next-step confirm" data-str-confirm="<?= _t('install.js.confirm_reinstall') ?>"
+				<button type="submit" class="btn btn-important" tabindex="1" ><?= _t('install.action.keep_install') ?></button>
+				<a class="btn btn-attention confirm" data-str-confirm="<?= _t('install.js.confirm_reinstall') ?>"
 					href="?step=2" tabindex="2" ><?= _t('install.action.reinstall') ?></a>
 			</form>
 		</div>
@@ -446,7 +446,7 @@ function printStep1() {
 	<?php } elseif ($res['all'] == 'ok') { ?>
 	<div class="form-group form-actions">
 		<div class="group-controls">
-			<a class="btn btn-important next-step" href="?step=2" tabindex="1" ><?= _t('install.action.next_step') ?></a>
+			<a class="btn btn-important" href="?step=2" tabindex="1" ><?= _t('install.action.next_step') ?></a>
 		</div>
 	</div>
 	<?php } else { ?>
@@ -629,7 +629,7 @@ function printStep4() {
 	<p class="alert alert-success"><span class="alert-head"><?= _t('install.congratulations') ?></span> <?= _t('install.ok') ?></p>
 	<div class="form-group form-actions">
 		<div class="group-controls">
-			<a class="btn btn-important next-step" href="?step=5" tabindex="1"><?= _t('install.action.finish') ?></a>
+			<a class="btn btn-important" href="?step=5" tabindex="1"><?= _t('install.action.finish') ?></a>
 		</div>
 	</div>
 <?php
