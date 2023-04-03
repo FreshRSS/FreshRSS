FreshRSS/FreshRSS

commit 288ed04ccc30b58373576dc3be811aee43e67034

PHPStan level 6 for all PDO and Exception classes (#5239)
* PHPStan level 6 for all PDO and Exception classes
Contributes to https://github.com/FreshRSS/FreshRSS/issues/4112

* Fix type

* Now also our remaining own librairies

* Motivation for a few more files

* A few more DAO classes

* Last interface

app/Controllers/feedController.php
@@ -47,10 +47,11 @@ class FreshRSS_feed_Controller extends FreshRSS_ActionController {
 		$url = trim($url);
 
 		/** @var string|null $url */
-		$url = Minz_ExtensionManager::callHook('check_url_before_add', $url);
-		if (null === $url) {
+		$urlHooked = Minz_ExtensionManager::callHook('check_url_before_add', $url);
+		if ($urlHooked === $url) {
 			throw new FreshRSS_FeedNotAdded_Exception($url);
 		}
+		$url = $urlHooked;
 
 		$cat = null;
 		if ($cat_id > 0) {

app/Controllers/tagController.php
@@ -126,7 +126,8 @@ class FreshRSS_tag_Controller extends FreshRSS_ActionController {
 		$sourceId = Minz_Request::param('id_tag');
 
 		if ($targetName == '' || $sourceId == '') {
-			return Minz_Error::error(400);
+			Minz_Error::error(400);
+			return;
 		}
 
 		$tagDAO = FreshRSS_Factory::createTagDao();

app/Controllers/userController.php
@@ -431,11 +431,13 @@ class FreshRSS_user_Controller extends FreshRSS_ActionController {
 		} elseif (FreshRSS_Auth::hasAccess()) {
 			$user_config = FreshRSS_Context::$user_conf;
 		} else {
-			return Minz_Error::error(403);
+			Minz_Error::error(403);
+			return;
 		}
 
 		if (!FreshRSS_UserDAO::exists($username) || $user_config === null) {
-			return Minz_Error::error(404);
+			Minz_Error::error(404);
+			return;
 		}
 
 		if ($user_config->email_validation_token === '') {

app/Exceptions/AlreadySubscribedException.php
@@ -1,14 +1,16 @@
 <?php
 
 class FreshRSS_AlreadySubscribed_Exception extends Exception {
+
+	/** @var string */
 	private $feedName = '';
 
-	public function __construct($url, $feedName) {
+	public function __construct(string $url, string $feedName) {
 		parent::__construct('Already subscribed! ' . $url, 2135);
 		$this->feedName = $feedName;
 	}
 
-	public function feedName() {
+	public function feedName(): string {
 		return $this->feedName;
 	}
 }

app/Exceptions/BadUrlException.php
@@ -2,7 +2,7 @@
 
 class FreshRSS_BadUrl_Exception extends FreshRSS_Feed_Exception {
 
-	public function __construct($url) {
+	public function __construct(string $url) {
 		parent::__construct('`' . $url . '` is not a valid URL');
 	}
 

app/Exceptions/FeedNotAddedException.php
@@ -1,14 +1,16 @@
 <?php
 
 class FreshRSS_FeedNotAdded_Exception extends Exception {
+
+	/** @var string */
 	private $url = '';
 
-	public function __construct($url) {
+	public function __construct(string $url) {
 		parent::__construct('Feed not added! ' . $url, 2147);
 		$this->url = $url;
 	}
 
-	public function url() {
+	public function url(): string {
 		return $this->url;
 	}
 }

app/Exceptions/ZipException.php
@@ -1,14 +1,16 @@
 <?php
 
 class FreshRSS_Zip_Exception extends Exception {
+
+	/** @var int */
 	private $zipErrorCode = 0;
 
-	public function __construct($zipErrorCode) {
+	public function __construct(int $zipErrorCode) {
 		parent::__construct('ZIP error!', 2141);
 		$this->zipErrorCode = $zipErrorCode;
 	}
 
-	public function zipErrorCode() {
+	public function zipErrorCode(): int {
 		return $this->zipErrorCode;
 	}
 }

app/Mailers/UserMailer.php
@@ -10,7 +10,7 @@ class FreshRSS_User_Mailer extends Minz_Mailer {
 	 */
 	protected $view;
 
-	public function send_email_need_validation($username, $user_config) {
+	public function send_email_need_validation(string $username, FreshRSS_UserConfiguration $user_config): bool {
 		Minz_Translate::reset($user_config->language);
 
 		$this->view->_path('user_mailer/email_need_validation.txt.php');

commit 3ff563e43aa543472b88dcd65d76655adcc2e659

Fix addFeed Fix https://github.com/FreshRSS/FreshRSS/issues/5252 Regression from https://github.com/FreshRSS/FreshRSS/pull/5239

app/Controllers/feedController.php
@@ -48,7 +48,7 @@ class FreshRSS_feed_Controller extends FreshRSS_ActionController {
 
 		/** @var string|null $url */
 		$urlHooked = Minz_ExtensionManager::callHook('check_url_before_add', $url);
-		if ($urlHooked === $url) {
+		if ($urlHooked === null) {
 			throw new FreshRSS_FeedNotAdded_Exception($url);
 		}
 		$url = $urlHooked;