0859e0e6a1b17817e81a1643425d9dc5894f1425
Nob Shen <66322601+NobShen@users.noreply.github.com>
2023-04-05 18:29:10 -0700

    Update add-feeds.php
    Replace spaces with tabs.

--- cli/add-feeds.php
+++ cli/add-feeds.php
@@ -26,45 +26,45 @@ if (!is_readable($filename)) {
 	fail('FreshRSS error: file is not readable “' . $filename . '”');
 }
 
-echo '[jv] FreshRSS importing OPML for user “', $username, "”…\n";
+echo 'FreshRSS importing $filename for user “', $username, "”…\n";
 
 // [jv] so we're able to read the file
 // [jv] we'll add each feed
 
 $addFeedController = new FreshRSS_feed_Controller();
 $attributes = array(
-    'ssl_verify' => null,
-    'timeout' => null,
-    'curl_params' => null,
+	'ssl_verify' => null,
+	'timeout' => null,
+	'curl_params' => null,
 );
 
 $fp = @fopen($filename, "r");
 $ok = true;
 if ($fp) {
-    while (($url = fgets($fp, 4096)) !== false) {
-        echo $url;
-        try {
-            $feed = $addFeedController->addFeed($url, $title = '', $cat_id = 0, $new_cat_name = '', $http_auth = '', $attributes = array(), $kind = FreshRSS_Feed::KIND_RSS);
-        } catch (FreshRSS_BadUrl_Exception $e) {
-            // Given url was not a valid url!
-            Minz_Log::warning($e->getMessage());
-        } catch (FreshRSS_Feed_Exception $e) {
-            // Something went bad (timeout, server not found, etc.)
-            Minz_Log::warning($e->getMessage());
-        } catch (Minz_FileNotExistException $e) {
-            // Cache directory doesn’t exist!
-            Minz_Log::error($e->getMessage());
-        } catch (FreshRSS_AlreadySubscribed_Exception $e) {
-            Minz_Log::error($e->getMessage());
-        } catch (FreshRSS_FeedNotAdded_Exception $e) {
-            Minz_Log::error($e->getMessage());
-        }
-    }
-    if (!feof($fp)) {
-        echo "Error: unexpected fgets() fail\n";
-        $ok = false;
-    }
-    fclose($fp);
+	while (($url = fgets($fp, 4096)) !== false) {
+		echo $url;
+		try {
+			$feed = $addFeedController->addFeed($url, $title = '', $cat_id = 0, $new_cat_name = '', $http_auth = '', $attributes = array(), $kind = FreshRSS_Feed::KIND_RSS);
+		} catch (FreshRSS_BadUrl_Exception $e) {
+			// Given url was not a valid url!
+			Minz_Log::warning($e->getMessage());
+		} catch (FreshRSS_Feed_Exception $e) {
+			// Something went bad (timeout, server not found, etc.)
+			Minz_Log::warning($e->getMessage());
+		} catch (Minz_FileNotExistException $e) {
+			// Cache directory doesn’t exist!
+			Minz_Log::error($e->getMessage());
+		} catch (FreshRSS_AlreadySubscribed_Exception $e) {
+			Minz_Log::error($e->getMessage());
+		} catch (FreshRSS_FeedNotAdded_Exception $e) {
+			Minz_Log::error($e->getMessage());
+		}
+	}
+	if (!feof($fp)) {
+		echo "Error: unexpected fgets() fail\n";
+		$ok = false;
+	}
+	fclose($fp);
 }
 // invalidateHttpCache($username);
         
