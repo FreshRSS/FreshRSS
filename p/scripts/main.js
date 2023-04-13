2300fa00f383d023172a856ca9e06aa4df045ffa
mathContao <math-home@web.de>
2023-04-13 13:37:09 +0200

    share to clipboard improved with animation and icon

--- p/scripts/main.js
+++ p/scripts/main.js
@@ -1089,6 +1089,9 @@ function init_stream(stream) {
 		el = ev.target.closest('.item.share > button[data-type="clipboard"]');
 		if (el && navigator.clipboard) {	// Clipboard
 			navigator.clipboard.writeText(el.dataset.url);
+			el.classList.remove('ok');
+			void el.offsetWidth; // it does nothwing, but it is needed. See hack: https://css-tricks.com/restart-css-animation/
+			el.classList.add('ok');
 			return false;
 		}
 
