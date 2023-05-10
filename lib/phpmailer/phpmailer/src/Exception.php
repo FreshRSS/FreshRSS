18cd73d4fdecb8667400038d96fd6bfddb75fec3
Luc <sanchezluc+freshrss@gmail.com>
2023-05-10 13:43:41 +0200

    Update PHPMailer

--- lib/phpmailer/phpmailer/src/Exception.php
+++ lib/phpmailer/phpmailer/src/Exception.php
@@ -28,13 +28,13 @@ namespace PHPMailer\PHPMailer;
  */
 class Exception extends \Exception
 {
-    /**
-     * Prettify error message output.
-     *
-     * @return string
-     */
-    public function errorMessage()
-    {
-        return '<strong>' . htmlspecialchars($this->getMessage(), ENT_COMPAT | ENT_HTML401) . "</strong><br />\n";
-    }
+	/**
+	 * Prettify error message output.
+	 *
+	 * @return string
+	 */
+	public function errorMessage()
+	{
+		return '<strong>' . htmlspecialchars($this->getMessage(), ENT_COMPAT | ENT_HTML401) . "</strong><br />\n";
+	}
 }
