FreshRSS/FreshRSS

commit d23d10bcde1a9b86c784d58b891f61e740e0124e

Phpstan Level6 for View.php (#5269)
* Remarque's from Alkarex

* indentation

* indentation

* Apply suggestions from code review

Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

* Remarque's from Alkarex

* A few improvements

* Remarque's from Alkarex

* Remarque's from Alkarex

* Remarque's from Alkarex

* Remarque's from Alkarex

* Fixes and improvments

* Fix getTagsForEntry

---------

Co-authored-by: Luc <sanchezluc+freshrss@gmail.com>
Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

app/views/index/logs.phtml
@@ -14,7 +14,7 @@
 	?>
 
 	<?php if (!empty($items)) { ?>
-	<?php $this->logsPaginator->render('logs_pagination.phtml', 'page'); ?>
+	<?php $this->logsPaginator->render('logs_pagination.phtml', 0); ?>
 	<div id="loglist-wrapper" class="table-wrapper">
 		<table id="loglist">
 			<thead>
@@ -41,7 +41,7 @@
 		</tbody>
 		</table>
 	</div>
-	<?php $this->logsPaginator->render('logs_pagination.phtml', 'page'); ?>
+	<?php $this->logsPaginator->render('logs_pagination.phtml', 0); ?>
 
 
 

app/Controllers/authController.php
@@ -111,7 +111,7 @@ class FreshRSS_auth_Controller extends FreshRSS_ActionController {
 		FreshRSS_View::appendScript(Minz_Url::display('/scripts/bcrypt.min.js?' . @filemtime(PUBLIC_PATH . '/scripts/bcrypt.min.js')));
 
 		$limits = FreshRSS_Context::$system_conf->limits;
-		$this->view->cookie_days = round($limits['cookie_duration'] / 86400, 1);
+		$this->view->cookie_days = (int)round($limits['cookie_duration'] / 86400, 1);
 
 		$isPOST = Minz_Request::isPost() && !Minz_Session::param('POST_to_GET');
 		Minz_Session::_param('POST_to_GET');

app/Controllers/entryController.php
@@ -59,8 +59,8 @@ class FreshRSS_entry_Controller extends FreshRSS_ActionController {
 			FreshRSS_Context::$state = 0;
 		}
 
-		$params = array();
-		$this->view->tags = array();
+		$params = [];
+		$this->view->tagsForEntries = [];
 
 		$entryDAO = FreshRSS_Factory::createEntryDao();
 		if ($id == false) {
@@ -112,7 +112,7 @@ class FreshRSS_entry_Controller extends FreshRSS_ActionController {
 			foreach ($tagsForEntries as $line) {
 				$tags['t_' . $line['id_tag']][] = $line['id_entry'];
 			}
-			$this->view->tags = $tags;
+			$this->view->tagsForEntries = $tags;
 		}
 
 		if (!$this->ajax) {

app/Controllers/subscriptionController.php
@@ -48,7 +48,7 @@ class FreshRSS_subscription_Controller extends FreshRSS_ActionController {
 		FreshRSS_View::appendScript(Minz_Url::display('/scripts/feed.js?' . @filemtime(PUBLIC_PATH . '/scripts/feed.js')));
 		FreshRSS_View::prependTitle(_t('sub.title') . ' · ');
 
-		$this->view->onlyFeedsWithError = Minz_Request::paramTernary('error');
+		$this->view->onlyFeedsWithError = Minz_Request::paramBoolean('error');
 
 		$id = Minz_Request::paramInt('id');
 		$this->view->displaySlider = false;

app/Controllers/tagController.php
@@ -85,9 +85,9 @@ class FreshRSS_tag_Controller extends FreshRSS_ActionController {
 		$this->view->_layout(false);
 		header('Content-Type: application/json; charset=UTF-8');
 		header('Cache-Control: private, no-cache, no-store, must-revalidate');
-		$id_entry = Minz_Request::paramInt('id_entry');
+		$id_entry = Minz_Request::paramString('id_entry');
 		$tagDAO = FreshRSS_Factory::createTagDao();
-		$this->view->tags = $tagDAO->getTagsForEntry($id_entry);
+		$this->view->tagsForEntry = $tagDAO->getTagsForEntry($id_entry);
 	}
 
 	public function addAction(): void {

app/Controllers/userController.php
@@ -629,7 +629,8 @@ class FreshRSS_user_Controller extends FreshRSS_ActionController {
 		$this->view->details = $this->retrieveUserDetails($username);
 	}
 
-	private function retrieveUserDetails($username) {
+	/** @return array<string,int|string|bool> */
+	private function retrieveUserDetails($username): array {
 		$feedDAO = FreshRSS_Factory::createFeedDao($username);
 		$entryDAO = FreshRSS_Factory::createEntryDao($username);
 		$databaseDAO = FreshRSS_Factory::createDatabaseDAO($username);

app/Models/CategoryDAO.php
@@ -281,7 +281,8 @@ SQL;
 		return $categories;
 	}
 
-	public function listCategories($prePopulateFeeds = true, $details = false) {
+	/** @return array<FreshRSS_Category>|false */
+	public function listCategories(bool $prePopulateFeeds = true, bool $details = false) {
 		if ($prePopulateFeeds) {
 			$sql = 'SELECT c.id AS c_id, c.name AS c_name, c.kind AS c_kind, c.`lastUpdate` AS c_last_update, c.error AS c_error, c.attributes AS c_attributes, '
 				. ($details ? 'f.* ' : 'f.id, f.name, f.url, f.website, f.priority, f.error, f.`cache_nbEntries`, f.`cache_nbUnreads`, f.ttl ')
@@ -435,13 +436,12 @@ SQL;
 		return $n;
 	}
 
-	public static function daoToCategoryPrepopulated($listDAO) {
+	/**
+	 * @param array<string,mixed> $listDAO
+	 * @return array<int,FreshRSS_Category>
+	 */
+	private static function daoToCategoryPrepopulated(array $listDAO) {
 		$list = array();
-
-		if (!is_array($listDAO)) {
-			$listDAO = array($listDAO);
-		}
-
 		$previousLine = null;
 		$feedsDao = array();
 		$feedDao = FreshRSS_Factory::createFeedDAO();
@@ -481,7 +481,7 @@ SQL;
 		return $list;
 	}
 
-	public static function daoToCategory($listDAO) {
+	private static function daoToCategory($listDAO) {
 		$list = array();
 
 		if (!is_array($listDAO)) {

app/Models/Entry.php
@@ -48,7 +48,8 @@ class FreshRSS_Entry extends Minz_Model {
 	 */
 	private $feed;
 
-	private $tags;
+	/** @var array<string> */
+	private $tags = [];
 	private $attributes = [];
 
 	public function __construct(int $feedId = 0, string $guid = '', string $title = '', string $authors = '', string $content = '',
@@ -347,7 +348,8 @@ HTML;
 		return $this->feedId;
 	}
 
-	public function tags($asString = false) {
+	/** @return string|array<string> */
+	public function tags(bool $asString = false) {
 		if ($asString) {
 			return $this->tags == null ? '' : '#' . implode(' #', $this->tags);
 		} else {

commit c8d2ead7635e58a5c78d95a9b9a1a74e99a1bcef

Fix logs pagination (#5403)
* Fix logs pagination
Regression from https://github.com/FreshRSS/FreshRSS/pull/5269

* Add better default

app/views/index/logs.phtml
@@ -14,7 +14,7 @@
 	?>
 
 	<?php if (!empty($items)) { ?>
-	<?php $this->logsPaginator->render('logs_pagination.phtml'); ?>
+	<?php $this->logsPaginator->render('logs_pagination.phtml', 'page'); ?>
 	<div id="loglist-wrapper" class="table-wrapper">
 		<table id="loglist">
 			<thead>
@@ -41,7 +41,7 @@
 		</tbody>
 		</table>
 	</div>
-	<?php $this->logsPaginator->render('logs_pagination.phtml'); ?>
+	<?php $this->logsPaginator->render('logs_pagination.phtml', 'page'); ?>
 
 
 

lib/Minz/Paginator.php
@@ -49,7 +49,7 @@ class Minz_Paginator {
 	 * @param string $view nom du fichier de vue situé dans /app/views/helpers/
 	 * @param string $getteur variable de type $_GET[] permettant de retrouver la page
 	 */
-	public function render(string $view, string $getteur = '0'): void {
+	public function render(string $view, string $getteur = 'page'): void {
 		$view = APP_PATH . '/views/helpers/' . $view;
 
 		if (file_exists($view)) {