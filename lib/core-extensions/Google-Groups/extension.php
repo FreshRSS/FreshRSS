0d1276c7f7f0cd098a4ba6f01bff63f7c8aa0ab1
Alexandre Alapetite <alexandre@alapetite.fr>
2023-05-16 15:51:38 +0200

    Start PHPStan Level 8

--- lib/core-extensions/Google-Groups/extension.php
+++ lib/core-extensions/Google-Groups/extension.php
@@ -6,6 +6,6 @@ class GoogleGroupsExtension extends Minz_Extension {
 	}
 
 	public static function findFeed(string $url): string {
-		return preg_replace('%^(https?://groups.google.com/forum)/#!forum/(.+)$%i', '$1/feed/$2/msgs/rss.xml', $url);
+		return preg_replace('%^(https?://groups.google.com/forum)/#!forum/(.+)$%i', '$1/feed/$2/msgs/rss.xml', $url) ?? '';
 	}
 }
