FreshRSS/FreshRSS

commit 7f9594b8c7d7799f2e5f89328bd5981410db8cf0

fix many "Only booleans are allowed in an if condition" (#5501)
* fix many "Only booleans are allowed in an if condition"

* Update cli/create-user.php

Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

* Update cli/i18n/I18nUsageValidator.php

Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

* Fix several regressions and other minor things

* Fix another regression

* Update lib/http-conditional.php

Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

---------

Co-authored-by: Luc <sanchezluc+freshrss@gmail.com>
Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

cli/create-user.php
@@ -10,7 +10,7 @@ if (!FreshRSS_user_Controller::checkUsername($username)) {
 }
 
 $usernames = listUsers();
-if (preg_grep("/^$username$/i", $usernames)) {
+if (preg_grep("/^$username$/i", $usernames) !== false) {
 	fail('FreshRSS warning: username already exists “' . $username . '”', EXIT_CODE_ALREADY_EXISTS);
 }
 
@@ -31,7 +31,7 @@ if (!$ok) {
 if (!empty($options['api_password'])) {
 	$username = cliInitUser($username);
 	$error = FreshRSS_api_Controller::updatePassword($options['api_password']);
-	if ($error) {
+	if ($error !== false) {
 		fail($error);
 	}
 }

cli/delete-user.php
@@ -19,7 +19,7 @@ if (!FreshRSS_user_Controller::checkUsername($username)) {
 }
 
 $usernames = listUsers();
-if (!preg_grep("/^$username$/i", $usernames)) {
+if (preg_grep("/^$username$/i", $usernames) === false) {
 	fail('FreshRSS error: username not found “' . $username . '”');
 }
 

cli/i18n/I18nUsageValidator.php
@@ -42,7 +42,7 @@ class I18nUsageValidator implements I18nValidatorInterface {
 		foreach ($this->reference as $file => $data) {
 			foreach ($data as $key => $value) {
 				$this->totalEntries++;
-				if (preg_match('/\._$/', $key) && in_array(preg_replace('/\._$/', '', $key), $this->code, true)) {
+				if (preg_match('/\._$/', $key) === 1 && in_array(preg_replace('/\._$/', '', $key), $this->code, true)) {
 					continue;
 				}
 				if (!in_array($key, $this->code, true)) {

lib/Minz/ExtensionException.php
@@ -1,8 +1,8 @@
 <?php
 
 class Minz_ExtensionException extends Minz_Exception {
-	public function __construct(string $message, ?string $extension_name = null, int $code = self::ERROR) {
-		if ($extension_name) {
+	public function __construct(string $message, string $extension_name = '', int $code = self::ERROR) {
+		if ($extension_name !== '') {
 			$message = 'An error occurred in `' . $extension_name . '` extension with the message: ' . $message;
 		} else {
 			$message = 'An error occurred in an unnamed extension with the message: ' . $message;

lib/Minz/ExtensionManager.php
@@ -145,7 +145,7 @@ final class Minz_ExtensionManager {
 				continue;
 			}
 			$meta_raw_content = file_get_contents($metadata_filename) ?: '';
-			/** @var array{'name':string,'entrypoint':string,'path':string,'author'?:string,'description'?:string,'version'?:string,'type'?:'system'|'user'}|null */
+			/** @var array{'name':string,'entrypoint':string,'path':string,'author'?:string,'description'?:string,'version'?:string,'type'?:'system'|'user'}|null $meta_json */
 			$meta_json = json_decode($meta_raw_content, true);
 			if (!$meta_json || !self::isValidMetadata($meta_json)) {
 				// metadata.json is not a json file? Invalid!

lib/Minz/Log.php
@@ -81,12 +81,12 @@ class Minz_Log {
 		$maxSize = defined('MAX_LOG_SIZE') ? MAX_LOG_SIZE : 1048576;
 		if ($maxSize > 0 && @filesize($file_name) > $maxSize) {
 			$fp = fopen($file_name, 'c+');
-			if ($fp && flock($fp, LOCK_EX)) {
-				fseek($fp, -intval($maxSize / 2), SEEK_END);
+			if (is_resource($fp) && flock($fp, LOCK_EX)) {
+				fseek($fp, -(int)($maxSize / 2), SEEK_END);
 				$content = fread($fp, $maxSize);
 				rewind($fp);
 				ftruncate($fp, 0);
-				fwrite($fp, $content ? $content : '');
+				fwrite($fp, $content ?: '');
 				fwrite($fp, sprintf("[%s] [notice] --- Log rotate.\n", date('r')));
 				fflush($fp);
 				flock($fp, LOCK_UN);

lib/Minz/Migrator.php
@@ -70,9 +70,7 @@ class Minz_Migrator
 		}
 
 		$migrator = new self($migrations_path);
-		if ($applied_migrations) {
-			$migrator->setAppliedVersions($applied_migrations);
-		}
+		$migrator->setAppliedVersions($applied_migrations);
 		$results = $migrator->migrate();
 
 		foreach ($results as $migration => $result) {

lib/Minz/ModelPdo.php
@@ -69,7 +69,7 @@ class Minz_ModelPdo {
 				$this->pdo->setPrefix($db['prefix'] . $this->current_user . '_');
 				break;
 			case 'sqlite':
-				$dsn = 'sqlite:' . join_path(DATA_PATH, 'users', $this->current_user, 'db.sqlite');
+				$dsn = 'sqlite:' . DATA_PATH . '/users/' . $this->current_user . '/db.sqlite';
 				$this->pdo = new Minz_PdoSqlite($dsn . $dsnParams, $db['user'], $db['password'], $driver_options);
 				$this->pdo->setPrefix('');
 				break;

commit 4dc1b7c153e6f48be6c4c57df2a48b12d2efc8eb

fix: Fix check of existing usernames in cli scripts
`preg_grep` returns an empty array if the username matches no elements
from the usernames array.

Regression introduced in 7f9594b8c7d7799f2e5f89328bd5981410db8cf0

Reference: https://github.com/FreshRSS/FreshRSS/pull/5501

cli/create-user.php
@@ -10,7 +10,7 @@ if (!FreshRSS_user_Controller::checkUsername($username)) {
 }
 
 $usernames = listUsers();
-if (preg_grep("/^$username$/i", $usernames) !== false) {
+if (preg_grep("/^$username$/i", $usernames)) {
 	fail('FreshRSS warning: username already exists “' . $username . '”', EXIT_CODE_ALREADY_EXISTS);
 }
 

cli/delete-user.php
@@ -19,7 +19,7 @@ if (!FreshRSS_user_Controller::checkUsername($username)) {
 }
 
 $usernames = listUsers();
-if (preg_grep("/^$username$/i", $usernames) === false) {
+if (!preg_grep("/^$username$/i", $usernames)) {
 	fail('FreshRSS error: username not found “' . $username . '”');
 }
 