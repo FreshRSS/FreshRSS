FreshRSS/FreshRSS

commit 06d00995049db9c7b915f67cfd4a5708aace458f

Require PHP 7.4+ (#5720)
* Require PHP 7.4+
https://github.com/FreshRSS/FreshRSS/discussions/5474

* Update Docker oldest
Alpine 3.13 with PHP 7.4.26

* Add missing packets to Docker oldest

* Update to typed properties
https://php.net/migration74.new-features#migration74.new-features.core.typed-properties

* More types

app/Models/Feed.php
@@ -37,54 +37,32 @@ class FreshRSS_Feed extends Minz_Model {
 	public const ARCHIVING_RETENTION_COUNT_LIMIT = 10000;
 	public const ARCHIVING_RETENTION_PERIOD = 'P3M';
 
-	/** @var int */
-	private $id = 0;
-	/** @var string */
-	private $url = '';
-	/** @var int */
-	private $kind = 0;
-	/** @var int */
-	private $categoryId = 1;
-	/** @var FreshRSS_Category|null */
-	private $category;
-	/** @var int */
-	private $nbEntries = -1;
-	/** @var int */
-	private $nbNotRead = -1;
-	/** @var int */
-	private $nbPendingNotRead = 0;
-	/** @var string */
-	private $name = '';
-	/** @var string */
-	private $website = '';
-	/** @var string */
-	private $description = '';
-	/** @var int */
-	private $lastUpdate = 0;
-	/** @var int */
-	private $priority = self::PRIORITY_MAIN_STREAM;
-	/** @var string */
-	private $pathEntries = '';
-	/** @var string */
-	private $httpAuth = '';
-	/** @var bool */
-	private $error = false;
-	/** @var int */
-	private $ttl = self::TTL_DEFAULT;
+	private int $id = 0;
+	private string $url = '';
+	private int $kind = 0;
+	private int $categoryId = 1;
+	private ?FreshRSS_Category $category;
+	private int $nbEntries = -1;
+	private int $nbNotRead = -1;
+	private int $nbPendingNotRead = 0;
+	private string $name = '';
+	private string $website = '';
+	private string $description = '';
+	private int $lastUpdate = 0;
+	private int $priority = self::PRIORITY_MAIN_STREAM;
+	private string $pathEntries = '';
+	private string $httpAuth = '';
+	private bool $error = false;
+	private int $ttl = self::TTL_DEFAULT;
 	/** @var array<string,mixed> */
-	private $attributes = [];
-	/** @var bool */
-	private $mute = false;
-	/** @var string */
-	private $hash = '';
-	/** @var string */
-	private $lockPath = '';
-	/** @var string */
-	private $hubUrl = '';
-	/** @var string */
-	private $selfUrl = '';
+	private array $attributes = [];
+	private bool $mute = false;
+	private string $hash = '';
+	private string $lockPath = '';
+	private string $hubUrl = '';
+	private string $selfUrl = '';
 	/** @var array<FreshRSS_FilterAction> $filterActions */
-	private $filterActions = null;
+	private array $filterActions = [];
 
 	public function __construct(string $url, bool $validate = true) {
 		if ($validate) {

Docker/Dockerfile-Oldest
@@ -1,4 +1,4 @@
-FROM alpine:3.8
+FROM alpine:3.13
 
 ENV TZ UTC
 SHELL ["/bin/ash", "-eo", "pipefail", "-c"]
@@ -7,7 +7,7 @@ RUN apk add --no-cache \
 	tzdata \
 	apache2 php7-apache2 \
 	php7 php7-curl php7-gmp php7-intl php7-mbstring php7-xml php7-zip \
-	php7-ctype php7-dom php7-iconv php7-json php7-opcache php7-openssl php7-phar php7-session php7-xmlreader php7-xml php7-zlib \
+	php7-ctype php7-dom php7-iconv php7-json php7-opcache php7-openssl php7-phar php7-session php7-simplexml php7-xmlreader php7-xmlwriter php7-xml php7-tokenizer php7-zlib \
 	php7-pdo_sqlite php7-pdo_mysql php7-pdo_pgsql
 
 RUN mkdir -p /var/www/FreshRSS /run/apache2/

app/Controllers/entryController.php
@@ -7,9 +7,8 @@ class FreshRSS_entry_Controller extends FreshRSS_ActionController {
 
 	/**
 	 * JavaScript request or not.
-	 * @var bool
 	 */
-	private $ajax = false;
+	private bool $ajax = false;
 
 	/**
 	 * This action is called before every other action in that class. It is

app/Controllers/importExportController.php
@@ -5,11 +5,9 @@
  */
 class FreshRSS_importExport_Controller extends FreshRSS_ActionController {
 
-	/** @var FreshRSS_EntryDAO */
-	private $entryDAO;
+	private FreshRSS_EntryDAO $entryDAO;
 
-	/** @var FreshRSS_FeedDAO */
-	private $feedDAO;
+	private FreshRSS_FeedDAO $feedDAO;
 
 	/**
 	 * This action is called before every other action in that class. It is

app/Controllers/tagController.php
@@ -7,9 +7,8 @@ class FreshRSS_tag_Controller extends FreshRSS_ActionController {
 
 	/**
 	 * JavaScript request or not.
-	 * @var bool|mixed
 	 */
-	private $ajax = false;
+	private bool $ajax = false;
 
 	/**
 	 * This action is called before every other action in that class. It is

app/Exceptions/AlreadySubscribedException.php
@@ -2,8 +2,7 @@
 
 class FreshRSS_AlreadySubscribed_Exception extends Exception {
 
-	/** @var string */
-	private $feedName = '';
+	private string $feedName = '';
 
 	public function __construct(string $url, string $feedName) {
 		parent::__construct('Already subscribed! ' . $url, 2135);

app/Exceptions/FeedNotAddedException.php
@@ -2,8 +2,7 @@
 
 class FreshRSS_FeedNotAdded_Exception extends Exception {
 
-	/** @var string */
-	private $url = '';
+	private string $url = '';
 
 	public function __construct(string $url) {
 		parent::__construct('Feed not added! ' . $url, 2147);

app/Exceptions/ZipException.php
@@ -2,8 +2,7 @@
 
 class FreshRSS_Zip_Exception extends Exception {
 
-	/** @var int */
-	private $zipErrorCode = 0;
+	private int $zipErrorCode = 0;
 
 	public function __construct(int $zipErrorCode) {
 		parent::__construct('ZIP error!', 2141);

commit 5ca9101ce86ede176a555e037d13533556a09ab6

Fix type regression filterActions (#5786)
Regression from https://github.com/FreshRSS/FreshRSS/pull/5720

app/Models/Feed.php
@@ -61,8 +61,8 @@ class FreshRSS_Feed extends Minz_Model {
 	private string $lockPath = '';
 	private string $hubUrl = '';
 	private string $selfUrl = '';
-	/** @var array<FreshRSS_FilterAction> $filterActions */
-	private array $filterActions = [];
+	/** @var array<FreshRSS_FilterAction>|null $filterActions */
+	private ?array $filterActions = null;
 
 	public function __construct(string $url, bool $validate = true) {
 		if ($validate) {