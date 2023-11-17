FreshRSS/FreshRSS

commit 30c7a61a9b410f023c56ef19b9389a61647d8768

Use strict_types (#5830)
* Little's optimisations and booleans in conditions

* Apply strict type

* Apply strict type

* Apply strict type

* Fix multiple bugs with PHP 8.2 and 8.3

* Many declares missing, more errors fixed

* Apply strict type

* Another approach

* Stronger typing for Minz_Session

* Fix case of SQLite

---------

Co-authored-by: Luc <sanchezluc+freshrss@gmail.com>
Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

lib/Minz/Extension.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 /**
  * The extension base class.

commit 445e49db15ea7ae41dc55efea2d67903557f9182

Fix Minz_Extension::getVersion (#5891)
Regression from https://github.com/FreshRSS/FreshRSS/pull/5830

lib/Minz/Extension.php
@@ -56,7 +56,7 @@ abstract class Minz_Extension {
 		$this->path = $meta_info['path'];
 		$this->author = isset($meta_info['author']) ? $meta_info['author'] : '';
 		$this->description = isset($meta_info['description']) ? $meta_info['description'] : '';
-		$this->version = isset($meta_info['version']) ? $meta_info['version'] : '0.1';
+		$this->version = isset($meta_info['version']) ? (string)$meta_info['version'] : '0.1';
 		$this->setType(isset($meta_info['type']) ? $meta_info['type'] : 'user');
 
 		$this->is_enabled = false;