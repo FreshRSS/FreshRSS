FreshRSS/FreshRSS

commit 30c7a61a9b410f023c56ef19b9389a61647d8768

Use strict_types (#5830)
* Little's optimisations and booleans in conditions

* Apply strict type

* Apply strict type

* Apply strict type

* Fix multiple bugs with PHP 8.2 and 8.3

* Many declares missing, more errors fixed

* Apply strict type

* Another approach

* Stronger typing for Minz_Session

* Fix case of SQLite

---------

Co-authored-by: Luc <sanchezluc+freshrss@gmail.com>
Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

app/Models/Feed.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 class FreshRSS_Feed extends Minz_Model {
 

commit 803fa8fdb50c8b98efed2b89a7adf3eb6df189d9

Fix PHP7 susbtr (#5929)
fix https://github.com/FreshRSS/FreshRSS/issues/5928
`susbstr` might return false in PHP7
Regression from https://github.com/FreshRSS/FreshRSS/pull/5830

app/Models/Feed.php
@@ -580,7 +580,7 @@ class FreshRSS_Feed extends Minz_Model {
 					}
 				}
 			}
-			$authorNames = substr($authorNames, 0, -2);
+			$authorNames = substr($authorNames, 0, -2) ?: '';
 
 			$entry = new FreshRSS_Entry(
 				$this->id(),