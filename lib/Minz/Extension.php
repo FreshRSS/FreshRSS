FreshRSS/FreshRSS

commit 6d2e53178b6dc83845f4eaabde458c2c5b2d24fb

Typed Minz_Extension (#5957)
Replaces https://github.com/FreshRSS/FreshRSS/pull/5837
The signature of methods supposed to be overriden by existing thid-party extensions cannot be modified

lib/Minz/Extension.php
@@ -5,24 +5,18 @@ declare(strict_types=1);
  * The extension base class.
  */
 abstract class Minz_Extension {
-	/** @var string */
-	private $name;
-	/** @var string */
-	private $entrypoint;
-	/** @var string */
-	private $path;
-	/** @var string */
-	private $author;
-	/** @var string */
-	private $description;
-	/** @var string */
-	private $version;
+	private string $name;
+	private string $entrypoint;
+	private string $path;
+	private string $author;
+	private string $description;
+	private string $version;
 	/** @var 'system'|'user' */
-	private $type;
+	private string $type;
 	/** @var array<string,mixed>|null */
-	private $user_configuration;
+	private ?array $user_configuration;
 	/** @var array<string,mixed>|null */
-	private $system_configuration;
+	private ?array $system_configuration;
 
 	/** @var array{0:'system',1:'user'} */
 	public static array $authorized_types = [
@@ -30,8 +24,7 @@ abstract class Minz_Extension {
 		'user',
 	];
 
-	/** @var bool */
-	private $is_enabled;
+	private bool $is_enabled;
 
 	/**
 	 * The constructor to assign specific information to the extension.

commit ad990a21a136a28230ea87dd444eff5790af13fd

Fix extensions getUserConfiguration fix https://github.com/FreshRSS/FreshRSS/issues/5970 Regression from https://github.com/FreshRSS/FreshRSS/pull/5957

lib/Minz/Extension.php
@@ -14,9 +14,9 @@ abstract class Minz_Extension {
 	/** @var 'system'|'user' */
 	private string $type;
 	/** @var array<string,mixed>|null */
-	private ?array $user_configuration;
+	private ?array $user_configuration = null;
 	/** @var array<string,mixed>|null */
-	private ?array $system_configuration;
+	private ?array $system_configuration = null;
 
 	/** @var array{0:'system',1:'user'} */
 	public static array $authorized_types = [