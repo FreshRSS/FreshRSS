8beb02bd6ca508b87463cee0870abc7e9c101073
Kasimir Cash <kasimir.cash@outlook.com>
2024-01-04 12:23:22 +0000

    Updates update-user to use parseCliParams

--- cli/update-user.php
+++ cli/update-user.php
@@ -5,22 +5,22 @@ declare(strict_types=1);
 $isUpdate = true;
 require(__DIR__ . '/_update-or-create-user.php');
 
-$username = cliInitUser($GLOBALS['options']['user']);
+$username = cliInitUser($GLOBALS['options']['valid']['user']);
 
 echo 'FreshRSS updating user “', $username, "”…\n";
 
 $ok = FreshRSS_user_Controller::updateUser(
 	$username,
-	empty($options['email']) ? null : $options['email'],
-	empty($options['password']) ? '' : $options['password'],
+	empty($options['valid']['email']) ? null : $options['valid']['email'],
+	empty($options['valid']['password']) ? '' : $options['valid']['password'],
 	$GLOBALS['values']);
 
 if (!$ok) {
 	fail('FreshRSS could not update user!');
 }
 
-if (!empty($options['api_password'])) {
-	$error = FreshRSS_api_Controller::updatePassword($options['api_password']);
+if (!empty($options['valid']['api_password'])) {
+	$error = FreshRSS_api_Controller::updatePassword($options['valid']['api_password']);
 	if ($error) {
 		fail($error);
 	}
