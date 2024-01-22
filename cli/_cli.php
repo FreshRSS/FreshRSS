e363ec11f62537776ac865cf46048f7a5e5d066a
Kasimir Cash <kasimir.cash@outlook.com>
2024-01-22 14:42:14 +0000

    Adds logic for validation

--- cli/_cli.php
+++ cli/_cli.php
@@ -6,9 +6,11 @@ if (php_sapi_name() !== 'cli') {
 }
 
 const EXIT_CODE_ALREADY_EXISTS = 3;
-const REGEX_INPUT_OPTIONS = '/^-{2}|^-{1}/';
+const REGEX_INPUT_OPTIONS = "/^--(?'long'\w.+)|^-(?'short'\w+)/";
 
 require(__DIR__ . '/../constants.php');
+require_once __DIR__ . '/i18n/I18nData.php';
+require_once __DIR__ . '/i18n/I18nFile.php';
 require(LIB_PATH . '/lib_rss.php');	//Includes class autoloader
 require(LIB_PATH . '/lib_install.php');
 
@@ -76,44 +78,30 @@ function performRequirementCheck(string $databaseType): void {
 
 /**
  * Parses parameters used with FreshRSS' CLI commands.
- * @param array{'long':array<string,string>,'short':array<string,string>,'deprecated':array<string,string>} $parameters
- * Matrix of 'long': map of long option names as keys and their respective getopt() notations as values,
- * 'short': map of short option names as values and their equivalent long options as keys, 'deprecated': map of
- * replacement option names as keys and their respective deprecated option names as values.
- * @return array{'valid':array<string,string>,'invalid':array<string>} Matrix of 'valid': map of of all known
- * option names used and their respective values and 'invalid': list of all unknown options used.
+ * @param array<string,array{'getopt':string,'required':bool,'short':string,'deprecated':string,'read':callable,
+ * 'validators':array<callable>}> $parameters
+ * @return array{'valid':array<string,string>,'invalid':array<string,string>} Matrix of 'valid': map of of all known
+ * option names used and their respective values and 'invalid': map of all unknown options used and their respective
+ * error messages.
  */
 function parseCliParams(array $parameters): array {
 	global $argv;
-	$longOptions = [];
-	$shortOptions = '';
+	$getoptLongOptions = returnGetoptLongOptions($parameters);
+	$getoptShortOptions = returnGetoptShortOptions($parameters);
 
-	foreach ($parameters['long'] as $name => $getopt_note) {
-		$longOptions[] = $name . $getopt_note;
-	}
-	foreach ($parameters['deprecated'] as $name => $deprecatedName) {
-		$longOptions[] = $deprecatedName . $parameters['long'][$name];
-	}
-	foreach ($parameters['short'] as $name => $shortName) {
-		$shortOptions .= $shortName . $parameters['long'][$name];
-	}
-
-	$options = getopt($shortOptions, $longOptions);
+	$options = getopt($getoptShortOptions, $getoptLongOptions);
 
 	$valid = is_array($options) ? $options : [];
 
 	array_walk($valid, static fn(&$option) => $option = $option === false ? '' : $option);
 
 	/** @var array<string,string> $valid */
-	checkForDeprecatedOptions(array_keys($valid), $parameters['deprecated']);
+	checkForDeprecatedOptions(array_keys($valid), returnMapOfStrings('deprecated', $parameters));
 
-	$valid = replaceOptions($valid, $parameters['short']);
-	$valid = replaceOptions($valid, $parameters['deprecated']);
+	$valid = replaceOptions($valid, returnMapOfStrings('short', $parameters));
+	$valid = replaceOptions($valid, returnMapOfStrings('deprecated', $parameters));
 
-	$invalid = findInvalidOptions(
-		$argv,
-		array_merge(array_keys($parameters['long']), array_values($parameters['short']), array_values($parameters['deprecated']))
-	);
+	$invalid = findInvalidOptions($argv, returnAllOptions($parameters));
 
 	return [
 		'valid' => $valid,
@@ -126,19 +114,27 @@ function parseCliParams(array $parameters): array {
  * @return array<string>
  */
 function getOptions(array $options, string $regex): array {
-	$longOptions = array_filter($options, static function (string $a) use ($regex) {
-		return preg_match($regex, $a) === 1;
-	});
-	return array_map(static function (string $a) use ($regex) {
-		return preg_replace($regex, '', $a) ?? '';
-	}, $longOptions);
+	$foundOptions = [];
+
+	foreach ($options as $option) {
+		preg_match($regex, $option, $matches);
+
+		if(!empty($matches['long'])) {
+			$foundOptions[] = $matches['long'];
+		}
+		if(!empty($matches['short'])) {
+			$foundOptions = array_merge($foundOptions, str_split($matches['short']));
+		}
+	}
+
+	return $foundOptions;
 }
 
 /**
  * Checks for presence of unknown options.
  * @param array<string> $input List of command line arguments to check for validity.
  * @param array<string> $params List of valid options to check against.
- * @return array<string> Returns a list all unknown options found.
+ * @return array<string,string> Returns a list all unknown options found.
  */
 function findInvalidOptions(array $input, array $params): array {
 	$sanitizeInput = getOptions($input, REGEX_INPUT_OPTIONS);
@@ -148,8 +144,13 @@ function findInvalidOptions(array $input, array $params): array {
 		return [];
 	}
 
-	fwrite(STDERR, sprintf("FreshRSS error: unknown options: %s\n", implode (', ', $unknownOptions)));
-	return $unknownOptions;
+	$invalid = [];
+
+	foreach ($unknownOptions as $unknownOption) {
+		$invalid[$unknownOption] = 'unknown option: ' . $unknownOption;
+	}
+
+	return $invalid;
 }
 
 /**
@@ -168,8 +169,8 @@ function checkForDeprecatedOptions(array $optionNames, array $params): bool {
 	}
 
 	fwrite(STDERR, "FreshRSS deprecation warning: the CLI option(s): " . implode(', ', $deprecatedOptions) .
-		" are deprecated and will be removed in a future release. Use: "
-		. implode(', ', $replacements) . " instead\n");
+		" are deprecated and will be removed in a future release. Use: " . implode(', ', $replacements) .
+		" instead\n\n");
 	return true;
 }
 
@@ -177,7 +178,7 @@ function checkForDeprecatedOptions(array $optionNames, array $params): bool {
  * Switches items in a list to their provided replacements.
  * @param array<string,string> $options Map with items to check for replacement as keys.
  * @param array<string,string> $replacements Map of replacement items as keys and the item they replace as their values.
- * @return array<string,string>  Returns $options with replacements.
+ * @return array<string,string> Returns $options with replacements.
  */
 function replaceOptions(array $options, array $replacements): array {
 	$updatedOptions = [];
@@ -188,4 +189,171 @@ function replaceOptions(array $options, array $replacements): array {
 	}
 
 	return $updatedOptions;
+}
+
+/**
+ * @param array<string,string> $input
+ * @param array<string,array{'getopt':string,'required':bool,'short':string,'deprecated':string,'read':callable,
+ * 'validators':array<callable>}> $validations
+ * @return array{'valid':array<string,string>,'invalid':array<string,string>}
+ */
+function validateCliParams(array $input, array $validations): array {
+	$valid = [];
+	$invalid = [];
+
+	foreach ($input as $key => $value) {
+		$isValid = true;
+
+		if (!key_exists($key, $validations)) {
+			$invalid[$key] = 'unknown option: ' . $key;
+			$isValid = false;
+		}
+
+		foreach ($validations[$key]['validators'] ?? [] as $validator) {
+			if ($validator($key, $value)) {
+				$invalid[$key] = $validator($key, $value);
+				$isValid = false;
+			}
+		}
+
+		if ($isValid) {
+			$valid[$key] = $value;
+		}
+	}
+
+	foreach ($validations as $key => $checks) {
+		if ($checks['required'] && !key_exists($key, $input)) {
+			$invalid[$key] = $key . ' cannot be empty';
+		}
+	}
+
+	return [
+		'valid' => $valid,
+		'invalid' => $invalid,
+	];
+}
+
+/**
+ * @param array<string> $validValues
+ */
+function validateOneOf(array $validValues, string $errorMessageName, ?string $errorMessagePrompt = null): callable {
+	$errorMessagePrompt = $errorMessagePrompt ? $errorMessagePrompt : 'one of { ' . implode(', ', $validValues) . ' }';
+
+	return function (string $name, string $value) use ($validValues, $errorMessageName, $errorMessagePrompt): ?string {
+		return !in_array($value, $validValues, true)
+		? 'invalid ' . $errorMessageName . '. ' . $name . ' must be ' . $errorMessagePrompt
+		: null;
+	};
+}
+
+function validateRegex(string $regex, string $errorMessageName, string $errorMessagePrompt): callable {
+
+	return function (string $name, string $value) use ($regex, $errorMessageName, $errorMessagePrompt): ?string {
+		return preg_match('/^' . $regex . '$/', $value) !== 1
+			? 'invalid ' . $errorMessageName . '. ' . $name . ' must be ' . $errorMessagePrompt
+			: null;
+	};
+}
+
+/**
+ * @param array<string,array{'getopt':string,'required':bool,'short':string,'deprecated':string,'read':callable,
+ * 'validators':array<callable>}> $parameters
+ * @return array<string>
+ * */
+function returnGetoptLongOptions(array $parameters): array {
+	$output = [];
+
+	foreach ($parameters as $name => $data) {
+		$output[] = $name. $data['getopt'];
+		$output[] = $data['deprecated'] ?? 0 ? $data['deprecated'] . $data['getopt'] : '';
+	}
+
+	return array_filter($output);
+}
+
+/**
+ * @param array<string,array{'getopt':string,'required':bool,'short':string,'deprecated':string,'read':callable,
+ * 'validators':array<callable>}> $parameters
+ */
+function returnGetoptShortOptions(array $parameters): string {
+	$output = '';
+
+	foreach ($parameters as $data) {
+		$output .= $data['short'] ?? 0 ? $data['short'] . $data['getopt'] : '';
+	}
+
+	return $output;
+}
+
+/**
+ * @param array<string,array{'getopt':string,'required':bool,'short':string,'deprecated':string,'read':callable,
+ * 'validators':array<callable>}> $haystack
+ * @return array<string,string>
+ */
+function returnMapOfStrings(string $needle, array $haystack): array {
+	$output = [];
+
+	foreach ($haystack as $option => $data) {
+		$output[$option] = isset($data[$needle]) && is_string($data[$needle]) ? $data[$needle] : '';
+	}
+
+	return array_filter($output);
+}
+
+/**
+ * @param array<string,array{'getopt':string,'required':bool,'short':string,'deprecated':string,'read':callable,
+ * 'validators':array<callable>}> $parameters
+ * @return array<string>
+ */
+function returnAllOptions(array $parameters): array {
+	$output = [];
+
+	foreach ($parameters as $name => $data) {
+		$output[] = $name;
+		$output[] = $data['deprecated'] ?? 0 ? $data['deprecated'] : '';
+		$output[] = $data['short'] ?? 0 ? $data['short'] : '';
+	}
+
+	return array_filter($output);
+}
+
+/**
+ * Parses parameters used with FreshRSS' CLI commands.
+ * @param array<string,array{'getopt':string,'required':bool,'short':string,'deprecated':string,'read':callable,
+ * 'validators':array<callable>}> $parameters
+ * @return array{'valid':array<string,string>,'invalid':array<string,string>} Matrix of 'valid': map of of all known
+ * option names used and their respective values and 'invalid': map of all unknown options used and their respective
+ * error messages.
+ */
+function parseAndValidateCliParams(array $parameters): array {
+	$parsedParams = parseCliParams($parameters);
+
+	if(!empty($parsedParams['invalid'])) {
+		return $parsedParams;
+	}
+
+	return validateCliParams($parsedParams['valid'], $parameters);
+}
+
+function readAsString(): callable {
+
+	return function (string $name, array $options): ?string {
+		return isset($options[$name]) ? strval($options[$name]) : null;
+	};
+}
+
+function readAsInt(): callable {
+
+	return function (string $name, array $options): ?int {
+		return isset($options[$name]) && ctype_digit($options[$name]) ? intval($options[$name]) : null;
+	};
+}
+
+function readAsBool(): callable {
+
+	return function (string $name, array $options): ?bool {
+		return isset($options[$name]) && preg_match('/^true$|^false$/', $options[$name],)
+			? filter_var($options[$name], FILTER_VALIDATE_BOOL, FILTER_NULL_ON_FAILURE)
+			: null;
+	};
 }
