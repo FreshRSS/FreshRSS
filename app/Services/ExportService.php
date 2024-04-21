6187f89dd33174ad56d9728bfc24f282d0e7aae1
math-gh <1645099+math-GH@users.noreply.github.com>
2024-04-21 20:51:32 +0200

    TYPE_JSON_DOTNOTATION = 'JSON+DotPath' => 'JSON+DotNotation'

--- app/Services/ExportService.php
+++ app/Services/ExportService.php
@@ -20,7 +20,7 @@ class FreshRSS_Export_Service {
 	public const TYPE_HTML_XPATH = 'HTML+XPath';
 	public const TYPE_XML_XPATH = 'XML+XPath';
 	public const TYPE_RSS_ATOM = 'rss';
-	public const TYPE_JSON_DOTNOTATION = 'JSON+DotPath';
+	public const TYPE_JSON_DOTNOTATION = 'JSON+DotNotation';
 	public const TYPE_JSONFEED = 'JSONFeed';
 
 	/**
