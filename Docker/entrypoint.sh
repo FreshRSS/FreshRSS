f5932f6cd60d9dd9a518c397c2f370bb2a7c8581
нездалисько <105280814+crisukbot@users.noreply.github.com>
2024-07-20 14:35:52 +0300

    fix: entrypoint.sh OIDCSessionMaxDuration and OIDCSessionInactivityTimeout and Alpine support

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -23,10 +23,12 @@ fi
 
 if [ -n "$OIDC_ENABLED" ] && [ "$OIDC_ENABLED" -ne 0 ]; then
 	# Debian
-	(which a2enmod >/dev/null && a2enmod -q auth_openidc)
- 	(echo "OIDCSessionMaxDuration ${OIDC_SESS_MAX_DURATION:-2592000}" >> /etc/apache2/mods-available/auth_openidc.conf && echo "OIDCSessionInactivityTimeout ${OIDCSessionInactivityTimeout:-86400}" >> /etc/apache2/mods-available/auth_openidc.conf) ||
+	(which a2enmod >/dev/null && a2enmod -q auth_openidc) ||
 		# Alpine
 		(mv /etc/apache2/conf.d/mod-auth-openidc.conf.bak /etc/apache2/conf.d/mod-auth-openidc.conf && echo 'Enabling module auth_openidc.')
+	OIDC_CONF=$(test -e /etc/apache2/mods-available/auth_openidc.conf && echo /etc/apache2/mods-available/auth_openidc.conf || echo /etc/apache2/conf.d/mod-auth-openidc.conf)
+	echo "OIDCSessionMaxDuration ${OIDC_SESS_MAX_DURATION:-2592000}" >> "$OIDC_CONF"
+	echo "OIDCSessionInactivityTimeout ${OIDCSessionInactivityTimeout:-86400}" >> "$OIDC_CONF"
 	if [ -n "$OIDC_SCOPES" ]; then
 		# Compatibility with : as separator instead of space
 		OIDC_SCOPES=$(echo "$OIDC_SCOPES" | tr ':' ' ')
