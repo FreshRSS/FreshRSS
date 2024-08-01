8f1906b2db2fd650e76434ea887329c1aa734f97
Alexandre Alapetite <alexandre@alapetite.fr>
2024-08-02 01:22:07 +0200

    Migrate to ESLint 9 * https://eslint.org/docs/latest/use/migrate-to-9.0.0 * https://eslint.style/guide/migration * https://github.com/neostandard/neostandard/ (https://github.com/standard/standard/issues/1948)
    fix broken Dependabot PRs such as https://github.com/FreshRSS/FreshRSS/pull/6680

---
+++ eslint.config.mjs
@@ -0,0 +1,57 @@
+import globals from "globals";
+import js from "@eslint/js";
+import neostandard from 'neostandard';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default [
+	{
+		files: ["**/*.js"],
+		languageOptions: {
+			globals: globals.browser,
+			sourceType: "script",
+		},
+	},
+	{
+		ignores: [
+			".git/",
+			"**/*.min.js",
+			"extensions/",
+			"node_modules/",
+			"p/scripts/vendor/",
+			"vendor/",
+		],
+	},
+	js.configs.recommended,
+	...neostandard(),
+	{
+		plugins: {
+			"@stylistic": stylistic,
+		},
+		rules: {
+			"camelcase": "off",
+			"eqeqeq": "off",
+			"no-empty": ["error", { "allowEmptyCatch": true }],
+			"no-unused-vars": ["error", {
+				"argsIgnorePattern": "^e",
+				"caughtErrors": "none",
+			}],
+			"yoda": "off",
+			"@stylistic/comma-dangle": ["warn", {
+				"arrays": "always-multiline",
+				"objects": "always-multiline",
+			}],
+			"@stylistic/indent": ["warn", "tab", { "SwitchCase": 1 }],
+			"@stylistic/linebreak-style": ["error", "unix"],
+			"@stylistic/max-len": ["warn", 165],
+			"@stylistic/no-tabs": "off",
+			"@stylistic/quotes": ["off", "single", { "avoidEscape": true }],
+			"@stylistic/quote-props": ["warn", "consistent"],
+			"@stylistic/semi": ["warn", "always"],
+			"@stylistic/space-before-function-paren": ["warn", {
+				"anonymous": "always",
+				"asyncArrow": "always",
+				"named": "never",
+			}],
+		},
+	},
+];
