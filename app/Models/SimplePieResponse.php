59ed82d656110625c4ee03b3180c7b794f7e253a
Alexandre Alapetite <alexandre@alapetite.fr>
2024-08-17 23:08:10 +0200

    Fixes

--- app/Models/SimplePieResponse.php
+++ app/Models/SimplePieResponse.php
@@ -6,6 +6,6 @@ final class FreshRSS_SimplePieResponse extends \SimplePie\File
 	public function __construct(string $url, int $timeout = 10, int $redirects = 5, ?array $headers = null,
 		?string $useragent = null, bool $force_fsockopen = false, array $curl_options = []) {
 		parent::__construct($url, $timeout, $redirects, $headers, $useragent, $force_fsockopen, $curl_options);
-		syslog(LOG_INFO, 'SimplePie GET ' . $this->status_code . ' ' . \SimplePie\Misc::url_remove_credentials($url));
+		syslog(LOG_INFO, 'SimplePie GET ' . $this->get_status_code() . ' ' . \SimplePie\Misc::url_remove_credentials($url));
 	}
 }
