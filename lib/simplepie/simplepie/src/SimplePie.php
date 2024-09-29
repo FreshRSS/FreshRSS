a792c195d23390f4b23c95cca2071045915b0d5c
Alexandre Alapetite <alexandre@alapetite.fr>
2024-09-29 16:17:26 +0200

    Sync SimplePie (#6840)
    FreshRSS upstream PR merged https://github.com/simplepie/simplepie/pull/883

--- lib/simplepie/simplepie/src/SimplePie.php
+++ lib/simplepie/simplepie/src/SimplePie.php
@@ -1978,7 +1978,7 @@ class SimplePie
                     $this->data = [];
                 }
                 // Check if the cache has been updated
-                elseif (empty($this->data['cache_expiration_time']) || $this->data['cache_expiration_time'] < time()) { // FreshRSS https://github.com/simplepie/simplepie/pull/846
+                elseif (!isset($this->data['cache_expiration_time']) || $this->data['cache_expiration_time'] < time()) {
                     // Want to know if we tried to send last-modified and/or etag headers
                     // when requesting this file. (Note that it's up to the file to
                     // support this, but we don't always send the headers either.)
