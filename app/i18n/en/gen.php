3f0badd64f9cf8cee60e0d8b7ecbd95a1c90ef4d
math-gh <1645099+math-GH@users.noreply.github.com>
2024-09-30 00:09:33 +0200

    fix

--- app/i18n/en/gen.php
+++ app/i18n/en/gen.php
@@ -33,9 +33,9 @@ return array(
 			'open' => 'Open menu',
 		),
 		'nav_buttons' => array(
-			'next' => 'Nächster Artikel',
-			'prev' => 'Vorheriger Artikel',
-			'up' => 'Nach oben',
+			'next' => 'Next article',	// TODO
+			'prev' => 'Previous article',	// TODO
+			'up' => 'Go up',	// TODO
 		),
 		'open_url' => 'Open URL',
 		'promote' => 'Promote',
