77756edd99b24c2181652ecc1da0bbe9845fc811
Alexandre Alapetite <alexandre@alapetite.fr>
2024-10-06 11:49:53 +0200

    Typo

--- app/Models/Feed.php
+++ app/Models/Feed.php
@@ -503,7 +503,7 @@ class FreshRSS_Feed extends Minz_Model {
 				$unicityCriteria = 'link';
 			}
 
-			// Automatic fallback to next (degrated) unicity criteria
+			// Automatic fallback to next (degraded) unicity criteria
 			$newUnicityCriteria = match ($unicityCriteria) {
 				null => 'sha1:link_published',
 				'link' => 'sha1:link_published',
