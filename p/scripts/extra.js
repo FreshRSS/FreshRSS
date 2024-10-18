FreshRSS/FreshRSS

commit f5aaf5f46076e0ac11ab52ef2ea940fd789ff174

New limit option when reloading a feed (#4370)

p/scripts/extra.js
@@ -233,11 +233,18 @@ function init_select_observers() {
 	});
 }
 
-function data_leave_validation(parent) {
+/**
+ * Returns true when no input element is changed, false otherwise.
+ * When excludeForm is defined, will only report changes outside the specified form.
+ */
+function data_leave_validation(parent, excludeForm = null) {
 	const ds = parent.querySelectorAll('[data-leave-validation]');
 
 	for (let i = ds.length - 1; i >= 0; i--) {
 		const input = ds[i];
+		if (excludeForm && excludeForm === input.form) {
+			continue;
+		}
 		if (input.type === 'checkbox' || input.type === 'radio') {
 			if (input.checked != input.getAttribute('data-leave-validation')) {
 				return false;
@@ -251,7 +258,7 @@ function data_leave_validation(parent) {
 
 function init_configuration_alert() {
 	window.onsubmit = function (e) {
-		window.hasSubmit = true;
+		window.hasSubmit = data_leave_validation(document.body, e.submitter ? e.submitter.form : null);
 	};
 	window.onbeforeunload = function (e) {
 		if (window.hasSubmit) {

app/Controllers/feedController.php
@@ -814,7 +814,8 @@ class FreshRSS_feed_Controller extends FreshRSS_ActionController {
 		@set_time_limit(300);
 
 		//Get Feed ID.
-		$feed_id = Minz_Request::param('id');
+		$feed_id = intval(Minz_Request::param('id', 0));
+		$limit = intval(Minz_Request::param('reload_limit', 10));
 
 		$feedDAO = FreshRSS_Factory::createFeedDao();
 		$entryDAO = FreshRSS_Factory::createEntryDao();
@@ -831,8 +832,7 @@ class FreshRSS_feed_Controller extends FreshRSS_ActionController {
 		self::actualizeFeed($feed_id, '', false);
 
 		//Extract all feed entries from database, load complete content and store them back in database.
-		$entries = $entryDAO->listWhere('f', $feed_id, FreshRSS_Entry::STATE_ALL, 'DESC', 0);
-		//TODO: Parameter to limit the number of articles to reload
+		$entries = $entryDAO->listWhere('f', $feed_id, FreshRSS_Entry::STATE_ALL, 'DESC', $limit);
 
 		//We need another DB connection in parallel for unbuffered streaming
 		Minz_ModelPdo::$usesSharedPdo = false;

app/i18n/cz/sub.php
@@ -108,7 +108,7 @@ return array(
 			'clear_cache' => 'Vymazat mezipaměť',
 			'clear_cache_help' => 'Vymazat mezipaměť pro tento kanál.',
 			'reload_articles' => 'Znovu načíst články',
-			'reload_articles_help' => 'Znovu načíst články a získat úplný obsah, pokud je definován přepínač.',
+			'reload_articles_help' => 'Znovu načíst články a získat úplný obsah, pokud je definován přepínač.',	// DIRTY
 			'title' => 'Údržba',
 		),
 		'moved_category_deleted' => 'Když odstraníte kategorii, její kanály jsou automaticky přesunuty do <em>%s</em>.',

app/i18n/de/sub.php
@@ -108,7 +108,7 @@ return array(
 			'clear_cache' => 'Zwischenspeicher leeren',
 			'clear_cache_help' => 'Zwischenspeicher für diesen Feed leeren.',
 			'reload_articles' => 'Artikel neuladen',
-			'reload_articles_help' => 'Artikel neuladen und komplette Inhalte holen, wenn ein Selektor festgelegt wurde.',
+			'reload_articles_help' => 'Artikel neuladen und komplette Inhalte holen, wenn ein Selektor festgelegt wurde.',	// DIRTY
 			'title' => 'Wartung',
 		),
 		'moved_category_deleted' => 'Wenn Sie eine Kategorie entfernen, werden deren Feeds automatisch in die Kategorie <em>%s</em> eingefügt.',

app/i18n/en-us/sub.php
@@ -108,7 +108,7 @@ return array(
 			'clear_cache' => 'Clear cache',	// IGNORE
 			'clear_cache_help' => 'Clear the cache for this feed.',	// IGNORE
 			'reload_articles' => 'Reload articles',	// IGNORE
-			'reload_articles_help' => 'Reload articles and fetch complete content if a selector is defined.',	// IGNORE
+			'reload_articles_help' => 'Reload that many articles and fetch complete content if a selector is defined.',	// IGNORE
 			'title' => 'Maintenance',	// IGNORE
 		),
 		'moved_category_deleted' => 'When you delete a category, its feeds are automatically classified under <em>%s</em>.',	// IGNORE

app/i18n/en/sub.php
@@ -108,7 +108,7 @@ return array(
 			'clear_cache' => 'Clear cache',
 			'clear_cache_help' => 'Clear the cache for this feed.',
 			'reload_articles' => 'Reload articles',
-			'reload_articles_help' => 'Reload articles and fetch complete content if a selector is defined.',
+			'reload_articles_help' => 'Reload that many articles and fetch complete content if a selector is defined.',
 			'title' => 'Maintenance',
 		),
 		'moved_category_deleted' => 'When you delete a category, its feeds are automatically classified under <em>%s</em>.',

app/i18n/es/sub.php
@@ -108,7 +108,7 @@ return array(
 			'clear_cache' => 'Borrar caché',
 			'clear_cache_help' => 'Borrar la memoria caché de esta fuente.',
 			'reload_articles' => 'Recargar artículos',
-			'reload_articles_help' => 'Vuelva a cargar artículos y obtenga contenido completo si se define un selector.',
+			'reload_articles_help' => 'Vuelva a cargar artículos y obtenga contenido completo si se define un selector.',	// DIRTY
 			'title' => 'Mantenimiento',
 		),
 		'moved_category_deleted' => 'Al borrar una categoría todas sus fuentes pasan automáticamente a la categoría <em>%s</em>.',

app/i18n/fr/sub.php
@@ -108,7 +108,7 @@ return array(
 			'clear_cache' => 'Vider le cache',
 			'clear_cache_help' => 'Supprime le cache de ce flux.',
 			'reload_articles' => 'Recharger les articles',
-			'reload_articles_help' => 'Recharge les articles et récupère le contenu complet si un sélecteur est défini.',
+			'reload_articles_help' => 'Recharge cette quantité d’articles et récupère le contenu complet si un sélecteur est défini.',
 			'title' => 'Maintenance',	// IGNORE
 		),
 		'moved_category_deleted' => 'Lors de la suppression d’une catégorie, ses flux seront automatiquement classés dans <em>%s</em>.',

commit 229c78b2e3bebec27248012074e4f43af0e6e482

JavaScript form validation compatibility older browsers (#6777)
Restore compatibility with older browsers (e.g. Firefox and Chrome older than 2020) and simplify code at the same time
https://developer.mozilla.org/en-US/docs/Web/API/SubmitEvent/submitter
Contributes to https://github.com/FreshRSS/FreshRSS/issues/6776
Was introduced by https://github.com/FreshRSS/FreshRSS/pull/4370 (to be retested a bit more, though)

p/scripts/extra.js
@@ -277,7 +277,7 @@ function init_2stateButton() {
 
 function init_configuration_alert() {
 	window.onsubmit = function (e) {
-		window.hasSubmit = data_leave_validation(document.body, e.submitter ? e.submitter.form : null);
+		window.hasSubmit = data_leave_validation(document.body, e.target);
 	};
 	window.onbeforeunload = function (e) {
 		if (window.hasSubmit) {