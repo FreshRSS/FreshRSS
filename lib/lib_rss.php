5bb33d7d593a0410a90e6a77de6bac78b30ab765
Inverle <inverle@proton.me>
2025-08-28 13:18:07 +0200

    Fix curl response parsing

--- lib/lib_rss.php
+++ lib/lib_rss.php
@@ -539,6 +539,20 @@ function enforceHtmlBase(string $html, string $href): string {
 }
 
 /**
+ * @param string $response Result from curl_exec() with CURLOPT_HEADER option enabled
+ * @return ?\SimplePie\HTTP\Parser<false>
+ */
+function parse_curl_response(string $response): ?\SimplePie\HTTP\Parser {
+	$parser = new \SimplePie\HTTP\Parser($response);
+	if (!$parser->parse()) return null;
+	while ($is_redirect = ($parser->status_code >= 300 && $parser->status_code <= 399)) {
+		$parser = new \SimplePie\HTTP\Parser($parser->body);
+		if (!$parser->parse()) return null;
+	}
+	return $parser;
+}
+
+/**
  * @param string $type {html,ico,json,opml,xml}
  * @param array<string,mixed> $attributes
  * @param array<int,mixed> $curl_options
@@ -643,8 +657,8 @@ function httpGet(string $url, string $cachePath, string $type = 'html', array $a
 	$c_redirect_count = curl_getinfo($ch, CURLINFO_REDIRECT_COUNT);
 	$c_error = curl_error($ch);
 
-	$parser = new \SimplePie\HTTP\Parser(is_string($response) ? $response : '');
-	if ($parser->parse()) {
+	$parser = parse_curl_response(is_string($response) ? $response : '');
+	if ($parser !== null) {
 		$headers = $parser->headers;
 		$body = $parser->body;
 	} else {
