248a07eec143c6ca972e8bb698d808890a9533e8
Alexandre Alapetite <alexandre@alapetite.fr>
2025-09-14 17:41:19 +0200

    Minor explicit namespace fix for PHPUnit

--- tests/cli/i18n/I18nFileTest.php
+++ tests/cli/i18n/I18nFileTest.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 require_once dirname(__DIR__, 3) . '/cli/i18n/I18nFile.php';
 
-class I18nFileTest extends PHPUnit\Framework\TestCase {
+class I18nFileTest extends \PHPUnit\Framework\TestCase {
 	public function test(): void {
 		$before = $this->computeFilesHash();
 
