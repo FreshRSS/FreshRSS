c58e8b44ccb459f27cbc6d9bbec201428ea3b48a
Alexandre Alapetite <alexandre@alapetite.fr>
2025-09-21 13:32:59 +0200

    Update app/i18n/de/feedback.php
    Co-authored-by: maTh <1645099+math-GH@users.noreply.github.com>

--- app/i18n/de/feedback.php
+++ app/i18n/de/feedback.php
@@ -98,7 +98,7 @@ return array(
 			'error' => 'Der Feed kann nicht aktualisiert werden',
 			'favicon' => array(
 				'too_large' => 'Das hochgeladene Symbol ist zu groß. Die maximale Dateigröße beträgt <em>%s</em>.',
-				'unsupported_format' => 'Nicht unterstütztes Bilddateiformat!',
+				'unsupported_format' => 'Das BIlddateiformat wird nicht unterstützt!',
 			),
 			'internal_problem' => 'Der RSS-Feed konnte nicht hinzugefügt werden. Für Details <a href="%s">prüfen Sie die FreshRSS-Protokolle</a>. Mit <code>#force_feed</code> am Ende der Feed-URL kann das Hinzufügen erzwungen werden.',
 			'invalid_url' => 'Die URL <em>%s</em> ist ungültig',
