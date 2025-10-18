4c14b8172c71333d8d2b807653bddbe9f12128b3
Inverle <inverle@proton.me>
2025-10-18 18:52:06 +0200

    improve

--- app/i18n/pl/gen.php
+++ app/i18n/pl/gen.php
@@ -223,7 +223,7 @@ return array(
 		'translated' => 'Progress',	// IGNORE
 	),
 	'search' => array(
-		'advanced_search_help' => 'Ten formularz pomaga w konstruowaniu zapytań dla wyszukiwarki, aczkolwiek ręczne zapytania są jeszcze bardziej potężne.',
+		'advanced_search_help' => 'Ten formularz pomaga w konstruowaniu zapytań dla wyszukiwarki, ale ręczne zapytania są jeszcze potężniejsze.',
 		'authors' => 'Autorzy',
 		'categories' => 'Kategorie',
 		'content' => 'Zawartość',
