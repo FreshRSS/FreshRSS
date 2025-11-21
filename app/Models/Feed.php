e882f8b48f4004d2afd4b58a4d7ea6b9f7b22bb1
Peter Vaverčák <vavercak.peter1@gmail.com>
2025-11-21 11:38:11 +0100

    added getter and seter for local sort

--- app/Models/Feed.php
+++ app/Models/Feed.php
@@ -540,7 +540,6 @@ class FreshRSS_Feed extends Minz_Model {
 	public function set_default_sort(string $sort, string $order) {
 		$this->_attribute('default_sort', $sort);	
 		$this->_attribute('default_order', $order);
-
 	}
 	public function get_default_sort(): string {
 		return $this->attributeString('default_sort') ?? 'id';
