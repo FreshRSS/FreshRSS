930c3cfda894ab96562f915cff8de080b0e0f3f7
Alexandre Alapetite <alexandre@alapetite.fr>
2025-12-29 15:27:29 +0100

    Fix encoding of filter actions for labels fix https://github.com/FreshRSS/FreshRSS/issues/8367 Forgotten from https://github.com/FreshRSS/FreshRSS/pull/8324

--- app/Controllers/tagController.php
+++ app/Controllers/tagController.php
@@ -118,7 +118,7 @@ class FreshRSS_tag_Controller extends FreshRSS_ActionController {
 			}
 
 			if ($ok) {
-				$tag->_filtersAction('label', Minz_Request::paramTextToArray('filteractions_label'));	// Keep as HTML
+				$tag->_filtersAction('label', Minz_Request::paramTextToArray('filteractions_label', plaintext: true));
 				$ok = $tagDAO->updateTagAttributes($tag->id(), $tag->attributes()) !== false;
 			}
 
