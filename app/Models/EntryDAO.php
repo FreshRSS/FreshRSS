781260fa6974682c1f15063a607249ab9b5705a0
Alexandre Alapetite <alexandre@alapetite.fr>
2026-01-23 10:34:25 +0100

    Better criteria

--- app/Models/EntryDAO.php
+++ app/Models/EntryDAO.php
@@ -1454,7 +1454,10 @@ SQL;
 			sort: $sort, order: $order, continuation_id: $continuation_id, continuation_values: $continuation_values);
 
 		// Help MySQL/MariaDB's optimizer with the query plan:
-		$useEntryIndex = $this->pdo->dbType() === 'mysql' && in_array($type, ['a', 'A', 'Z', 'i', 'f'], true) ? 'USE INDEX (entry_feed_read_index) ' : '';
+		$useEntryIndex = ($this->pdo->dbType() === 'mysql' &&	// Only relevant for MySQL/MariaDB,
+				in_array($type, ['a', 'A', 'Z', 'i', 'f'], true) &&	// for some queries,
+				preg_match('/is_read\\s*=\\s*[01]\\b/', $search))	// where is_read is a criteria
+			? 'USE INDEX (entry_feed_read_index) ' : '';
 
 		return [array_merge($values, $searchValues), 'SELECT '
 			. ($type === 'T' ? 'DISTINCT ' : '')
