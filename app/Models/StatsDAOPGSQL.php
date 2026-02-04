4838dbb69edc56fb6ea2c807b988a23f2612dedd
Alexandre Alapetite <alexandre@alapetite.fr>
2026-02-04 22:10:18 +0100

    Date field sanitize

--- app/Models/StatsDAOPGSQL.php
+++ app/Models/StatsDAOPGSQL.php
@@ -5,6 +5,9 @@ class FreshRSS_StatsDAOPGSQL extends FreshRSS_StatsDAO {
 
 	#[\Override]
 	protected function sqlDateToIsoGranularity(string $field, int $precision, string $granularity): string {
+		if (!preg_match('/^[a-zA-Z0-9_.]+$/', $field)) {
+			throw new InvalidArgumentException('Invalid date field!');
+		}
 		$offset = $this->getTimezoneOffset();
 		return match ($granularity) {
 			'day' => "to_char(to_timestamp(($field / $precision) + $offset), 'YYYY-MM-DD')",
