f5d2b6ecaeea4d84671ddecd7f287dfac8a6975b
Inverle <inverle@proton.me>
2026-03-01 14:49:26 +0100

    Update app/i18n/pl/conf.php

--- app/i18n/pl/conf.php
+++ app/i18n/pl/conf.php
@@ -54,7 +54,7 @@ return array(
 			'timeout' => 'Czas wyświetlania powiadomienia HTML5',
 		),
 		'show_nav_buttons' => 'Pokaż przyciski nawigacyjne',
-		'sidebar_hidden_by_default' => 'Hide sidebar by default',	// TODO
+		'sidebar_hidden_by_default' => 'Ukryj pasek boczny domyślnie',
 		'theme' => array(
 			'_' => 'Motyw',
 			'deprecated' => array(
