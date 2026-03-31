5a68af45a172004332ec984666c0697f931fcac6
pe1uca <15692727+pe1uca@users.noreply.github.com>
2026-03-31 08:10:13 -0400

    Remove duplicate return

--- app/Controllers/feedController.php
+++ app/Controllers/feedController.php
@@ -471,7 +471,7 @@ class FreshRSS_feed_Controller extends FreshRSS_ActionController {
 
 		$feeds = Minz_ExtensionManager::callHook(Minz_HookType::FeedsListBeforeActualize, $feeds);
 		if (!is_array($feeds)) {
-			return [$nbUpdatedFeeds, null, $nbNewArticles, $feedsCacheToRefresh];
+			$feeds = [];
 		}
 
 		foreach ($feeds as $feed) {
