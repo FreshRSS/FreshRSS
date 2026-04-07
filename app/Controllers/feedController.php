a7e48878d45f39f93d908c9d204145e1459dc1b0
pe1uca <15692727+pe1uca@users.noreply.github.com>
2026-04-07 15:14:25 -0400

    Update app/Controllers/feedController.php
    Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

--- app/Controllers/feedController.php
+++ app/Controllers/feedController.php
@@ -472,7 +472,7 @@ class FreshRSS_feed_Controller extends FreshRSS_ActionController {
 		$categoriesEntriesGuid = [];
 
 		$feeds = Minz_ExtensionManager::callHook(Minz_HookType::FeedsListBeforeActualize, $feeds);
-		if (!is_array($feeds) && !($feeds instanceof Traversable)) {
+		if (!is_iterable($feeds)) {
 			$feeds = [];
 		}
 
