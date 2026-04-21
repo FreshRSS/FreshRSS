b9fda67890d9dc785b48b3fb0ccf181a40d38eb0
IEEE-754 <253034919+IEEE-754@users.noreply.github.com>
2026-04-22 00:07:25 +0800

    Update the zh-tw translation for install.php

--- app/i18n/zh-TW/install.php
+++ app/i18n/zh-TW/install.php
@@ -13,8 +13,8 @@
 return array(
 	'action' => array(
 		'finish' => '完成安裝',
-		'fix_errors_before' => '請在繼續下一步前修復錯誤',
-		'keep_install' => '保留當前配置',
+		'fix_errors_before' => '請在繼續下一步前修復所有錯誤',
+		'keep_install' => '保留之前的配置',
 		'next_step' => '下一步',
 		'reinstall' => '重新安裝 FreshRSS',
 	),
@@ -22,37 +22,37 @@ return array(
 		'_' => '資料庫',
 		'conf' => array(
 			'_' => '資料庫配置',
-			'ko' => '請驗證你的資料庫資訊',
-			'ok' => '數據庫配置已保存',
+			'ko' => '驗證您的資料庫配置。',
+			'ok' => '資料庫配置已儲存。',
 		),
 		'host' => '主機',
 		'password' => '密碼',
-		'prefix' => '表前綴',
+		'prefix' => '表格前綴',
 		'type' => '資料庫類型',
-		'username' => '使用者名',
+		'username' => '使用者名稱',
 	),
 	'check' => array(
 		'_' => '檢查',
-		'already_installed' => '我們檢測到 FreshRSS 已經安裝！',
+		'already_installed' => '我們偵測到 FreshRSS 已被安裝！',
 		'cache' => array(
-			'nok' => '請檢查 <em>%s</em> 目錄權限。HTTP 伺服器必須有其寫入權限。',
+			'nok' => '請檢查 <em>%2$s</em> 使用者的 <em>%1$s</em> 目錄權限。HTTP 伺服器必須具有寫入權限。',
 			'ok' => 'cache 目錄權限正常',
 		),
 		'ctype' => array(
-			'nok' => '找不到字符類型檢測庫（php-ctype）',
-			'ok' => '已找到字符類型檢測庫',
+			'nok' => '無法找到字元型別檢查所需的函式庫 (php-ctype)。',
+			'ok' => '您擁有字元型別檢查所需的函式庫 (ctype)。',
 		),
 		'curl' => array(
-			'nok' => '找不到 cURL 庫（php-curl）',
-			'ok' => '已找到 cURL 庫',
+			'nok' => '無法找到所需的 cURL 函式庫 (php-curl 套件)。',
+			'ok' => '您擁有所需的 cURL 函式庫。',
 		),
 		'data' => array(
-			'nok' => '請檢查 <em>%s</em> 目錄權限。HTTP 伺服器必須有其寫入權限。',
+			'nok' => '請檢查 <em>%2$s</em> 使用者的 <em>%1$s</em> 目錄權限。HTTP 伺服器必須具有寫入權限。',
 			'ok' => 'data 目錄權限正常',
 		),
 		'database-connection' => array(
-			'nok' => 'Database connection error.',	// TODO
-			'ok' => 'Database connection is good.',	// TODO
+			'nok' => '資料庫連線錯誤。',
+			'ok' => '資料庫連線狀態良好',
 		),
 		'database-table' => array(
 			'nok' => 'Database table "%s" is incomplete.',	// TODO
@@ -62,101 +62,101 @@ return array(
 			'nok' => 'Some database tables are missing.',	// TODO
 			'ok' => 'All database tables exist.',	// TODO
 		),
-		'database-title' => 'Database',	// TODO
+		'database-title' => '資料庫',
 		'dom' => array(
-			'nok' => '找不到用於瀏覽 DOM 的庫（php-xml）',
-			'ok' => '已找到用於瀏覽 DOM 的庫',
+			'nok' => '無法找到瀏覽 DOM 所需的函式庫。',
+			'ok' => '您擁有瀏覽 DOM 所需的函式庫。',
 		),
 		'favicons' => array(
-			'nok' => '請檢查 <em>%s</em> 目錄權限。HTTP 伺服器必須有其寫入權限。',
+			'nok' => '請檢查 <em>%2$s</em> 使用者的 <em>%1$s</em> 目錄權限。HTTP 伺服器必須具有寫入權限。',
 			'ok' => 'favicons 目錄權限正常',
 		),
 		'fileinfo' => array(
-			'nok' => '找不到 PHP fileinfo 庫（php-fileinfo）',
-			'ok' => '已找到 fileinfo 庫',
+			'nok' => '無法找到推薦的 PHP fileinfo 函式庫 (fileinfo 套件)。',
+			'ok' => '您擁有推薦的 PHP fileinfo 函式庫 (fileinfo 套件)。',
 		),
-		'files' => '文件相關',
+		'files' => '檔案安裝',
 		'intl' => array(
-			'nok' => 'Cannot find the recommended library php-intl for internationalisation.',	// TODO
-			'ok' => 'You have the recommended library php-intl for internationalisation.',	// TODO
+			'nok' => '無法找到推薦用於國際化的 php-intl 函式庫。',
+			'ok' => '您擁有推薦用於國際化的 php-intl 函式庫。',
 		),
 		'json' => array(
-			'nok' => '找不到推薦的 JSON 解析庫',
-			'ok' => '已找到推薦的 JSON 解析庫',
+			'nok' => '無法找到解析 JSON 所需的函式庫。',
+			'ok' => '您擁有解析 JSON 所需的函式庫。',
 		),
 		'mbstring' => array(
-			'nok' => '找不到推薦的 Unicode 解析庫（mbstring)',
-			'ok' => '已找到推薦的 Unicode 解析庫',
+			'nok' => '無法找到推薦用於 Unicode 處理的 mbstring 函式庫。',
+			'ok' => '您擁有推薦用於 Unicode 處理的 mbstring 函式庫。',
 		),
 		'pcre' => array(
-			'nok' => '找不到正則表達式解析庫（php-pcre）',
-			'ok' => '已找到正則表達式解析庫',
+			'nok' => '無法找到處理正規表達式所需的函式庫 (php-pcre)。',
+			'ok' => '您擁有處理正規表達式所需的函式庫 (PCRE)。',
 		),
 		'pdo-mysql' => array(
-			'nok' => 'Cannot find the required PDO driver for MySQL/MariaDB.',	// TODO
+			'nok' => '無法找到 MySQL/MariaDB 所需的 PDO 驅動程式。',
 		),
 		'pdo-pgsql' => array(
-			'nok' => 'Cannot find the required PDO driver for PostgreSQL.',	// TODO
+			'nok' => '無法找到 PostgreSQL 所需的 PDO 驅動程式。',
 		),
 		'pdo-sqlite' => array(
-			'nok' => 'Cannot find the PDO driver for SQLite.',	// TODO
-			'ok' => 'You have the PDO driver for SQLite.',	// TODO
+			'nok' => '無法找到 SQLite 的 PDO 驅動程式。',
+			'ok' => '您擁有 SQLite 的 PDO 驅動程式。',
 		),
 		'pdo' => array(
-			'nok' => '找不到 PDO 或支持的驅動（pdo_mysql、pdo_sqlite、pdo_pgsql）',
-			'ok' => '已找到 PDO 和支持的至少一種驅動（pdo_mysql、pdo_sqlite、pdo_pgsql）',
+			'nok' => '無法找到 PDO 或任何支援的驅動程式 (pdo_sqlite, pdo_pgsql, pdo_mysql)。',
+			'ok' => '您擁有 PDO 或至少一個支援的驅動程式 (pdo_sqlite, pdo_pgsql, pdo_mysql)。',
 		),
 		'php' => array(
-			'_' => 'PHP 相關',
-			'nok' => '你的 PHP 版本為 %s，但 FreshRSS 最低需要 %s',
-			'ok' => '你的 PHP 版本為 %s，與 FreshRSS 相容',
+			'_' => 'PHP 安裝',
+			'nok' => '您的 PHP 版本為 %s 但 FreshRSS 需要至少 %s。',
+			'ok' => '您的 PHP 版本為 %s 相容於 FreshRSS。',
 		),
-		'reload' => '再檢查一遍',
+		'reload' => '再次檢查',
 		'tmp' => array(
-			'nok' => '請檢查 <em>%s</em> 目錄權限。HTTP 伺服器必須有其寫入權限。',
-			'ok' => '緩存目錄權限正常。',
+			'nok' => '請檢查 <em>%2$s</em> 使用者的 <em>%1$s</em> 目錄權限。HTTP 伺服器必須具有寫入權限。',
+			'ok' => 'temp 目錄權限正常。',
 		),
 		'tokens' => array(
-			'nok' => '請檢查 <em>./data/tokens</em> 目錄權限。HTTP 伺服器必須有其寫入權限。',
+			'nok' => '請檢查 <em>./data/tokens</em> 目錄權限。HTTP 伺服器必須具有寫入權限。',
 			'ok' => 'tokens 目錄權限正常',
 		),
 		'unknown_process_username' => '未知',
 		'users' => array(
-			'nok' => '請檢查 <em>%s</em> 目錄權限。HTTP 伺服器必須有其寫入權限。',
+			'nok' => '請檢查 <em>%2$s</em> 使用者的 <em>%1$s</em> 目錄權限。HTTP 伺服器必須具有寫入權限。',
 			'ok' => 'users 目錄權限正常',
 		),
 		'xml' => array(
-			'nok' => '找不到用於 XML 解析庫',
-			'ok' => '已找到 XML 解析庫',
+			'nok' => '無法找到解析 XML 所需的函式庫。',
+			'ok' => '您擁有解析 XML 所需的函式庫。',
 		),
 		'zip' => array(
-			'nok' => '找不到 ZIP 擴展（php-zip）',
-			'ok' => '已找到 ZIP 擴展',
+			'nok' => '無法找到推薦的 ZIP 擴充功能 (php-zip package)。',
+			'ok' => '您擁有推薦的 ZIP 擴充功能 (php-zip package)。',
 		),
 	),
 	'conf' => array(
-		'_' => '常規配置',
-		'ok' => '常規配置已保存',
+		'_' => '一般配置',
+		'ok' => '一般配置已儲存',
 	),
 	'congratulations' => '恭喜！',
 	'default_user' => array(
-		'_' => '預設使用者名',
+		'_' => '預設使用者名稱',
 		'max_char' => '最多 16 個數字或字母',
 	),
 	'fix_errors_before' => '請在繼續下一步前修復錯誤',
-	'javascript_is_better' => '啟用 JavaScript 會使 FreshRSS 工作得更好',
+	'javascript_is_better' => '啟用 JavaScript 會使 FreshRSS 使用體驗更佳',
 	'js' => array(
-		'confirm_reinstall' => '重新安裝 FreshRSS 將會重置之前的配置。你確定要繼續嗎？',
+		'confirm_reinstall' => '重新安裝 FreshRSS 將會導致您丟失之前的配置。您確定要繼續嗎？',
 	),
 	'language' => array(
 		'_' => '語言',
 		'choose' => '為 FreshRSS 選擇語言',
-		'defined' => '語言已指定',
+		'defined' => '語言已被定義',
 	),
-	'missing_applied_migrations' => '出現錯誤，你需要手動創建一個空白檔案 <em>%s</em>。',
+	'missing_applied_migrations' => '出現錯誤，您應該建立一個空白檔案 <em>%s</em>。',
 	'ok' => '安裝成功',
 	'session' => array(
-		'nok' => 'Web 伺服器似乎未正確配置 PHP 會話所需的 cookie！',
+		'nok' => 'Web 伺服器似乎未正確配置 PHP 工作階段所需的 cookie！',
 	),
 	'step' => '步驟 %d',
 	'steps' => '步驟',
