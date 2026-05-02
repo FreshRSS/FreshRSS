e10440a671535067499e433cce485d105fc91edc
Alexandre Alapetite <alexandre@alapetite.fr>
2026-05-02 19:18:54 +0200

    Ignore PHPStan warning for PHP 8.2 and PHP 8.3

--- lib/Minz/PdoMysql.php
+++ lib/Minz/PdoMysql.php
@@ -14,7 +14,7 @@ class Minz_PdoMysql extends Minz_Pdo {
 	public function __construct(string $dsn, ?string $username = null, ?string $passwd = null, ?array $options = null) {
 		parent::__construct($dsn, $username, $passwd, $options);
 		if (class_exists('Pdo\Mysql')) {
-			assert(is_int(Pdo\Mysql::ATTR_USE_BUFFERED_QUERY));
+			assert(is_int(Pdo\Mysql::ATTR_USE_BUFFERED_QUERY));	// For PHPStan with PHP 8.4+
 			$this->setAttribute(Pdo\Mysql::ATTR_USE_BUFFERED_QUERY, false);
 		} else {
 			$this->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);	// PHP < 8.4
