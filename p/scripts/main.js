FreshRSS/FreshRSS

commit 57471eda195a205d92245ef852dfc423a3b3a508

Don't mark as read if middle click is outside of article link (#8553)
* Don't mark as read if middle click is outside of article link

Closes https://github.com/FreshRSS/FreshRSS/issues/6451

https://developer.mozilla.org/en-US/docs/Web/API/Element/auxclick_event

> The auxclick event is fired at an [Element](https://developer.mozilla.org/en-US/docs/Web/API/Element) when a non-primary pointing device button (any mouse button other than the primary—usually leftmost—button) has been pressed and released both within the same element.

> auxclick is fired after the [mousedown](https://developer.mozilla.org/en-US/docs/Web/API/Element/mousedown_event) and [mouseup](https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseup_event) events have been fired, in that order.

* Split up `onmouseup` and `onauxclick` logic

Co-authored-by: Frans de Jonge <fransdejonge@gmail.com>

---------

Co-authored-by: Frans de Jonge <fransdejonge@gmail.com>

p/scripts/main.js
@@ -1504,38 +1504,40 @@ function init_stream(stream) {
 		}
 	};
 
-	stream.onmouseup = function (ev) {	// Mouseup enables us to catch middle click, and control+click in IE/Edge
-		if (ev.altKey || ev.metaKey || ev.shiftKey) {
+	stream.onmouseup = function (ev) {	// Mouseup enables us to catch control+click in IE/Edge
+		if (ev.altKey || ev.metaKey || ev.shiftKey || ev.which != 1) {
+			return;
+		}
+
+		const el = ev.target.closest('.item a.title');
+		if (el) {
+			if (ev.ctrlKey) {	// Control+click
+				if (context.auto_mark_site) {
+					mark_read(el.closest('.flux'), true, false);
+				}
+			} else {
+				el.parentElement.click();	// Normal click, just toggle article.
+			}
+		}
+	};
+
+	stream.onauxclick = function (ev) {	// Auxclick enables us to catch middle click
+		if (ev.altKey || ev.metaKey || ev.shiftKey || ev.which != 2 || ev.ctrlKey) {
 			return;
 		}
 
 		let el = ev.target.closest('.item a.title');
 		if (el) {
-			if (ev.which == 1) {
-				if (ev.ctrlKey) {	// Control+click
-					if (context.auto_mark_site) {
-						mark_read(el.closest('.flux'), true, false);
-					}
-				} else {
-					el.parentElement.click();	// Normal click, just toggle article.
-				}
-			} else if (ev.which == 2 && !ev.ctrlKey) {	// Simple middle click: same behaviour as CTRL+click
-				if (context.auto_mark_article) {
-					const new_active = el.closest('.flux');
-					mark_read(new_active, true, false);
-				}
+			if (context.auto_mark_article) {
+				const new_active = el.closest('.flux');
+				mark_read(new_active, true, false);
 			}
 			return;
 		}
 
 		if (context.auto_mark_site) {
-			// catch mouseup instead of click so we can have the correct behaviour
-			// with middle button click (scroll button).
 			el = ev.target.closest('.flux .link > a');
 			if (el) {
-				if (ev.which == 3) {
-					return;
-				}
 				mark_read(el.closest('.flux'), true, false);
 			}
 		}

commit c0d926b9f2e908290be5866a947f7b433f5dc371

Fix click mark as read Fix https://github.com/FreshRSS/FreshRSS/issues/8806 Regression from https://github.com/FreshRSS/FreshRSS/pull/8553 Apply same logic to onmouseup than onauxclick.

p/scripts/main.js
@@ -1512,7 +1512,7 @@ function init_stream(stream) {
 			return;
 		}
 
-		const el = ev.target.closest('.item a.title');
+		let el = ev.target.closest('.item a.title');
 		if (el) {
 			if (ev.ctrlKey) {	// Control+click
 				if (context.auto_mark_site) {
@@ -1520,6 +1520,14 @@ function init_stream(stream) {
 				}
 			} else {
 				el.parentElement.click();	// Normal click, just toggle article.
+			}
+			return;
+		}
+
+		if (context.auto_mark_site) {
+			el = ev.target.closest('.flux .link > a');
+			if (el) {
+				mark_read(el.closest('.flux'), true, false);
 			}
 		}
 	};