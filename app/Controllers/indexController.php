FreshRSS/FreshRSS

commit c2619d9a265b73af022e580e4d126aa5a80be3e1

Fix search in shared user queries (#8789)
The internal search was shown in the UI as the user search.
This was due to the lazy nature of the Generator. Improve the try/catch behaviour at the same time.

How to test:
* Make a user query with a search parameter
* Share the user query as HTML
* Observe the search field (should be empty with this PR,  while it contained the internal search before)

app/Controllers/indexController.php
@@ -258,6 +258,7 @@ class FreshRSS_index_Controller extends FreshRSS_ActionController {
 
 		try {
 			$this->view->entries = FreshRSS_index_Controller::listEntriesByContext();
+			$this->view->entries->current();	// Init the generator to catch potential exceptions
 		} catch (FreshRSS_EntriesGetter_Exception $e) {
 			Minz_Log::notice($e->getMessage());
 			Minz_Error::error(404);
@@ -335,10 +336,10 @@ class FreshRSS_index_Controller extends FreshRSS_ActionController {
 	/**
 	 * This method returns a list of entries based on the Context object.
 	 * @param int $postsPerPage override `FreshRSS_Context::$number`
-	 * @return Traversable<FreshRSS_Entry>
+	 * @return Generator<FreshRSS_Entry>
 	 * @throws FreshRSS_EntriesGetter_Exception
 	 */
-	public static function listEntriesByContext(?int $postsPerPage = null): Traversable {
+	public static function listEntriesByContext(?int $postsPerPage = null): Generator {
 		$entryDAO = FreshRSS_Factory::createEntryDao();
 
 		$get = FreshRSS_Context::currentGet(true);

p/api/query.php
@@ -117,8 +117,10 @@ $view = new FreshRSS_View();
 
 try {
 	FreshRSS_Context::updateUsingRequest(false);
-	Minz_Request::_param('search', $userSearch->toString());	// Restore user search
 	$view->entries = FreshRSS_index_Controller::listEntriesByContext();
+	$view->entries->current();	// Init the generator to consume the aggregated search and catch potential exceptions
+	Minz_Request::_param('search', $userSearch->toString());	// Restore user search for display and exports
+	FreshRSS_Context::$search = $userSearch;	// Restore user search for display and exports
 } catch (Minz_Exception) {
 	Minz_Error::error(400, 'Bad user query!');
 	die();

commit 370f9d5fa87f32c2d87d5e43a9b826459d5e7ea8

Fix empty entry Generator Fix https://github.com/FreshRSS/FreshRSS/issues/8857 Regression from https://github.com/FreshRSS/FreshRSS/pull/8789

app/Controllers/indexController.php
@@ -259,6 +259,9 @@ class FreshRSS_index_Controller extends FreshRSS_ActionController {
 		try {
 			$this->view->entries = FreshRSS_index_Controller::listEntriesByContext();
 			$this->view->entries->current();	// Init the generator to catch potential exceptions
+			if (!$this->view->entries->valid()) {
+				$this->view->entries = (static function () { yield from []; })();
+			}
 		} catch (FreshRSS_EntriesGetter_Exception $e) {
 			Minz_Log::notice($e->getMessage());
 			Minz_Error::error(404);

p/api/query.php
@@ -119,6 +119,9 @@ try {
 	FreshRSS_Context::updateUsingRequest(false);
 	$view->entries = FreshRSS_index_Controller::listEntriesByContext();
 	$view->entries->current();	// Init the generator to consume the aggregated search and catch potential exceptions
+	if (!$view->entries->valid()) {
+		$view->entries = (static function () { yield from []; })();
+	}
 	Minz_Request::_param('search', $userSearch->toString());	// Restore user search for display and exports
 	FreshRSS_Context::$search = $userSearch;	// Restore user search for display and exports
 } catch (Minz_Exception) {