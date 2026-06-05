2a679f0903d2a384701d87ebec5bfe9826b95462
Inverle <inverle@proton.me>
2026-06-05 18:25:44 +0200

    Set limits for regex during search

--- app/Models/EntryDAOSQLite.php
+++ app/Models/EntryDAOSQLite.php
@@ -54,7 +54,16 @@ class FreshRSS_EntryDAOSQLite extends FreshRSS_EntryDAO {
 		// https://www.sqlite.org/lang_expr.html#the_like_glob_regexp_match_and_extract_operators
 		$this->pdo->sqliteCreateFunction('regexp',
 			function (string $pattern, string $text): bool {
-				return preg_match($pattern, $text) === 1;
+				$oldBacktrackLimit = ini_set('pcre.backtrack_limit', '10000');
+				$oldRecursionLimit = ini_set('pcre.recursion_limit', '100');
+				$matches = preg_match($pattern, $text) === 1;
+				if ($oldBacktrackLimit !== false) {
+					ini_set('pcre.backtrack_limit', $oldBacktrackLimit);
+				}
+				if ($oldRecursionLimit !== false) {
+					ini_set('pcre.recursion_limit', $oldRecursionLimit);
+				}
+				return $matches;
 			},
 			2
 		);
