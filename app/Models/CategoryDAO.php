94f89fdc4891553e088920b730cf228341a3e0ea
Alexandre Alapetite <alexandre@alapetite.fr>
2026-06-05 14:11:12 +0200

    Fix likely bug

--- app/Models/CategoryDAO.php
+++ app/Models/CategoryDAO.php
@@ -497,7 +497,7 @@ class FreshRSS_CategoryDAO extends Minz_ModelPdo {
 					$previousLine['c_name'],
 					$previousLine['c_id'],
 					$feedDao::daoToFeeds($feedsDao, $previousLine['c_id']),
-					$previousLine['c_attributes'] ?? '[]'
+					$previousLine['c_attributes'] ?? null
 				);
 				$cat->_kind($previousLine['c_kind']);
 				$cat->_lastUpdate($previousLine['c_last_update'] ?? 0);
@@ -517,7 +517,8 @@ class FreshRSS_CategoryDAO extends Minz_ModelPdo {
 			$cat = new FreshRSS_Category(
 				$previousLine['c_name'],
 				$previousLine['c_id'],
-				$feedDao::daoToFeeds($feedsDao, $previousLine['c_id'])
+				$feedDao::daoToFeeds($feedsDao, $previousLine['c_id']),
+				$previousLine['c_attributes'] ?? null
 			);
 			$cat->_kind($previousLine['c_kind']);
 			$cat->_lastUpdate($previousLine['c_last_update'] ?? 0);
