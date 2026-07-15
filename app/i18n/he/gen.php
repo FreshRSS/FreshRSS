706ba3e97deae98e0a86f611ece5fd0dc7bb5f9a
Gerard Alvear <gerard.alvear@logiqd.me>
2026-07-15 13:29:03 +0200

    i18n: retain untranslated Hebrew username hint

--- app/i18n/he/gen.php
+++ app/i18n/he/gen.php
@@ -73,7 +73,7 @@ return array(
 		),
 		'username' => array(
 			'_' => 'שם משתמש',
-			'format' => '<small>1-39 characters: letters, digits, and . _ @ -</small>',	// DIRTY
+			'format' => '<small>1-39 characters: letters, digits, and . _ @ -</small>',	// TODO
 		),
 	),
 	'date' => array(
