0269042f1af2abc6835dd4d17e8772b59081e943
Gerard Alvear <gerard.alvear@logiqd.me>
2026-07-22 15:58:50 +0200

    Translate Polish username format

--- app/i18n/pl/gen.php
+++ app/i18n/pl/gen.php
@@ -73,7 +73,7 @@ return array(
 		),
 		'username' => array(
 			'_' => 'Nazwa użytkownika',
-			'format' => '<small>1-39 characters: letters, digits, and <code>. _ @ -</code></small>',	// TODO
+			'format' => '<small>1-39 znaków: litery, cyfry oraz <code>. _ @ -</code></small>',
 		),
 	),
 	'date' => array(
