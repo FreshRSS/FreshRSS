FreshRSS/FreshRSS

commit 1b7178a18beff3b2f73dafb3ed7a1b68c9494222

Security: apply encodeURIComponent to share template (#9146)
For instance if an article title is `<title>XSS &quot;&gt;&lt;img src=x&gt; END</title>`

Reported by @meifukun

p/scripts/main.js
@@ -822,11 +822,11 @@ function show_share_menu(el) {
 		const title = title_el.textContent;
 		const titleText = title;
 		const template = document.getElementById(templateId).innerHTML
-			.replace(/--entryId--/g, id)
-			.replace(/--link--/g, link)
-			.replace(/--titleText--/g, titleText)
-			.replace(/--websiteName--/g, websiteName)
-			.replace(/--articleAuthors--/g, articleAuthorsText);
+			.replace(/--entryId--/g, encodeURIComponent(id))
+			.replace(/--link--/g, encodeURIComponent(link))
+			.replace(/--titleText--/g, encodeURIComponent(titleText))
+			.replace(/--websiteName--/g, encodeURIComponent(websiteName))
+			.replace(/--articleAuthors--/g, encodeURIComponent(articleAuthorsText));
 
 		div.insertAdjacentHTML('beforeend', template);
 	}

commit 1b91d5d2170df6aa889897822e571be19b1367a4

Fix regression sharing links Fix https://github.com/FreshRSS/FreshRSS/issues/9302 Some links must be decoded before use. Regression from https://github.com/FreshRSS/FreshRSS/pull/9146

p/scripts/main.js
@@ -1523,7 +1523,7 @@ function init_stream(stream) {
 		el = ev.target.closest('.item.share > button[data-type="clipboard"]');
 		if (el) { // Clipboard
 			if (navigator.clipboard) {
-				navigator.clipboard.writeText(el.dataset.url)
+				navigator.clipboard.writeText(decodeURIComponent(el.dataset.url))
 					.then(() => {
 						toggleClass(el, 'ok');
 					})
@@ -1534,7 +1534,7 @@ function init_stream(stream) {
 			} else {
 				// fallback, if navigator.clipboard is not available f.e. if access is not via https or localhost
 				const inputElement = document.createElement('input');
-				inputElement.value = el.dataset.url;
+				inputElement.value = decodeURIComponent(el.dataset.url);
 				document.body.appendChild(inputElement);
 				inputElement.select();
 				if (document.execCommand && document.execCommand('copy')) {
@@ -1552,7 +1552,7 @@ function init_stream(stream) {
 		el = ev.target.closest('.item.share > button[data-type="web-sharing-api"]');
 		if (el && navigator.share) {	// https://developer.mozilla.org/docs/Web/API/Navigator/share
 			const shareData = {
-				url: el.dataset.url,
+				url: decodeURIComponent(el.dataset.url),
 				title: decodeURI(el.dataset.title),
 			};
 			navigator.share(shareData);
@@ -1569,6 +1569,8 @@ function init_stream(stream) {
 		if (el) {	// Share by POST
 			const f = el.parentElement.querySelector('form');
 			f.disabled = false;
+			const input = f.querySelector('input');
+			input.value = decodeURIComponent(input.value);
 			f.submit();
 			return false;
 		}